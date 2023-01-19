aa3184eb97f462c019181e6aacf78ebaa39cd1c8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-19 17:04:29 +0400

    Global Variable

---
+++ JS/Javatpoint/p1/p1.js
@@ -0,0 +1,14 @@
+// Javascript Example
+
+console.log("Hello World"); // document.write
+
+// creating first Javascript External File
+
+/* <script type="text/javascript">  
+document.write("JavaScript is a simple language for javatpoint learners");  
+</script> } */
+
+
+/// code between the body tag
+
+alert("Hello Java")
---
+++ JS/Javatpoint/var/var.js
@@ -0,0 +1,7 @@
+var x = 10;
+
+var y = 20;
+
+var z = x + y;
+
+console.log(z);
