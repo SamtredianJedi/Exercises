2c06e83b0d369b2d60affbb27c98c5a1daabb063
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-23 23:53:36 +0400

    stack

---
+++ JS/JS Algorithms/stack.js
@@ -0,0 +1,118 @@
+// Array-based Solution
+
+export default class Stack {
+    constructor() {
+        this._items = [];
+    }
+
+    /**
+     * Pushes an item onto the top of the stack.
+     * @param {*} item The item to be pushed onto the stack.
+     * @return {number} The new length of the stack.
+     */
+    push(item) {
+        return this._items.push(item);
+    }
+
+    /**
+     * Remove an item at the top of the stack.
+     * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+     */
+    pop() {
+        return this._items.pop();
+    }
+
+    /**
+     * Determines if the stack is empty.
+     * @return {boolean} `true` if the stack has no items, `false` otherwise.
+     */
+    isEmpty() {
+        return this.length() === 0;
+    }
+
+    /**
+     * Returns the item at the top of the stack without removing it from the stack.
+     * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+     */
+    peek() {
+        return this.isEmpty() ? undefined : this._items[this.length() - 1];
+    }
+
+    /**
+     * Returns the number of items in the stack.
+     * @return {number} The number of items in the stack.
+     */
+    length() {
+        return this._items.length;
+    }
+}
+
+
+// Linked List-based Solution
+
+// class Node {
+//     constructor(value) {
+//       this.value = value;
+//       this.prev = null;
+//     }
+//   }
+
+//   export default class Stack {
+//     constructor() {
+//       this._top = null;
+//       this._length = 0;
+//     }
+
+//     /**
+//      * Pushes an item onto the top of the stack.
+//      * @param {*} item The item to be pushed onto the stack.
+//      * @return {number} The new length of the stack.
+//      */
+//     push(item) {
+//       const node = new Node(item);
+//       node.prev = this._top;
+//       this._top = node;
+//       this._length++;
+//       return this._length;
+//     }
+
+//     /**
+//      * Remove an item at the top of the stack.
+//      * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+//      */
+//     pop() {
+//       if (this.isEmpty()) {
+//         return undefined;
+//       }
+
+//       const node = this._top;
+//       this._top = node.prev;
+//       node.prev = null;
+//       this._length--;
+//       return node.value;
+//     }
+
+//     /**
+//      * Determines if the stack is empty.
+//      * @return {boolean} `true` if the stack has no items, `false` otherwise.
+//      */
+//     isEmpty() {
+//       return this._length === 0;
+//     }
+
+//     /**
+//      * Returns the item at the top of the stack without removing it from the stack.
+//      * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+//      */
+//     peek() {
+//       return this.isEmpty() ? undefined : this._top.value;
+//     }
+
+//     /**
+//      * Returns the number of items in the stack.
+//      * @return {number} The number of items in the stack.
+//      */
+//     length() {
+//       return this._length;
+//     }
+//   }
