359522830cfc610638d1cf3f8a739b82ef68a1fc
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-25 21:45:45 +0400

    checkPrimeNum

---
+++ JS/Programs/randomNum.js
@@ -0,0 +1,17 @@
+// generating a random number
+const a = Math.random() * (10 - 1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
+
+
+/**
+ * 
+ * // input from the user
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
+ */
