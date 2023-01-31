80db18c3368849abf0ad623320581bd4d5d870fb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-31 22:09:45 +0400

    Prime Number Program in Java

--- Java/src/main/Programs/Basic Programs/task10.java
+++ Java/src/main/Programs/Basic Programs/task10.java
@@ -1,5 +1,7 @@
 // Prime Number Program in Java
 
+
+
 Prime number in Java: Prime number is a number that is greater than 1 and divided by 1 or itself only. In other words, prime numbers can't be divided by other numbers than itself or 1. For example 2, 3, 5, 7, 11, 13, 17.... are the prime numbers.
 
 Note: 0 and 1 are not prime numbers. The 2 is the only even prime number because all the other even numbers can be divided by 2.
