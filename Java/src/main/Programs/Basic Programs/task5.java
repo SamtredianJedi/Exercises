f7a4b0c30f8adba9243238195b05199b2c8a8c65
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-31 22:06:51 +0400

    How to Create Object in Java

--- Java/src/main/Programs/Basic Programs/task5.java
+++ Java/src/main/Programs/Basic Programs/task5.java
@@ -1,5 +1,7 @@
 // How to Create Object in Java
 
+
+
 The object is a basic building block of an OOPs language. In Java, we cannot execute any program without creating an object. There is various way to create an object in Java that we will discuss in this section, and also learn how to create an object in Java.
 
 Java provides five ways to create an object.
