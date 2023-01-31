652a5e670a6f1b95f0dfc5e35114fb735fa7d82b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-31 22:09:13 +0400

    Palindrome Program in Java

--- Java/src/main/Programs/Basic Programs/task9.java
+++ Java/src/main/Programs/Basic Programs/task9.java
@@ -1,5 +1,7 @@
 // Palindrome Program in Java
 
+
+
 Palindrome number in java: A palindrome number is a number that is same after reverse. For example 545, 151, 34543, 343, 171, 48984 are the palindrome numbers. It can also be a string like LOL, MADAM etc.
 
 Palindrome number algorithm
