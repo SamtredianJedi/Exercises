fbb9f187d8f53462ef56fecf67a7352b2d6df819
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-31 22:05:42 +0400

    Fibonacci series in Java

--- Java/src/main/Programs/Basic Programs/task3.java
+++ Java/src/main/Programs/Basic Programs/task3.java
@@ -1,6 +1,8 @@
 // Fibonacci series in Java
 
 
+
+
 In fibonacci series, next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. The first two numbers of fibonacci series are 0 and 1.
 
 There are two ways to write the fibonacci series program in java:
