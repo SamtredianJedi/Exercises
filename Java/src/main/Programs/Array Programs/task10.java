a7eb9a0e75e2811fd085a76338c0dcdd664d9e0d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-04 23:05:46 +0400

    Java Program to remove duplicate element in an Array

--- Java/src/main/Programs/Array Programs/task10.java
+++ Java/src/main/Programs/Array Programs/task10.java
@@ -1,5 +1,7 @@
 // Java Program to remove duplicate element in an Array
 
+
+
 We can remove duplicate element in an array by 2 ways: using temporary array or using separate index. To remove the duplicate element from array, the array must be in sorted order. If array is not sorted, you can sort it by calling Arrays.sort(arr) method.
 
 1) Remove Duplicate Element in Array using Temporary Array
