9a80163e7ced62bb9a3366d88b021469360391e8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-05 20:57:54 +0400

    Program to print the largest element in an array

--- Java/src/main/Programs/Array Programs/task22.java
+++ Java/src/main/Programs/Array Programs/task22.java
@@ -1,5 +1,7 @@
 // Program to print the largest element in an array
 
+
+
 In this program, we need to find out the largest element present in the array and display it. This can be accomplished by looping through the array from start to end by comparing max with all the elements of an array. If any of element is greater than max, then store a value of the element in max. Initially, max will hold the value of the first element. At the end of the loop, max represents the largest element in the array.
 
 In the above array, initially, max will hold the value 25. In the 1st iteration, max will be compared with 11, since 11 is less than max. Max will retain its value. In the next iteration, it will be compared to 7, 7 is also less than max, no change will be made to the max. Now, max will be compared to 75. 75 is greater than max so that max will hold the value of 75. Continue this process until the end of the array is reached. At the end of the loop, max will hold the largest element in the array.
