521b5496047f775c74ac5e0966174e6bf9901a5f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-05 20:58:22 +0400

    Program to print the smallest element in an array

--- Java/src/main/Programs/Array Programs/task23.java
+++ Java/src/main/Programs/Array Programs/task23.java
@@ -1,5 +1,7 @@
 // Program to print the smallest element in an array
 
+
+
 In this program, we need to find out the smallest element present in the array. This can be achieved by maintaining a variable min which initially will hold the value of the first element. Loop through the array by comparing the value of min with elements of the array. If any of the element's value is less than min, store the value of the element in min.
 
 Consider above array. Initially, min will hold the value 25. In the 1st iteration, min will be compared with 11. Since 11 is less than 25. Min will hold the value 11. In a 2nd iteration, 11 will be compared with 7. Now, 7 is less than 11. So, min will take the value 7. Continue this process until the end of the array is reached. At last, min will hold the smallest value element in the array.
