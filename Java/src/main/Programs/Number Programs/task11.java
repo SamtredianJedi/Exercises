3c890abe10ef327f5e96ed29e50b1b3df0e55d49
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-07 23:33:07 +0400

    ISBN Number Java

--- Java/src/main/Programs/Number Programs/task11.java
+++ Java/src/main/Programs/Number Programs/task11.java
@@ -1,5 +1,7 @@
 // ISBN Number Java
 
+
+
 ISBN is another special number in Java. ISBN stands for the International Standard Book Number that is carried by almost each every book. The ISBN is a ten-digit unique number. With the help of the ISBN, we can easily find any book. The ISBN number is a legal number when 1*Digit1 + 2*Digit2 + 3*Digit3 + 4*Digit4 + 5*Digit5 + 6*Digit6 + 7*Digit7 + 8*Digit8 + 9*Digit9 + 10*Digit10 is divisible by 11. The digits are taken from right to left. So, if the ten-digit number is 7426985414, Digit1 and Digit10 will be 4 and 7, respectively.
 
 Let's take two numbers and check whether the numbers are legal or not.
