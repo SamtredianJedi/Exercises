b1ce9589031dbb85ab1c5079146bfffa0c9814bb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-08 23:09:15 +0400

    Java Program to Find Sum of Natural Numbers

--- Java/src/main/Programs/Number Programs/task22.java
+++ Java/src/main/Programs/Number Programs/task22.java
@@ -1,5 +1,7 @@
 // Java Program to Find Sum of Natural Numbers
 
+
+
 The natural numbers are the numbers that include all the positive integers from 1 to infinity. For example, 1, 2, 3, 4, 5, ......, n. When we add these numbers together, we get the sum of natural numbers.
 
 In this section, we will create the following programs:
