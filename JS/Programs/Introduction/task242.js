60d53346c8988974b6131ed57bcebd36fea8ff94
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-11 23:41:30 +0400

    JavaScript: Change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a

--- JS/Programs/Introduction/task242.js
+++ JS/Programs/Introduction/task242.js
@@ -1,6 +1,8 @@
 // JavaScript: Change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a
 
 
+
+
 /**
  * 
  * 
