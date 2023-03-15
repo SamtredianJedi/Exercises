2da179ed111fa6241432df0915a902d0d424fcbf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-15 23:46:48 +0400

    update1

---
+++ JS/Programs/Introduction/task287.js
@@ -0,0 +1,14 @@
+// JavaScript: Accept a list of words and returns the longest
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-25 with Solution
+Write a JavaScript function that accept a list of country names as input and returns the longest country name as output
+
+Sample function : Longest_Country_Name(["Australia", "Germany", "United States of America"])
+Expected output : "United States of America"
+
+
+ */
