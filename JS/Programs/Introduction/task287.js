1e3e62eb66a305647c9cfdc4312602a25b21f743
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-15 23:48:27 +0400

    update3

--- JS/Programs/Introduction/task287.js
+++ JS/Programs/Introduction/task287.js
@@ -28,4 +28,19 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+function Longest_Country_Name(country_name)
+  {
+  return country_name.reduce(function(lname, country) 
+  {
+    return lname.length > country.length ? lname : country;
+  }, 
+"");
+}
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
+
+
  */
