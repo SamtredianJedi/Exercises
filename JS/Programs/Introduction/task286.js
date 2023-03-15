73798c48a68820cd5d07adefdd710b51e5939a8f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-15 23:45:48 +0400

    update3

--- JS/Programs/Introduction/task286.js
+++ JS/Programs/Introduction/task286.js
@@ -64,4 +64,49 @@ console.log(bubble_Sort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 
 
 
 
+
+Live Demo:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Bubble Sort algorithm</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+
+function bubble_Sort(a)
+{
+    var swapp;
+    var n = a.length-1;
+    var x=a;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+            if (x[i] < x[i+1])
+            {
+               var temp = x[i];
+               x[i] = x[i+1];
+               x[i+1] = temp;
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+ return x; 
+}
+
+console.log(bubble_Sort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
+
+
  */
