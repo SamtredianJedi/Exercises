bcdc85bc738040ac4c3f0b6463f27e4a16a2685e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-15 23:49:21 +0400

    update4

--- JS/Programs/Introduction/task287.js
+++ JS/Programs/Introduction/task287.js
@@ -43,4 +43,39 @@ function Longest_Country_Name(country_name)
 console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Find longest word</title>
+</head>
+<body>
+​
+</body>
+
+
+
+
+
+
+
+function Longest_Country_Name(country_name)
+  {
+  return country_name.reduce(function(lname, country) 
+  {
+    return lname.length > country.length ? lname : country;
+  }, 
+"");
+}
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
+
+
  */
