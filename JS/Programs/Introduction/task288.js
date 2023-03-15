053e605814c776011f9a32dcbaba93e7f3070eb6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-15 23:50:27 +0400

    tasks

---
+++ JS/Programs/Introduction/task288.js
@@ -0,0 +1,28 @@
+// JavaScript: Tips of the Day 8
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * const tips_dig = (obj, target) =>
+  target in obj
+    ? obj[target]
+    : Object.values(obj).reduce((acc, val) => {
+      if (acc !== undefined) return acc;
+      if (typeof val === 'object') return tips_dig(val, target);
+    }, undefined);
+
+const data = {
+  level1: {
+    level2: {
+      level3: 'JavaScript'
+    }
+  }
+};
+console.log(tips_dig(data, 'level3'));
+console.log(tips_dig(data, 'level4'));
+
+
+
+ */
