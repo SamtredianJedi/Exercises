ca70d4e37dea2eb7c66987e349e0d332c83de29b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-16 23:36:45 +0400

    update1

---
+++ JS/Programs/Introduction/task290.js
@@ -0,0 +1,15 @@
+// JavaScript: The longest palindrome in a specified string
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-27 with Solution
+Write a JavaScript function that returns the longest palindrome in a given string.
+
+Note: According to Wikipedia "In computer science, the longest palindromic substring or longest symmetric factor problem is the problem of finding a maximum-length contiguous substring of a given string that is also a palindrome. For example, the longest palindromic substring of "bananas" is "anana". The longest palindromic substring is not guaranteed to be unique; for example, in the string "abracadabra", there is no palindromic substring with length greater than three, but there are two palindromic substrings with length three, namely, "aca" and "ada".
+In some applications it may be necessary to return all maximal palindromic substrings (that is, all substrings that are themselves palindromes and cannot be extended to larger palindromic substrings) rather than returning only one substring or returning the maximum length of a palindromic substring.
+
+
+
+ */
