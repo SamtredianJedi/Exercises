344700e31e7c167755270c6fc2074392edbd273d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-16 23:42:09 +0400

    update1

---
+++ JS/Programs/Introduction/task291.js
@@ -0,0 +1,11 @@
+// JavaScript: Pass a JavaScript function as parameter
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-28 with Solution
+Write a JavaScript program to pass a 'JavaScript function' as parameter.
+
+
+ */
