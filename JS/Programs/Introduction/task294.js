eb474fd20619e50c015851781f7d88e5280c3f09
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:34:23 +0400

    task

---
+++ JS/Programs/Introduction/task294.js
@@ -0,0 +1,13 @@
+// JavaScript: Find the greatest common divisor of two positive numbers
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-2 with Solution
+Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
+
+
+ */
