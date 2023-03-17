54eb674ae612a9da243ece49a51796c98184f785
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:36:38 +0400

    update3

--- JS/Programs/Introduction/task294.js
+++ JS/Programs/Introduction/task294.js
@@ -41,4 +41,39 @@ var gcd = function(a, b) {
 console.log(gcd(2154, 458));
 
 
+
+
+Live Demo:
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Recursive function to find the GCD of two numbers</title>
+</head>
+<body>
+
+</body>
+
+
+
+
+js:
+
+
+
+
+var gcd = function(a, b) {
+    if ( ! b) {
+        return a;
+    }
+​
+    return gcd(b, a % b);
+};
+console.log(gcd(2154, 458));
+
+
  */
