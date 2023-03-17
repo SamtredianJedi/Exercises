cb507e741420670458cba1d3d86a9db4a4b7784f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:31:53 +0400

    update3

--- JS/Programs/Introduction/task293.js
+++ JS/Programs/Introduction/task293.js
@@ -34,4 +34,21 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+function factorial(x) 
+{ 
+
+  if (x === 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);
+         
+}
+console.log(factorial(5));
+
+
  */
