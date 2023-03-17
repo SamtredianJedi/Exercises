808acc27a4f2c23d5dcc27354b14dd28d2a08610
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:35:23 +0400

    update1

--- JS/Programs/Introduction/task294.js
+++ JS/Programs/Introduction/task294.js
@@ -10,4 +10,21 @@
 Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
 
 
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Recursive function to find the GCD of two numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
