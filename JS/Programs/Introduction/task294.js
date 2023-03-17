7a88fda7f3de501bf2481ba1ded80fe016c4506a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:35:51 +0400

    update2

--- JS/Programs/Introduction/task294.js
+++ JS/Programs/Introduction/task294.js
@@ -27,4 +27,18 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+var gcd = function(a, b) {
+    if ( ! b) {
+        return a;
+    }
+
+    return gcd(b, a % b);
+};
+console.log(gcd(2154, 458));
+
+
  */
