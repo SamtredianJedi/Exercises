4d8637d462b515a082b2634430bf642219d12c41
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-17 23:30:56 +0400

    update1

--- JS/Programs/Introduction/task293.js
+++ JS/Programs/Introduction/task293.js
@@ -10,4 +10,12 @@
  * 
  * 
  * 
+ * 
+ JavaScript Function: Exercise-1 with Solution
+Write a JavaScript program to calculate the factorial of a number.
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+
+
  */
