355bdf5333690f3b3f346886fd70199cf5d27f9a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:33:42 +0400

    task

---
+++ JS/Programs/Introduction/task295.js
@@ -0,0 +1,20 @@
+// JavaScript: Tips of the Day 8
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * Returns a list of elements that exist in both arrays
+
+Example:
+
+const tips_intersection = (a, b) => {
+  const s = new Set(b);
+  return [...new Set(a)].filter(x => s.has(x));
+};
+
+console.log(tips_intersection([1, 2, 3, 5], [5, 4, 3, 2]));
+
+
+ */
