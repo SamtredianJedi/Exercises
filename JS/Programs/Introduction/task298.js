660c359d4908145554e96a5d3a77085c2aac15b7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:45:03 +0400

    task

---
+++ JS/Programs/Introduction/task298.js
@@ -0,0 +1,15 @@
+// JavaScript: Compute the exponent of a number
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-5 with Solution
+Write a JavaScript program to compute the exponent of a number.
+
+Note: The exponent of a number says how many times the base number is used as a factor.
+82 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent.
+
+
+ */
