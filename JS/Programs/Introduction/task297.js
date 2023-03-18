3f56bebb8234daa674424bc1293ab2dce3cb5524
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:40:58 +0400

    task

---
+++ JS/Programs/Introduction/task297.js
@@ -0,0 +1,17 @@
+// JavaScript: Compute the sum of an array of integers
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-4 with Solution
+Write a JavaScript program to compute the sum of an array of integers.
+
+Example : var array = [1, 2, 3, 4, 5, 6]
+Expected Output : 21
+
+
+
+ */
