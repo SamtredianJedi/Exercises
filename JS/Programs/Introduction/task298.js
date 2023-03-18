69ba36fb0415fc2edad39758fea87d9af4029786
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:47:06 +0400

    update3

--- JS/Programs/Introduction/task298.js
+++ JS/Programs/Introduction/task298.js
@@ -29,4 +29,23 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+var exponent = function(a, n) 
+{
+   if (n === 0) 
+   {
+    return 1;
+    }
+  else 
+  {
+    return a * exponent(a, n-1);
+  }
+};
+
+console.log(exponent(4, 2));
+
+
  */
