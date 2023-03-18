a156d6917fd7eeed336099f7f9eed314edf7c588
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:46:20 +0400

    update1

--- JS/Programs/Introduction/task298.js
+++ JS/Programs/Introduction/task298.js
@@ -12,4 +12,21 @@ Note: The exponent of a number says how many times the base number is used as a 
 82 = 8 x 8 = 64. Here 8 is the base and 2 is the exponent.
 
 
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exponent of a number</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
