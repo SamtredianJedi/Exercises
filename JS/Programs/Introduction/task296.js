79488da499b42dd201f47ff8ce911294648bd4b2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:38:33 +0400

    update3

--- JS/Programs/Introduction/task296.js
+++ JS/Programs/Introduction/task296.js
@@ -52,4 +52,44 @@ var range = function(start_num, end_num)
 console.log(range(2,9));
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Gets the integers from x to y</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+
+var range = function(start_num, end_num) 
+{
+  if (end_num - start_num === 2) 
+  {
+    return [start_num + 1];
+  } 
+  else 
+  {
+    var list = range(start_num, end_num - 1);
+    list.push(end_num - 1);
+    return list;
+  }
+};
+
+console.log(range(2,9));
+
+
+
  */
