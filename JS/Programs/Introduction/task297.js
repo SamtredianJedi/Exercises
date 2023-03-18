de98f0c2e258c250354eff95be14f46fd6d1cae0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:42:12 +0400

    update2

--- JS/Programs/Introduction/task297.js
+++ JS/Programs/Introduction/task297.js
@@ -31,4 +31,19 @@ HTML Code:
 </html>
 
 
+
+JavaScript Code:
+
+var array_sum = function(my_array) {
+  if (my_array.length === 1) {
+    return my_array[0];
+  }
+  else {
+    return my_array.pop() + array_sum(my_array);
+  }
+};
+
+console.log(array_sum([1,2,3,4,5,6]));
+
+
  */
