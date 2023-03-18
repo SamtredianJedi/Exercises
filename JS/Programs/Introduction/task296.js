78274c547a2fca13c715cc68651c950446fafa91
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:35:07 +0400

    task

---
+++ JS/Programs/Introduction/task296.js
@@ -0,0 +1,16 @@
+// JavaScript: Get the integers in a range
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-3 with Solution
+Write a JavaScript program to get the integers in range (x, y).
+
+Example : range(2, 9)
+Expected Output : [3, 4, 5, 6, 7, 8]
+
+
+
+ */
