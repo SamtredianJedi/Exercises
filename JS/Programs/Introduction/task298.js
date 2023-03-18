b1fe0839901c9944731e89502802bc24d56c7ab9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-18 23:47:57 +0400

    update3

--- JS/Programs/Introduction/task298.js
+++ JS/Programs/Introduction/task298.js
@@ -48,4 +48,44 @@ var exponent = function(a, n)
 console.log(exponent(4, 2));
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Exponent of a number</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+
+
+var exponent = function(a, n) 
+{
+   if (n === 0) 
+   {
+    return 1;
+    }
+  else 
+  {
+    return a * exponent(a, n-1);
+  }
+};
+
+console.log(exponent(4, 2));
+
+
  */
