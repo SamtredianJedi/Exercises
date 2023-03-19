76cb5fcaa2b5125ac8373bd52e8b3631820368a3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:28:32 +0400

    task

---
+++ JS/Programs/Introduction/task299.js
@@ -0,0 +1,16 @@
+// JavaScript: Get the first n Fibonacci numbers
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-6 with Solution
+Write a JavaScript program to get the first n Fibonacci numbers.
+
+Note: The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
+
+
+
+ */
