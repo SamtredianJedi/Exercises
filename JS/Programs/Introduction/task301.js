5846d4ad830db9fb83d1b01a2e2727e66c78ea89
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:43:22 +0400

    update1

--- JS/Programs/Introduction/task301.js
+++ JS/Programs/Introduction/task301.js
@@ -12,4 +12,23 @@ Sample array : [0,1,2,3,4,5,6]
 console.log(l.br_search(5)) will return '5'
 
 
+
+
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Binary search using recursion</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
