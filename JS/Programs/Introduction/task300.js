71b59ed838327c52ecd555d81a1fa39155fbbcbf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:34:48 +0400

    update3

--- JS/Programs/Introduction/task300.js
+++ JS/Programs/Introduction/task300.js
@@ -70,4 +70,54 @@ console.log(is_Odd(4));
 console.log(is_Odd(107));
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Use recursion to determine if a number is or not.</title>
+</head>
+<body>
+</body>
+
+
+js:
+
+
+
+
+
+function is_even_recursion(number)
+{
+  if (number < 0) 
+  {
+    number = Math.abs(number);
+  }
+  if (number===0) 
+  {
+    return true;
+  }
+  if (number===1) 
+  {
+    return false;
+  }
+  else 
+  {
+    number = number - 2;
+    return is_even_recursion(number);
+  }
+}
+console.log(is_even_recursion(234)); //true
+console.log(is_even_recursion(-45)); // false
+console.log(is_even_recursion(-45)); // false
+
+
  */
