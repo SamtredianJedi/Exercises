2138f2decee4df9df059bb479b27db7ad4cc5065
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:32:39 +0400

    task

---
+++ JS/Programs/Introduction/task300.js
@@ -0,0 +1,11 @@
+// JavaScript: Check whether a number is even or not
+
+/**
+ * 
+ * 
+ * 
+ JavaScript Function: Exercise-7 with Solution
+Write a JavaScript program to check whether a number is even or not.
+
+
+ */
