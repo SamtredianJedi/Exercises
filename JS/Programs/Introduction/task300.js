65998e7b5f7160e5757774661dcb4efd7fcd6184
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:33:16 +0400

    update1

--- JS/Programs/Introduction/task300.js
+++ JS/Programs/Introduction/task300.js
@@ -8,4 +8,38 @@
 Write a JavaScript program to check whether a number is even or not.
 
 
+
+
+Sample Solution-1:
+
+Using recursion:
+
+JavaScript Code:
+
+function is_even_recursion(number)
+{
+  if (number < 0) 
+  {
+    number = Math.abs(number);
+  }
+  if (number===0) 
+  {
+    return true;
+  }
+  if (number===1) 
+  {
+    return false;
+  }
+  else 
+  {
+    number = number - 2;
+    return is_even_recursion(number);
+  }
+}
+console.log(is_even_recursion(234)); //true
+console.log(is_even_recursion(-45)); // false
+console.log(is_even_recursion(65)); // false
+
+
+
  */
