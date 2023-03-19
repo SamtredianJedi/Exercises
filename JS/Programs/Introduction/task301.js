9a32894d87f9dd2e249e401902631784f5cc7014
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:42:54 +0400

    task

---
+++ JS/Programs/Introduction/task301.js
@@ -0,0 +1,15 @@
+// JavaScript: Binary search using recursion
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-8 with Solution
+Write a JavaScript program for binary search.
+
+Sample array : [0,1,2,3,4,5,6]
+console.log(l.br_search(5)) will return '5'
+
+
+ */
