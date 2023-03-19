5923252305e5ef54a314d83db412a345b9c498e0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:44:38 +0400

    update3

--- JS/Programs/Introduction/task301.js
+++ JS/Programs/Introduction/task301.js
@@ -58,4 +58,51 @@ l= [0,1,2,3,4,5,6];
 console.log(l.br_search(5));
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Binary search using recursion</title>
+</head>
+<body>
+
+</body>
+
+
+
+
+Array.prototype.br_search = function (target) 
+{
+  var half = parseInt(this.length / 2);
+  if (target === this[half]) 
+  {
+    return half;
+  }
+  if (target > this[half]) 
+  {
+    return half + this.slice(half,this.length).br_search(target);
+  } 
+  else
+  {
+    return this.slice(0, half).br_search(target);
+  }
+};
+
+l= [0,1,2,3,4,5,6];
+
+console.log(l.br_search(5));
+
+
+
+
+
+
  */
