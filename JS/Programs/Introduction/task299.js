17f466e43c2764ba37cdeafbdfd88d518b9414df
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:31:09 +0400

    update3

--- JS/Programs/Introduction/task299.js
+++ JS/Programs/Introduction/task299.js
@@ -53,5 +53,50 @@ var fibonacci_series = function (n)
 
  console.log(fibonacci_series(8));
 
+
+
+
+ Live Demo:
+
+
+ html:
+
+
+
+
+ <head>
+  <meta charset="utf-8">
+  <title>Fibonacci Series</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+
+
+var fibonacci_series = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+ console.log(fibonacci_series(8));
+ 
+
+
  
  */
