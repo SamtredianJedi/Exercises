e2d4034b3896f745323a4f6707d7cc503d2c26c0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-19 23:33:52 +0400

    update2

--- JS/Programs/Introduction/task300.js
+++ JS/Programs/Introduction/task300.js
@@ -42,4 +42,32 @@ console.log(is_even_recursion(65)); // false
 
 
 
+
+
+Sample Solution-2:
+
+Without recursion:
+
+Test even number:
+
+Checks whether a number is odd or even using the modulo (%) operator.
+Returns true if the number is even, false if the number is odd.
+Test odd number:
+
+Check whether a number is odd or even using the modulo (%) operator.
+Return true if the number is odd, false if the number is even.
+JavaScript Code:
+
+const is_Even = num => num % 2 === 0;
+const is_Odd = num => num % 2 === 1;
+console.log("Test even number!")
+console.log(is_Even(1));
+console.log(is_Even(4));
+console.log(is_Even(100));
+console.log("Test odd number!")
+console.log(is_Odd(1));
+console.log(is_Odd(4));
+console.log(is_Odd(107));
+
+
  */
