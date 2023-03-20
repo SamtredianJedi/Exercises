36476e3fba66cd9a488a125ad89479febe8c24e2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:27:27 +0400

    update2

--- JS/Programs/Introduction/task303.js
+++ JS/Programs/Introduction/task303.js
@@ -28,4 +28,32 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+const test = (text) => {
+  if (typeof text !== 'string') {
+        return 'String should not be empty!'
+  }
+
+  if (text.length <= 1) {
+    return true
+  }
+
+  if (text[0] !== text[text.length - 1])
+  {
+    return false
+  } 
+  else 
+  {
+    return test(text.slice(1, text.length - 1))
+  }
+}
+console.log(test("madam"))
+console.log(test("abdb"))
+console.log(test("ab"))
+console.log(test("a"))
+
+
  */
