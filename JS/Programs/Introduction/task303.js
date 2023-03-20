b47f086881895f25091fb8255827ae03504096e2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:25:54 +0400

    task

---
+++ JS/Programs/Introduction/task303.js
@@ -0,0 +1,14 @@
+// JavaScript: Check a string for palindromes using recursion
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Function: Exercise-10 with Solution
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as the words madam or racecar, the date/time stamps 11/11/11 11:11 and 02/02/2020, and the sentence: "A man, a plan, a canal – Panama".
+Write a JavaScript program to check whether a given string is Palindrome or not using recursion.
+
+
+ */
