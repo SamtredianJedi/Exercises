f732eed871d30bcf2df89d5beae1f840be591a78
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:33:27 +0400

    update1

--- JS/Programs/Introduction/task304.js
+++ JS/Programs/Introduction/task304.js
@@ -10,4 +10,22 @@ Write a JavaScript program to convert Binary number (positive) to Decimal using 
 
 
 
+
+
+Sample Solution:
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Convert Binary to Decimal using recursion</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
