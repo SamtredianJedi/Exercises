67c8f07a5f54309b87b65d6f14ab54359c84dd69
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:28:22 +0400

    update3

--- JS/Programs/Introduction/task303.js
+++ JS/Programs/Introduction/task303.js
@@ -56,4 +56,49 @@ console.log(test("ab"))
 console.log(test("a"))
 
 
+
+
+Live Demo:
+
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Check a string for palindromes using recursion</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+const test = (text) => {
+  if (typeof text !== 'string') {
+        return 'String should not be empty!'
+  }
+
+  if (text.length <= 1) {
+    return true
+  }
+
+  if (text[0] !== text[text.length - 1])
+  {
+    return false
+  } 
+  else 
+  {
+    return test(text.slice(1, text.length - 1))
+  }
+}
+console.log(test("madam"))
+console.log(test("abdb"))
+console.log(test("ab"))
+console.log(test("a"))
+
+
  */
