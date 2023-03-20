81fb41cfecbd570744792c2a261254210a1eb7df
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:13:16 +0400

    task

---
+++ JS/Programs/Introduction/task302.js
@@ -0,0 +1,16 @@
+// JavaScript: Marge sort - recursion
+
+
+/**
+ * 
+ * 
+
+
+JavaScript Function: Exercise-9 with Solution
+Write a merge sort program in JavaScript.
+
+Sample array : [34,7,23,32,5,62]
+Sample output : [5, 7, 23, 32, 34, 62]
+
+
+ */
