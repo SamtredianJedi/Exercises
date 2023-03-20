27465f006d82225a702915c82c544187ecc63560
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:34:25 +0400

    update2

--- JS/Programs/Introduction/task304.js
+++ JS/Programs/Introduction/task304.js
@@ -28,4 +28,22 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+const test = (n) => {
+  if (n === 0 || n === 1) 
+  {
+    return String(n)
+  }
+  return test(Math.floor(n / 2)) + String(n % 2)
+}
+console.log(test(1))
+console.log(test(0))
+console.log(test(10))
+console.log(test(101))
+
+
  */
