019d637f228829a6b3bfde6cf1f357fd088c5830
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-20 23:35:23 +0400

    update3

--- JS/Programs/Introduction/task304.js
+++ JS/Programs/Introduction/task304.js
@@ -46,4 +46,43 @@ console.log(test(10))
 console.log(test(101))
 
 
+
+
+Live Demo:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Convert Binary to Decimal using recursion</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+
+
+
+const test = (n) => {
+  if (n === 0 || n === 1) 
+  {
+    return String(n)
+  }
+  return test(Math.floor(n / 2)) + String(n % 2)
+}
+console.log(test(1))
+console.log(test(0))
+console.log(test(10))
+console.log(test(101))
+
+
+
  */
