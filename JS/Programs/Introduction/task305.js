fc057559fb5c1e3d5dcac430e0ffc4bd2eb8a0d3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:29:56 +0400

    update3

--- JS/Programs/Introduction/task305.js
+++ JS/Programs/Introduction/task305.js
@@ -68,5 +68,52 @@ console.log(binary_Search(myArray, 16));
 
 
 
+
+// Live Demo:
+
+
+// html:
+
+
+
+
+// <html>
+// <head>
+//   <meta charset="utf-8">
+//   <title>Binary Search Algorithm using recursion</title>
+// </head>
+// <body>
+
+// </body>
+// </html>
+
+
+
+// js:
+
+
+
+
+// const binary_Search = (arr, searchValue, low = 0, high = arr.length - 1) => {
+//   // base case
+//   if (high < low || arr.length === 0) return -1
+//   const mid = low + Math.floor((high - low) / 2)
+//   // If the element is present at the middle
+//   if (arr[mid] === searchValue) {
+//     return mid
+//   }
+//   // If element is smaller than mid, then
+//   // it can only be present in left subarray
+//   if (arr[mid] > searchValue) {
+//     return binary_Search(arr, searchValue, low, mid - 1)
+//   }
+//   // Else the element can only be present in right subarray
+//   return binary_Search(arr, searchValue, mid + 1, high)
+// }
+//  const myArray = [1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23];
+//  console.log(binary_Search(myArray, 6));
+//  console.log(binary_Search(myArray, 16));
+// ​
+
 *
 /
