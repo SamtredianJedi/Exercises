7cfa1d0fa00338aa9df524e9c15387841c781fb2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:31:37 +0400

    task

---
+++ JS/Programs/Introduction/task306.js
@@ -0,0 +1,29 @@
+// JavaScript: Letter combinations of a number
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Function: Exercise-13 with Solution
+A string consists of digits ranging from 2 to 9, inclusive.
+Write a JavaScript program to get all possible letter combinations that can represent the number using recursion.
+
+Letter Combination:
+1: 'abc',
+2: 'def',
+3: 'ghi',
+4: 'jkl',
+5: 'mno',
+6: 'pqr',
+7: 'stu',
+8: 'vwx'
+9: 'yz'
+
+Test Data:
+("12") -> ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+("9") -> ["y", "z"]
+
+
+
+ */
