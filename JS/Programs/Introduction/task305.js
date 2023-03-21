845921870f8c26bd170838a60c57dc2250a2ddf0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:26:49 +0400

    task

---
+++ JS/Programs/Introduction/task305.js
@@ -0,0 +1,15 @@
+// JavaScript: Binary Search Algorithm using recursion
+
+/**
+ * 
+ * 
+ * 
+ JavaScript Function: Exercise-12 with Solution
+Write a JavaScript program to search a given integer in an array of sorted integers using Binary Search Algorithm and recursion.
+
+Test Data:
+([1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23], 6) -> 4
+([1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23], 16) -> -1
+
+
+ */
