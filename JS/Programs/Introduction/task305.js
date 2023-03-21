69a47d6845c2088261a216514684b21f696ead69
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:28:54 +0400

    update2

--- JS/Programs/Introduction/task305.js
+++ JS/Programs/Introduction/task305.js
@@ -31,4 +31,42 @@ HTML Code:
 </html>
 
 
- */
+
+
+JavaScript Code:
+
+/**
+ * Source: bit.ly/3h77zX6
+ * @function BinarySearch
+ * @description Search the integer inside the sorted integers array using Binary Search Algorithm.
+ * @param {Integer[]} arr - sorted array of integers
+ * @param {Integer} low - The input integer
+ * @param {Integer} high - The input integer
+ * @param {Integer} searchValue - The input integer
+ * @return {Integer} - return index of searchValue if found else return -1.
+  */
+const binary_Search = (arr, searchValue, low = 0, high = arr.length - 1) => {
+    // base case
+    if (high < low || arr.length === 0) return -1
+    const mid = low + Math.floor((high - low) / 2)
+        // If the element is present at the middle
+    if (arr[mid] === searchValue) {
+        return mid
+    }
+    // If element is smaller than mid, then
+    // it can only be present in left subarray
+    if (arr[mid] > searchValue) {
+        return binary_Search(arr, searchValue, low, mid - 1)
+    }
+    // Else the element can only be present in right subarray
+    return binary_Search(arr, searchValue, mid + 1, high)
+}
+const myArray = [1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23];
+console.log(binary_Search(myArray, 6));
+console.log(binary_Search(myArray, 16));
+
+
+
+
+*
+/
