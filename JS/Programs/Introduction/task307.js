df3c0cad6da1be7143c73f1a6d8c80fbe913e36c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:35:03 +0400

    task

---
+++ JS/Programs/Introduction/task307.js
@@ -0,0 +1,13 @@
+// JavaScript conditional statements and loops - Exercises, Practice, Solution
+
+
+/**
+ * 
+ * JavaScript: Accept two integers and display the larger
+Last update on January 20 2023 13:15:21 (UTC/GMT +8 hours)
+JavaScript Conditional Statement and loops: Exercise-1 with Solution
+Write a JavaScript program that accept two integers and display the larger.
+
+
+
+ */
