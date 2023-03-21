259e1569e7caa59fc54eb145ac018db4f5c96f69
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-21 23:32:38 +0400

    update2

--- JS/Programs/Introduction/task306.js
+++ JS/Programs/Introduction/task306.js
@@ -45,4 +45,46 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+const test = (digits) => {
+  const length = digits?.length
+  const result = []
+  if (!length) {
+    return result
+  }
+  const digit_Map = {
+    1: 'abc',
+    2: 'def',
+    3: 'ghi',
+    4: 'jkl',
+    5: 'mno',
+    6: 'pqr',
+    7: 'stu',
+    8: 'vwx',
+    9: 'yz'
+  }
+
+  const letter_combinations = (index, combination) => {
+    let letter
+    let letterIndex
+    if (index >= length) {
+      result.push(combination)
+      return
+    }
+    const digit = digit_Map[digits[index]]
+    letterIndex = 0
+    while ((letter = digit[letterIndex++])) {
+      letter_combinations(index + 1, combination + letter)
+    }
+  }
+  letter_combinations(0, '')
+  return result
+}
+console.log(test("12"))
+console.log(test("9"))
+
+
  */
