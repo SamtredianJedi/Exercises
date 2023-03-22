afde53d92affb4d9d3ba3c3146e2673bdf889270
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:41:55 +0400

    update3

--- JS/Programs/Introduction/task310.js
+++ JS/Programs/Introduction/task310.js
@@ -65,4 +65,57 @@ else
 }
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+
+<head>
+<meta charset=utf-8 />
+<title>Write a JavaScript conditional statement to find the largest of five numbers</title>
+</head>
+<body>
+  
+</body>
+
+
+
+js:
+
+
+
+
+a=-5;
+b=-2;
+c=-6;
+d= 0;
+f=-1;
+if (a>b && a>c && a>d && a>f)
+{
+    console.log(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    console.log(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    console.log(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    console.log(d);
+}
+else
+{
+    console.log(f);
+}
+
+
  */
