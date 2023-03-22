3ca71191ad6356e23a6514dd4d04a74031505090
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:36:24 +0400

    task

---
+++ JS/Programs/Introduction/task309.js
@@ -0,0 +1,15 @@
+// JavaScript: How to sort three numbers
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-3 with Solution
+Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result.
+
+Sample numbers: 3, -7, 2
+Output : The sign is -
+
+
+ */
