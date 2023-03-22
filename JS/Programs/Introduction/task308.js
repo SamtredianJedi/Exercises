271fa3335e561df1142d7fc391a2212b4a5169aa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:33:34 +0400

    update1

--- JS/Programs/Introduction/task308.js
+++ JS/Programs/Introduction/task308.js
@@ -10,4 +10,22 @@ Sample numbers : 3, -7, 2
 Output : The sign is -
 
 
+
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JavaScript conditional statement to find the sign of product of three numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
