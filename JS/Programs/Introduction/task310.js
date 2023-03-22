605ff84cf724bc6c3a333c79e4bf653b0b85bac8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:41:05 +0400

    update2

--- JS/Programs/Introduction/task310.js
+++ JS/Programs/Introduction/task310.js
@@ -34,4 +34,35 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+a=-5;
+b=-2;
+c=-6;
+d= 0;
+f=-1;
+if (a>b && a>c && a>d && a>f)
+{
+    console.log(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    console.log(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    console.log(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    console.log(d);
+}
+else
+{
+    console.log(f);
+}
+
+
  */
