a6e19bb953095a67b4e68209f29893c633776a8f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:33:00 +0400

    task

---
+++ JS/Programs/Introduction/task308.js
@@ -0,0 +1,13 @@
+// JavaScript: Find the sign of product of three numbers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-2 with Solution
+Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
+Sample numbers : 3, -7, 2
+Output : The sign is -
+
+
+ */
