f3e7ca06a5e6584a9f6087f79b5b751ad78838ff
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:40:00 +0400

    task

---
+++ JS/Programs/Introduction/task310.js
@@ -0,0 +1,19 @@
+// JavaScript: Find the largest of five numbers
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-4 with Solution
+Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
+
+Sample numbers : -5, -2, -6, 0, -1
+Output : 0
+
+
+
+
+ */
