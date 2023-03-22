a9bb25f9b318f36b1b13e4f9ccf9d867841c9c99
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-22 23:34:07 +0400

    update2

--- JS/Programs/Introduction/task308.js
+++ JS/Programs/Introduction/task308.js
@@ -28,4 +28,33 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+var x=3;
+var y=-7;
+var z=2;
+if (x>0 && y>0 && z>0)
+{
+       alert("The sign is +");
+}
+else if (x<0 && y<0 && z<0)
+        {
+          console.log("The sign is -");
+        }
+        else if (x>0 && y<0 && z<0)
+        {
+          console.log("The sign is +");
+        }
+        else if (x<0 && y>0 && z<0)
+        {
+          console.log("The sign is +");
+        }
+        else
+        {
+          console.log("The sign is -");
+        }
+
+        
  */
