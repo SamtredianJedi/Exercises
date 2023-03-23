11b3ca1ec1e167c65561c0bfc9bad83ee6482f4d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:27:40 +0400

    task

---
+++ JS/Programs/Introduction/task312.js
@@ -0,0 +1,27 @@
+// JavaScript: Compute the average and grade of the students
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-6 with Solution
+Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade.
+
+Student Name	Marks
+David	80
+Vinoth	77
+Divya	88
+Ishitha	95
+Thomas	68
+The grades are computed as follows:
+
+Range	Grade
+<60	F
+<70	D
+<80	C
+<90	B
+<100	A
+
+
+
+ */
