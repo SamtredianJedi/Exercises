65770b8f36c8af2a5f2cdcb438906053c939a84f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:29:55 +0400

    update3

--- JS/Programs/Introduction/task312.js
+++ JS/Programs/Introduction/task312.js
@@ -74,4 +74,54 @@ console.log("Average grade: " + (Avgmarks)/students.length);
 
 
 
+Live Demo:
+
+
+
+html:
+
+
+
+<head>
+<meta charset=utf-8 />
+<title>Compute the average marks and grade</title>
+</head>
+<body>
+  
+</body>
+
+
+js:
+
+
+
+
+var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+
+for (var i=0; i < students.length; i++) {
+        Avgmarks += students[i][1];
+        var avg = (Avgmarks/students.length);
+}
+
+console.log("Average grade: " + (Avgmarks)/students.length);
+
+        if (avg < 60){
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A"); 
+}
+
+
+
  */
