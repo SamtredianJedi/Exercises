484344c7c9ff2312ab5f2cda9335a769de75f590
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:29:04 +0400

    update2

--- JS/Programs/Introduction/task312.js
+++ JS/Programs/Introduction/task312.js
@@ -42,4 +42,36 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+
+for (var i=0; i < students.length; i++) {
+        Avgmarks += students[i][1];
+        var avg = (Avgmarks/students.length);
+}
+
+console.log("Average grade: " + (Avgmarks)/students.length);
+
+        if (avg < 60){
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A"); 
+}
+
+
+
+
  */
