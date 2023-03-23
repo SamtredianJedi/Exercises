d18c800c88e7442b1693c9d87bdfd8183383d70b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:31:29 +0400

    task

---
+++ JS/Programs/Introduction/task313.js
@@ -0,0 +1,12 @@
+// JavaScript: Iterates the integers from 1 to 100
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Conditional Statement and loops: Exercise-7 with Solution
+Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+
+ */
