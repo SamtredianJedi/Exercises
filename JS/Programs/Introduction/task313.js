aa7db57362fc8a4b0b4489968a75a9a1d72fcf98
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:32:08 +0400

    update1

--- JS/Programs/Introduction/task313.js
+++ JS/Programs/Introduction/task313.js
@@ -9,4 +9,21 @@
 Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JavaScript FizzBuzz program</title>
+</head>
+<body>
+  
+</body>
+</html>
+
+
  */
