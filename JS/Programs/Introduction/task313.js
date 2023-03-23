2678acc14e33b8ed32e025d4ff279ee88452e671
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:36:27 +0400

    update3

--- JS/Programs/Introduction/task313.js
+++ JS/Programs/Introduction/task313.js
@@ -56,4 +56,49 @@ for ( var i = 1; i <= 100; i++ )
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+<meta charset=utf-8 />
+<title>JavaScript FizzBuzz program</title>
+</head>
+<body>
+  
+</body>
+
+
+
+js:
+
+
+
+
+
+for ( var i = 1; i <= 100; i++ )
+{
+  if ( i%3 === 0 && i%5 === 0 )
+  {
+    console.log( i + " FizzBuzz" );
+  }
+  else if ( i%3 === 0 ) 
+  {
+    console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+  {
+    console.log(i+ " Buzz" );
+  }
+  else
+  {
+    console.log(i);
+  }
+}
+
+
  */
