10eef0e2b596bb9b14a92a361790dee57e3baff3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:23:28 +0400

    task

---
+++ JS/Programs/Introduction/task311.js
@@ -0,0 +1,17 @@
+// JavaScript: For loop that will iterate from 0 to 15 to find even and odd numbers
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Conditional Statement and loops: Exercise-5 with Solution
+Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+Sample Output:
+"0 is even"
+"1 is odd"
+"2 is even"
+
+
+ */
