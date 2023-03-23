5556124594676307adb7d828986072a1076209fd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-23 23:25:08 +0400

    update2

--- JS/Programs/Introduction/task311.js
+++ JS/Programs/Introduction/task311.js
@@ -32,4 +32,21 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+for (var x=0; x<=15; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+}
+
+
+
  */
