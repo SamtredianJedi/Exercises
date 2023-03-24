27e3aeea5f062b1a94be03754c1a26f87df875f6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-24 23:50:35 +0400

    update3

--- JS/Programs/Introduction/task316.js
+++ JS/Programs/Introduction/task316.js
@@ -50,4 +50,40 @@ for(x=1; x <=6; x++)
 }
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+<meta charset=utf-8 />
+<title>JS nested for loop example</title>
+</head>
+<body>
+  
+</body>
+
+
+
+js:
+
+
+
+
+var x,y,chr;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+
+
  */
