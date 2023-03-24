6d875528d161e10fe2dc8a911de1efc8fb0b0e13
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-24 23:45:24 +0400

    task

---
+++ JS/Programs/Introduction/task315.js
@@ -0,0 +1,12 @@
+// JavaScript: Find the armstrong numbers of 3 digits
+
+/**
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-9 with Solution
+Write a JavaScript program to find the armstrong numbers of 3 digits.
+
+Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
+
+ */
