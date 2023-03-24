1ee11d263532e69f03527bdec53e1420ee807474
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-24 23:42:24 +0400

    update2

--- JS/Programs/Introduction/task314.js
+++ JS/Programs/Introduction/task314.js
@@ -29,4 +29,40 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function happy_number(num) 
+{
+    var m, n ;
+    var c = [] ;
+ 
+    while(num != 1 && c[num] !== true) 
+    {
+        c[num] = true ;
+        m = 0 ;
+        while (num > 0) {
+            n = num % 10 ;
+            m += n * n ;
+            num = (num  - n) / 10 ;
+        }
+        num = m ;
+    }
+    return (num == 1) ;
+}
+ 
+var cnt = 5;
+var num = 1;
+var f5 = ''; 
+while(cnt-- > 0) 
+{
+    while(!happy_number(num))
+        num++ ;
+f5 = f5+(num + ", ") ;
+
+    num++ ;
+}
+console.log('First 5 happy numbers are : '+f5);
+
+
  */
