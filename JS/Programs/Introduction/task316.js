4a872b9bfbc67cc5445d57ecae9aee6b260c3092
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-24 23:49:46 +0400

    update2

--- JS/Programs/Introduction/task316.js
+++ JS/Programs/Introduction/task316.js
@@ -34,4 +34,20 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+var x,y,chr;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+
+
  */
