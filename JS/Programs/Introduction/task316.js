7c50d03ce0395957e0ad55bfd02dc03f0916565e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-24 23:48:50 +0400

    task

---
+++ JS/Programs/Introduction/task316.js
@@ -0,0 +1,18 @@
+// JavaScript: Construct a pattern, using a nested for loop
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-10 with Solution
+Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+*  
+* *  
+* * *  
+* * * *  
+* * * * *
+
+
+
+ */
