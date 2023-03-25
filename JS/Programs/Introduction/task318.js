a28c122e53ced0cd2b06a6530c18c38a9fa8d55b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:42:00 +0400

    task

---
+++ JS/Programs/Introduction/task318.js
@@ -0,0 +1,13 @@
+// JavaScript: Sum the multiples of 3 and 5 under 1000
+
+/**
+ * 
+ * 
+ * 
+ JavaScript Conditional Statement and loops: Exercise-12 with Solution
+Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+
+
+
+
+ */
