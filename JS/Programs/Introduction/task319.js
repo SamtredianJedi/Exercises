8268f79fe2e78f71304cc6e6be94d4e2d9b6a0ac
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:49:17 +0400

    update1

--- JS/Programs/Introduction/task319.js
+++ JS/Programs/Introduction/task319.js
@@ -9,4 +9,23 @@
 Write a JavaScript function to convert a number from one base to another.
 
 
+
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Base convert</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
