bb8acb9d2a8affebcd811326b2cb0a466688f401
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:49:47 +0400

    update2

--- JS/Programs/Introduction/task319.js
+++ JS/Programs/Introduction/task319.js
@@ -28,4 +28,21 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+var base_convert = function(number, initial_base, change_base) {
+   if ((initial_base && change_base) <2 || (initial_base && change_base)>36)
+    return 'Base between 2 and 36';
+   
+    return parseInt(number + '', initial_base)
+    .toString(change_base);
+}
+
+console.log(base_convert('E164',16,8));
+console.log(base_convert(1000,2,8));
+
+
+
  */
