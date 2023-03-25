51c78a34ba01f51546f8ff1ee10f95548491fa23
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:38:37 +0400

    update1

--- JS/Programs/Introduction/task317.js
+++ JS/Programs/Introduction/task317.js
@@ -9,4 +9,23 @@
 Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers.
 
 
+
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>compute the greatest common divisor (GCD) of two positive integers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
