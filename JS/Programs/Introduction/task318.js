6f3f4b38b3a0324c84acd9b6eff1e81e17f00a28
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:42:53 +0400

    update1

--- JS/Programs/Introduction/task318.js
+++ JS/Programs/Introduction/task318.js
@@ -10,4 +10,21 @@ Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
 
 
 
+
+Sample Solution
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Sum the multiples of 3 and 5 under 1000</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
  */
