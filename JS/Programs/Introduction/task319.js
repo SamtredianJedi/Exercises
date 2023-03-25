18293a456a602a41024a45bbd69bffbc0d95eb83
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:51:04 +0400

    JavaScript: Convert a number from one base to another

--- JS/Programs/Introduction/task319.js
+++ JS/Programs/Introduction/task319.js
@@ -1,5 +1,7 @@
 // JavaScript: Convert a number from one base to another
 
+
+
 /**
  * 
  * 
@@ -43,6 +45,37 @@ var base_convert = function(number, initial_base, change_base) {
 console.log(base_convert('E164',16,8));
 console.log(base_convert(1000,2,8));
 
+
+
+
+Live Demo:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Base convert</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+var base_convert = function(number, initial_base, change_base) {
+   if ((initial_base && change_base) <2 || (initial_base && change_base)>36)
+    return 'Base between 2 and 36';
+   
+    return parseInt(number + '', initial_base)
+    .toString(change_base);
+}
+​
+console.log(base_convert('E164',16,8));
+console.log(base_convert(1000,2,8));
 
 
  */
