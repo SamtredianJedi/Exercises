a2eec8d2b57aeade20dfdb321984f3a5b2996d03
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:39:11 +0400

    update2

--- JS/Programs/Introduction/task317.js
+++ JS/Programs/Introduction/task317.js
@@ -28,4 +28,27 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+var a = 2154; //First number
+var b = 458;  //Second number 
+var gcd;
+while (a!=b)
+{
+	if (a>b)
+	{
+		a = a -b;
+	}
+	else
+	{
+		b = b - a;
+	}
+}
+gcd = a;
+console.log(gcd);
+
+
+
  */
