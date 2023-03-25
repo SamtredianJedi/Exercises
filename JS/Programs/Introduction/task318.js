114b4ec00d6c3dbe1d53fbbae014ce4850c3ccc5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:43:25 +0400

    update2

--- JS/Programs/Introduction/task318.js
+++ JS/Programs/Introduction/task318.js
@@ -27,4 +27,20 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
+
+
+
  */
