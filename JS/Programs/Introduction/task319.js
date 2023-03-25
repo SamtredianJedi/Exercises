f787ba8b2365abbd4397de9efcd92b16b4b74ea3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:47:33 +0400

    task

---
+++ JS/Programs/Introduction/task319.js
@@ -0,0 +1,12 @@
+// JavaScript: Convert a number from one base to another
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-1 with Solution
+Write a JavaScript function to convert a number from one base to another.
+
+
+ */
