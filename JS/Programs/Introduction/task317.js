5958a6cc0df028c5558e70365832c1065a7da0e8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:37:37 +0400

    task

---
+++ JS/Programs/Introduction/task317.js
@@ -0,0 +1,12 @@
+// JavaScript: Compute the greatest common divisor (GCD) of two positive integers
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Conditional Statement and loops: Exercise-11 with Solution
+Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers.
+
+
+ */
