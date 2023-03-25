7f49989801e905cca43ba4f7dc6cbff6de654c74
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-25 23:40:07 +0400

    update3

--- JS/Programs/Introduction/task317.js
+++ JS/Programs/Introduction/task317.js
@@ -51,4 +51,46 @@ console.log(gcd);
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>compute the greatest common divisor (GCD) of two positive integers</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+
+
+var a = 2154; //First number
+var b = 458;  //Second number 
+var gcd;
+while (a!=b)
+{
+	if (a>b)
+	{
+		a = a -b;
+	}
+	else
+	{
+		b = b - a;
+	}
+}
+gcd = a;
+console.log(gcd);
+
+
  */
