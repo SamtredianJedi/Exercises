db59cb272097513b841725d2327b71fd595406ea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:33:42 +0400

    update3

--- JS/Programs/Introduction/task321.js
+++ JS/Programs/Introduction/task321.js
@@ -66,4 +66,55 @@ console.log(dec_to_bho(120,'O'));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Convert a decimal number to binary, hexadecimal or octal number</title>
+</head>
+<body>
+
+</body>
+
+
+
+
+js:
+
+
+
+
+dec_to_bho  = function(n, base) {
+ 
+    if (n < 0) {
+      n = 0xFFFFFFFF + n + 1;
+     } 
+switch (base)  
+{  
+case 'B':  
+return parseInt(n, 10).toString(2);
+break;  
+case 'H':  
+return parseInt(n, 10).toString(16);
+break;  
+case 'O':  
+return parseInt(n, 10).toString(8);
+break;  
+default:  
+return("Wrong input.........");  
+}  
+}
+​
+console.log(dec_to_bho(120,'B'));
+console.log(dec_to_bho(120,'H'));
+console.log(dec_to_bho(120,'O'));
+
+
+
  */
