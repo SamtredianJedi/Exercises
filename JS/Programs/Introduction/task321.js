8dc19b8177e302945515102d17e7190d586ee6c7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:32:46 +0400

    update2

--- JS/Programs/Introduction/task321.js
+++ JS/Programs/Introduction/task321.js
@@ -35,4 +35,35 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+dec_to_bho  = function(n, base) {
+ 
+    if (n < 0) {
+      n = 0xFFFFFFFF + n + 1;
+     } 
+switch (base)  
+{  
+case 'B':  
+return parseInt(n, 10).toString(2);
+break;  
+case 'H':  
+return parseInt(n, 10).toString(16);
+break;  
+case 'O':  
+return parseInt(n, 10).toString(8);
+break;  
+default:  
+return("Wrong input.........");  
+}  
+}
+
+console.log(dec_to_bho(120,'B'));
+console.log(dec_to_bho(120,'H'));
+console.log(dec_to_bho(120,'O'));
+
+
+
  */
