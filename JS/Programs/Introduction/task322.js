44eb06bea446f7fe66b5701480bbe6433ba542b2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:34:50 +0400

    task

---
+++ JS/Programs/Introduction/task322.js
@@ -0,0 +1,24 @@
+// JavaScript: Generate a random integer
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-4 with Solution
+Write a JavaScript function to generate a random integer.
+
+Test Data :
+console.log(rand(20,1));
+console.log(rand(1,10));
+console.log(rand(6));
+console.log(rand());
+15
+5
+1
+0
+
+
+
+ */
