3e020a77be7bdd8cc898ab31bc30a2322305ef30
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:31:21 +0400

    task

---
+++ JS/Programs/Introduction/task321.js
@@ -0,0 +1,20 @@
+// JavaScript: Convert a decimal number to binary, hexadecimal or octal number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-3 with Solution
+Write a JavaScript function to convert a decimal number to binary, hexadecimal or octal number.
+
+Test Data:
+console.log(dec_to_bho(120,'B'));
+console.log(dec_to_bho(120,'H'));
+console.log(dec_to_bho(120,'O'));
+"1111000"
+"78"
+"170"
+
+
+
+ */
