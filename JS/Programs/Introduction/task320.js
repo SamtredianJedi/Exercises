0b65084b5037fb3470fc1de6d899a502df58ea2b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:26:21 +0400

    update2

--- JS/Programs/Introduction/task320.js
+++ JS/Programs/Introduction/task320.js
@@ -29,4 +29,19 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function bin_to_dec(bstr) { 
+    return parseInt((bstr + '')
+    .replace(/[^01]/gi, ''), 2);
+}
+console.log(bin_to_dec('110011'));
+console.log(bin_to_dec('100'));
+
+
+
  */
