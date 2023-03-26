c8f6d528d180c138f0669eda829a6468edf4b16c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:36:55 +0400

    update2

--- JS/Programs/Introduction/task322.js
+++ JS/Programs/Introduction/task322.js
@@ -38,4 +38,25 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+rand = function(min, max) {
+  if (min==null && max==null)
+    return 0;    
+  
+  if (max == null) {
+      max = min;
+      min = 0;
+    }
+    return min + Math.floor(Math.random() * (max - min + 1));
+  };
+ console.log(rand(20,1));
+ console.log(rand(1,10));
+ console.log(rand(6));
+ console.log(rand());
+
+
+ 
  */
