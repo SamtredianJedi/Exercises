c2822d3a2963fefc16bc65f6d0fbb6187e24531e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:38:20 +0400

    update3

--- JS/Programs/Introduction/task322.js
+++ JS/Programs/Introduction/task322.js
@@ -58,5 +58,45 @@ rand = function(min, max) {
  console.log(rand());
 
 
+
+
+
+ Live Demo:
+
+
+ html:
+
+
+
+ <head>
+  <meta charset="utf-8">
+  <title>Javascript random function</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+rand = function(min, max) {
+  if (min==null && max==null)
+    return 0;    
+  
+  if (max == null) {
+      max = min;
+      min = 0;
+    }
+    return min + Math.floor(Math.random() * (max - min + 1));
+  };
+ console.log(rand(20,1));
+ console.log(rand(1,10));
+ console.log(rand(6));
+ console.log(rand());
+
+
  
  */
