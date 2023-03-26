53e71d688fa06f834297fd3fe1e250a7c3d542d9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-26 23:22:12 +0400

    update1

--- JS/Programs/Introduction/task320.js
+++ JS/Programs/Introduction/task320.js
@@ -10,4 +10,23 @@ Write a JavaScript function to convert a binary number to a decimal number.
 Test Data: console.log(bin_to_dec('110011')); console.log(bin_to_dec('100')); 51 4
 
 
+
+
+Sample Solution:-
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Binary number to a decimal number</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
