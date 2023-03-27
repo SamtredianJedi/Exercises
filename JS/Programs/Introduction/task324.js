0f9d59c4a2f233496c2e2aa92c8c8f0f50df330f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:31:52 +0400

    update2

--- JS/Programs/Introduction/task324.js
+++ JS/Programs/Introduction/task324.js
@@ -30,6 +30,20 @@ HTML Code:
 </html>
 
 
+JavaScript Code:
+
+function max(input) {
+     if (toString.call(input) !== "[object Array]")  
+       return false;
+  return Math.max.apply(null, input);
+	}
+
+console.log(max([12,34,56,1]));
+console.log(max([-12,-34,0,-56,-1]));
+
+
+
+
 
 
  */
