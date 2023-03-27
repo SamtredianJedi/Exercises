1e612cdd6695fef8839d7b4712db95d2b15f26fc
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:19:21 +0400

    task

---
+++ JS/Programs/Introduction/task323.js
@@ -0,0 +1,20 @@
+// JavaScript: Format a number up to specified decimal places
+
+/**
+ * 
+ * 
+
+JavaScript Math: Exercise-5 with Solution
+Write a JavaScript function to format a number up to specified decimal places.
+
+Test Data :
+console.log(decimals(2.100212, 2));
+console.log(decimals(2.100212, 3));
+console.log(decimals(2100, 2));
+"2.10"
+"2.100"
+"2100.00"
+
+
+
+ */
