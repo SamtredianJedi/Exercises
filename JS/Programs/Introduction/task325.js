b1e103c4894f85778e8c3137a26fb1d5d9d9bb8a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:35:48 +0400

    task

---
+++ JS/Programs/Introduction/task325.js
@@ -0,0 +1,17 @@
+// JavaScript: Find the lowest value in an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-7 with Solution
+Write a JavaScript function to find the lowest value in an array.
+
+Test Data :
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
+1
+-56
+
+
+ */
