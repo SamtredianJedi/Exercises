f09dc69d325c4fe943687bbbafc9345d668ec51d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:29:11 +0400

    task

---
+++ JS/Programs/Introduction/task324.js
@@ -0,0 +1,17 @@
+// JavaScript: Find the highest value in an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-6 with Solution
+Write a JavaScript function to find the highest value in an array.
+
+Test Data :
+console.log(max([12,34,56,1]));
+console.log(max([-12,-34,0,-56,-1]));
+56
+0
+
+
+ */
