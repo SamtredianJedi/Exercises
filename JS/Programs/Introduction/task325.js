767ec506825538481ae9641bc59cf8a21266aaaf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:37:29 +0400

    update2

--- JS/Programs/Introduction/task325.js
+++ JS/Programs/Introduction/task325.js
@@ -30,4 +30,19 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function min(input) {
+     if (toString.call(input) !== "[object Array]")  
+       return false;
+  return Math.min.apply(null, input);
+	}
+
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
+
+
+
+
  */
