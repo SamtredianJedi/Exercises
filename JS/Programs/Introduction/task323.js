f89a0bc3b8ce5b8e579516928cc06892c3986290
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-27 23:21:21 +0400

    update2

--- JS/Programs/Introduction/task323.js
+++ JS/Programs/Introduction/task323.js
@@ -35,4 +35,23 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function decimals(n, d) {
+ if ((typeof n !== 'number') || (typeof d !== 'number'))
+   return false;
+    	n = parseFloat(n) || 0;
+	return n.toFixed(d);
+	}
+console.log(decimals(2.100212, 2));
+console.log(decimals(2.100212, 3));
+console.log(decimals(2100, 2));
+
+
+
+
+
+
+
+
  */
