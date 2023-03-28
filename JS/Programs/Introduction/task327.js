2d15e69e9f3abb2bdbdc7ffea1fc0b2891f35552
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:35:00 +0400

    task

---
+++ JS/Programs/Introduction/task327.js
@@ -0,0 +1,18 @@
+// JavaScript: Find the Greatest Common Divisor or GCD of more than 2 integers
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-9 with Solution
+Write a JavaScript function to find the GCD (greatest common divisor) of more than 2 integers.
+
+Test Data:
+console.log(gcd_more_than_two_numbers([3,15,27]));
+console.log(gcd_more_than_two_numbers([5,10,15,25]));
+Output :
+3
+5
+
+
+
+ */
