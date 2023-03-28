f7fec154f2d31b02b1dbc0ac19e17356539ea231
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:40:14 +0400

    update1

--- JS/Programs/Introduction/task328.js
+++ JS/Programs/Introduction/task328.js
@@ -20,4 +20,21 @@ Output :
 
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="utf-8">
+  <title>LCM of two numbers</title>
+  </head>
+  <body>
+</body>
+  </html>
+
+
+  
  */
