d13f06269800ed5e81eaad27e767cfbf766361bf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:30:38 +0400

    update1

--- JS/Programs/Introduction/task326.js
+++ JS/Programs/Introduction/task326.js
@@ -18,4 +18,28 @@ Output :
 
 
 
+Sample Solution-1:
+
+JavaScript Code:
+
+function gcd_two_numbers(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+
+console.log(gcd_two_numbers(12, 13));
+console.log(gcd_two_numbers(9, 3));
+
+
+
+
+
  */
