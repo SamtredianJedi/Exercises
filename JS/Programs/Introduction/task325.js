e68f6c532754b70a0d1b30131a44b6cd8b3c6b39
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:24:49 +0400

    update3

--- JS/Programs/Introduction/task325.js
+++ JS/Programs/Introduction/task325.js
@@ -44,5 +44,36 @@ console.log(min([-12,-34,0,-56,-1]));
 
 
 
+Live Demo:
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+<title>Javascript random function</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+function min(input) {
+     if (toString.call(input) !== "[object Array]")  
+       return false;
+  return Math.min.apply(null, input);
+  }
+​
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
+
+
+
 
  */
