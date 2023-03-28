7ba8ef4476fea07ac3011260814e4e3b289a4c5e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:31:15 +0400

    update2

--- JS/Programs/Introduction/task326.js
+++ JS/Programs/Introduction/task326.js
@@ -42,4 +42,28 @@ console.log(gcd_two_numbers(9, 3));
 
 
 
+Sample Solution-2:
+
+Calculates the greatest common divisor between two or more numbers/arrays.
+
+The inner _gcd function uses recursion.
+Base case is when y equals 0. In this case, return x.
+Otherwise, return the GCD of y and the remainder of the division x/y.
+JavaScript Code:
+
+//Source:https://bit.ly/3hEZdCl
+//gcd
+const gcd = (...arr) => {
+  const _gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  return [...arr].reduce((a, b) => _gcd(a, b));
+};
+console.log(gcd(12, 13));
+console.log(gcd(9, 3));
+console.log(gcd(8, 36));
+console.log(gcd(...[12, 8, 32]));
+
+
+
+
+
  */
