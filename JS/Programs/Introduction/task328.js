df3e77223a3167671a49bed6f2aea898540593f8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-28 23:40:49 +0400

    update2

--- JS/Programs/Introduction/task328.js
+++ JS/Programs/Introduction/task328.js
@@ -36,5 +36,30 @@ HTML Code:
   </html>
 
 
-  
+
+
+
+  JavaScript Code:
+
+function lcm_two_numbers(x, y) {
+   if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+}
+
+function gcd_two_numbers(x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+console.log(lcm_two_numbers(3,15));
+console.log(lcm_two_numbers(10,15));
+
+
+
  */
