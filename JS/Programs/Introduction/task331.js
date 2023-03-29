ac84a29392d55e1d7d31fb09aaaadad90c6bbb84
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:15:26 +0400

    update1

--- JS/Programs/Introduction/task331.js
+++ JS/Programs/Introduction/task331.js
@@ -17,4 +17,25 @@ true
 
 
 
+
+
+
+
+Sample Solution-1:
+
+JavaScript Code:
+
+function power_of_2(n) {
+ if (typeof n !== 'number') 
+      return 'Not a number'; 
+
+    return n && (n & (n - 1)) === 0;
+}
+
+console.log(power_of_2(16));
+console.log(power_of_2(18));
+console.log(power_of_2(256));
+
+
+
  */
