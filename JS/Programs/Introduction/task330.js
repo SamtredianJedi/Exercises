bcb45103fae3497dc0538b4210cc5ba5c57c82ff
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:10:52 +0400

    task

---
+++ JS/Programs/Introduction/task330.js
@@ -0,0 +1,26 @@
+// JavaScript: Find out if a number is a natural number or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-12 with Solution
+Write a JavaScript function to find out if a number is a natural number or not.
+
+Note:
+Natural numbers are whole numbers from 1 upwards : 1, 2, 3, and so on ... or from 0 upwards in some area of mathematics: 0, 1, 2, 3 and so on ...
+No negative numbers and no fractions.
+Test Data:
+console.log(is_Natural(-15));
+console.log(is_Natural(1));
+console.log(is_Natural(10.22));
+console.log(is_Natural(10/0));
+Output:
+false
+true
+false
+false
+
+
+
+ */
