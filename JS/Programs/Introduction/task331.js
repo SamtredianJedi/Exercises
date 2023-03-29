5597a4626d5f200ab3fe4f18091c25c24e759b57
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:14:46 +0400

    task

---
+++ JS/Programs/Introduction/task331.js
@@ -0,0 +1,20 @@
+// JavaScript: Test if a number is a power of 2
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-13 with Solution
+Write a JavaScript function to test if a number is a power of 2.
+
+Test Data:
+console.log(power_of_2(16));
+console.log(power_of_2(18));
+console.log(power_of_2(256));
+Output:
+true
+false
+true
+
+
+
+ */
