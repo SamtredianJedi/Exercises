1062bd0695325b5336086c35c2f948b6804e772a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:12:15 +0400

    update2

--- JS/Programs/Introduction/task330.js
+++ JS/Programs/Introduction/task330.js
@@ -39,7 +39,23 @@ HTML Code:
   
 
 
-  
+
+
+  JavaScript Code:
+
+function is_Natural(n) 
+      {
+	   if (typeof n !== 'number') 
+	        return 'Not a number'; 
+			
+	 return (n >= 0.0) && (Math.floor(n) === n) && n !== Infinity;
+	    }
+console.log(is_Natural(-15));
+console.log(is_Natural(1));console.log(is_Natural(10.22));
+console.log(is_Natural(10/0));
+
+
+
 
 
  */
