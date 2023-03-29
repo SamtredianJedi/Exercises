e5674300dce0e1514d9018ffa55a7c8260eae961
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:13:23 +0400

    update3

--- JS/Programs/Introduction/task330.js
+++ JS/Programs/Introduction/task330.js
@@ -57,5 +57,41 @@ console.log(is_Natural(10/0));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Checking for Natural Number</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+
+function is_Natural(n) 
+      {
+	   if (typeof n !== 'number') 
+	        return 'Not a number'; 
+			
+	 return (n >= 0.0) && (Math.floor(n) === n) && n !== Infinity;
+	    }
+console.log(is_Natural(-15));
+console.log(is_Natural(1));console.log(is_Natural(10.22));
+console.log(is_Natural(10/0));
+
+
+
+
 
  */
