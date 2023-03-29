d2db428fcc3c0b81c2e9acb40013ac479db1ce1d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:06:06 +0400

    task

---
+++ JS/Programs/Introduction/task329.js
@@ -0,0 +1,21 @@
+// JavaScript: Least common multiple (LCM) of more than 2 integers
+
+/**
+ * 
+ * 
+ * 
+
+
+JavaScript Math: Exercise-11 with Solution
+Write a JavaScript function to get the least common multiple (LCM) of more than 2 integers.
+
+Test Data :
+console.log(lcm_more_than_two_numbers([100,90,80,7]));
+console.log(lcm_more_than_two_numbers([5,10,15,25]));
+Output :
+25200
+150
+
+
+
+ */
