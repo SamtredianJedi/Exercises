cb14a54d1db38093076181ce65ca644ac2c5e53e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-29 23:15:58 +0400

    update2

--- JS/Programs/Introduction/task331.js
+++ JS/Programs/Introduction/task331.js
@@ -38,4 +38,15 @@ console.log(power_of_2(256));
 
 
 
+
+Sample Solution-2:
+
+JavaScript Code:
+
+const power_of_2 = n => !!n && (n & (n - 1)) == 0;
+console.log(power_of_2(16));
+console.log(power_of_2(18));
+console.log(power_of_2(256)); 
+
+
  */
