abbe0352a5d47fe77054eaea637e679443564962
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:30:52 +0400

    task

---
+++ JS/Programs/Introduction/task333.js
@@ -0,0 +1,25 @@
+// JavaScript: Check whether a value is an integer or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-15 with Solution
+Write a JavaScript function to check whether a value is an integer or not.
+
+Test Data:
+console.log(is_Int(23));
+console.log(is_Int(4e2));
+console.log(is_Int(NaN));
+console.log(is_Int(23.75));
+console.log(is_Int(-23));
+Output :
+true
+true
+false
+false
+true
+
+
+
+ */
