802fc8d2a7ccc543469d132628d19d21daa3e858
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:32:30 +0400

    update2

--- JS/Programs/Introduction/task333.js
+++ JS/Programs/Introduction/task333.js
@@ -37,6 +37,26 @@ HTML Code:
   </html>
 
 
+
+
+  JavaScript Code:
+
+function is_Int(num) {
   
+   if (typeof num !== 'number')
+       return false; 
+  
+  return !isNaN(num) && 
+         parseInt(Number(num)) == num && 
+         !isNaN(parseInt(num, 10));
+}
+console.log(is_Int(23));
+console.log(is_Int(4e2));
+console.log(is_Int(NaN));
+console.log(is_Int(23.75));
+console.log(is_Int(-23));
+
+
+
 
  */
