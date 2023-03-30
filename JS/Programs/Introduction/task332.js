36931f255fe51814f52572845e40658a70654646
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:28:32 +0400

    update3

--- JS/Programs/Introduction/task332.js
+++ JS/Programs/Introduction/task332.js
@@ -52,4 +52,40 @@ console.log(precise_round(10.49999,2));
 
 
 
+
+Live Demo :
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Round a number to a given decimal places</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+
+function precise_round(n, r) {
+    let int = Math.floor(n).toString()
+    if (typeof n !== 'number' || typeof r !== 'number') return 'Not a Number'
+    if (int[0] == '-' || int[0] == '+') int = int.slice(int[1], int.length)
+    return n.toPrecision(int.length + r)
+}
+console.log(precise_round(12.375,2));
+console.log(precise_round(-10.3079499, 3));
+console.log(precise_round(10.49999,0));
+console.log(precise_round(10.49999,2));
+
+
+
  */
