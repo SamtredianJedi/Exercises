7ab8fba992080cf9798cef8fec4f1e220f28b898
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:35:26 +0400

    task

---
+++ JS/Programs/Introduction/task334.js
@@ -0,0 +1,27 @@
+// JavaScript: Check whether a variable is numeric or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-16 with Solution
+Write a JavaScript function to check whether a variable is numeric or not.
+
+Test Data:
+console.log(is_Numeric(12));
+console.log(is_Numeric('abcd'));
+console.log(is_Numeric('12'));
+console.log(is_Numeric(' '));
+console.log(is_Numeric(1.20));
+console.log(is_Numeric(-200));
+Output:
+true
+false
+true
+false
+true
+true
+
+
+
+ */
