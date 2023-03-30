c3302e4ac38f2f8fbae0bbc25a9d76dd656f5e05
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:25:31 +0400

    task

---
+++ JS/Programs/Introduction/task332.js
@@ -0,0 +1,21 @@
+// JavaScript: Round a number to a given specific decimal places
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-14 with Solution
+Write a JavaScript function to round a number to a given decimal places.
+
+Test Data:
+console.log(precise_round(12.375,2));
+console.log(precise_round(12.37499,2));
+console.log(precise_round(-10.3079499, 3));
+Output :
+"12.38"
+"12.37"
+"-10.308"
+
+
+
+ */
