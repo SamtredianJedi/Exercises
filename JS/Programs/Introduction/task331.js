71ac1c798a687237c22e0df8704438eced2789a8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:22:48 +0400

    update3

--- JS/Programs/Introduction/task331.js
+++ JS/Programs/Introduction/task331.js
@@ -49,4 +49,37 @@ console.log(power_of_2(18));
 console.log(power_of_2(256)); 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+ <head>
+  <meta charset="utf-8">
+  <title>Test if a number is a power of 2</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+function power_of_2(n) {
+ if (typeof n !== 'number') 
+      return 'Not a number'; 
+
+    return n && (n & (n - 1)) === 0;
+}
+
+console.log(power_of_2(16));
+console.log(power_of_2(18));
+console.log(power_of_2(256));
+
+
  */
