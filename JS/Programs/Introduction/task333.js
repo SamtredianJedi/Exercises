c15564c181faf60fdc74171d812b046907f8c5dd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-30 22:33:22 +0400

    update3

--- JS/Programs/Introduction/task333.js
+++ JS/Programs/Introduction/task333.js
@@ -59,4 +59,45 @@ console.log(is_Int(-23));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Check whether a value is an integer or not</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+
+
+
+function is_Int(num) {
+  
+   if (typeof num !== 'number')
+       return false; 
+  
+  return !isNaN(num) && 
+         parseInt(Number(num)) == num && 
+         !isNaN(parseInt(num, 10));
+}
+console.log(is_Int(23));
+console.log(is_Int(4e2));
+console.log(is_Int(NaN));
+console.log(is_Int(23.75));
+console.log(is_Int(-23));
+
+
+
+
  */
