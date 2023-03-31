fec78b622dba870f749cfa40827af64b3fb232fa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:14:43 +0400

    update3

--- JS/Programs/Introduction/task334.js
+++ JS/Programs/Introduction/task334.js
@@ -57,6 +57,41 @@ console.log(is_Numeric(-200));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+ <head>
+  <meta charset="utf-8">
+  <title>Check whether a variable is numeric or not</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+
+function is_Numeric(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+}
+​
+console.log(is_Numeric(12));
+console.log(is_Numeric('abcd'));
+console.log(is_Numeric('12'));
+console.log(is_Numeric(' '));
+console.log(is_Numeric(1.20));
+console.log(is_Numeric(-200));
+
+
+
+
 
 
 
