35b1f75f54c664009afda9330cff7192b8413f99
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:24:57 +0400

    update3

--- JS/Programs/Introduction/task336.js
+++ JS/Programs/Introduction/task336.js
@@ -60,4 +60,44 @@ console.log(product([1,2,'a',3]));
 
 
 
+
+
+Live Demo:
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Product of values in an array</title>
+</head>
+<body></body>
+
+
+js:
+
+
+
+
+function product(input){
+ if (toString.call(input) !== "[object Array]")
+    return false;
+  
+         var total   =   1;
+  
+       for(var i=0;i<input.length;i++){
+               if(isNaN(input[i])){
+                 continue;
+               }
+                total   *=  Number(input[i]);
+            }
+          return total;
+        }
+console.log(product([1,2,3]));
+console.log(product([100,-200,3]));
+console.log(product([1,2,'a',3]));
+
+
+
+
+
  */
