1cc8e1e597f199ecab66f7c82dcfbb685cbbdc64
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:23:34 +0400

    update2

--- JS/Programs/Introduction/task336.js
+++ JS/Programs/Introduction/task336.js
@@ -34,4 +34,30 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function product(input){
+ if (toString.call(input) !== "[object Array]")
+    return false;
+  
+         var total   =   1;
+  
+       for(var i=0;i<input.length;i++){
+               if(isNaN(input[i])){
+                 continue;
+               }
+                total   *=  Number(input[i]);
+            }
+          return total;
+        }
+console.log(product([1,2,3]));
+console.log(product([100,-200,3]));
+console.log(product([1,2,'a',3]));
+
+
+
+
  */
