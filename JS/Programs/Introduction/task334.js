db1a5c29532847348a4ead996727b2bea33c47b9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:12:26 +0400

    update2

--- JS/Programs/Introduction/task334.js
+++ JS/Programs/Introduction/task334.js
@@ -39,4 +39,25 @@ HTML Code:
 
 
   
+
+
+  JavaScript Code:
+
+function is_Numeric(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+console.log(is_Numeric(12));
+console.log(is_Numeric('abcd'));
+console.log(is_Numeric('12'));
+console.log(is_Numeric(' '));
+console.log(is_Numeric(1.20));
+console.log(is_Numeric(-200));
+
+
+
+
+
+
+
  */
