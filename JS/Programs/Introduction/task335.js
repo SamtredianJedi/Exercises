f11fff757d5371f1936f511a0ad637effe7a40df
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:20:05 +0400

    update3

--- JS/Programs/Introduction/task335.js
+++ JS/Programs/Introduction/task335.js
@@ -60,4 +60,51 @@ console.log(sum([1,2,'a',3]));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Sum of values in an array</title>
+  </head>
+  <body>
+</body>
+
+
+
+js:
+
+
+
+
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
+console.log(sum([1,2,3]));
+console.log(sum([100,-200,3]));
+console.log(sum([1,2,'a',3]));
+
+
+
+
+
+
  */
