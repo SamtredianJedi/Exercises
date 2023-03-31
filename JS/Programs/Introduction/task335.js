287f56082324a44196dd311218c1094c055edb5a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:19:05 +0400

    update2

--- JS/Programs/Introduction/task335.js
+++ JS/Programs/Introduction/task335.js
@@ -32,7 +32,31 @@ HTML Code:
 
 
 
-  
+
+
+  JavaScript Code:
+
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
+console.log(sum([1,2,3]));
+console.log(sum([100,-200,3]));
+console.log(sum([1,2,'a',3]));
+
+
+
 
 
 
