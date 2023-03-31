2d3c519bed028c3748417f0cda0b98e5f939bfa6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-03-31 23:16:50 +0400

    task

---
+++ JS/Programs/Introduction/task335.js
@@ -0,0 +1,21 @@
+// JavaScript: Calculate the sum of values in an array
+
+/**
+ * 
+
+
+JavaScript Math: Exercise-17 with Solution
+Write a JavaScript function to calculate the sum of values in an array.
+
+Test Data :
+console.log(sum([1,2,3]));
+console.log(sum([100,-200,3]));
+console.log(sum([1,2,'a',3]));
+Output :
+6
+-97
+6
+
+
+
+ */
