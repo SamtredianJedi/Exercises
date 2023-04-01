1129fede0cbcf06baf8e03fc24bb4144b8b278e0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-01 15:52:02 +0400

    update3

--- JS/Programs/Introduction/task337.js
+++ JS/Programs/Introduction/task337.js
@@ -47,6 +47,39 @@ console.log(pythagorean_theorem(3, 4));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Pythagorean Theorem</title>
+</head>
+<body></body>
+
+
+
+js:
+
+
+
+function pythagorean_theorem(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+return false; 
+    return Math.sqrt(x * x + y * y);
+  }
+​
+console.log(pythagorean_theorem(2, 4));
+console.log(pythagorean_theorem(3, 4));
+​
+
+
+
+
 
 
 
