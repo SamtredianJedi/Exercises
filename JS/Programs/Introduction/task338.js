0fa359b4374ce87332adca42dd6080fd1c97eff2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-01 15:55:40 +0400

    update2

--- JS/Programs/Introduction/task338.js
+++ JS/Programs/Introduction/task338.js
@@ -37,5 +37,21 @@ HTML Code:
 
 
 
-  
+
+  JavaScript Code:
+
+function binomial(n, k) {
+     if ((typeof n !== 'number') || (typeof k !== 'number')) 
+  return false; 
+    var coeff = 1;
+    for (var x = n-k+1; x <= n; x++) coeff *= x;
+    for (x = 1; x <= k; x++) coeff /= x;
+    return coeff;
+}
+
+console.log(binomial(8,3));
+console.log(binomial(10,2));
+
+
+
  */
