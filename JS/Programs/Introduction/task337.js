8a7189b0ea6d567aaa49d9140af5b34d8762415e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-01 15:50:16 +0400

    update2

--- JS/Programs/Introduction/task337.js
+++ JS/Programs/Introduction/task337.js
@@ -33,6 +33,20 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function pythagorean_theorem(x, y) {
+  if ((typeof x !== 'number') || (typeof y !== 'number')) 
+return false; 
+		return Math.sqrt(x * x + y * y);
+	}
+
+console.log(pythagorean_theorem(2, 4));
+console.log(pythagorean_theorem(3, 4));
+
+
+
+
 
 
 
