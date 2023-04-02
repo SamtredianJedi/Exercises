01c507f0fc98cc41c6f23c1c5f628afd0dbe18f0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-02 23:44:59 +0400

    update3

--- JS/Programs/Introduction/task342.js
+++ JS/Programs/Introduction/task342.js
@@ -58,4 +58,44 @@ console.log(result);
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to round a number to a specified number of digitsand strip extra zeros (if any).</title>
+</head>
+<body>
+
+</body>
+
+
+
+
+js:
+
+
+
+//var a = -1.1230000;
+//var a = 0.0000009999999;
+//var a = 1.000000000;
+//var a = -4.55555;
+//var a = 5.0001000;
+var a = 1.1234000;
+
+
+var result = parseFloat(a.toFixed(4));
+console.log(result);
+
+
+
+
+
+
  */
