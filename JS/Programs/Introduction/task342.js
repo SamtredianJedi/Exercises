aaae7674066f8b7e2dc608eb7d835d76a2dd2376
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-02 23:36:02 +0400

    task

---
+++ JS/Programs/Introduction/task342.js
@@ -0,0 +1,19 @@
+// JavaScript: Round a number to a specified number of digits and strip extra zeros
+
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-24 with Solution
+Write a JavaScript function to round a number to a specified number of digits and strip extra zeros (if any).
+
+Test Data :
+var a = -4.55555;
+console.log(result);
+-4.5556
+var a = 5.0001000;
+console.log(result);
+5.0001
+
+
+ */
