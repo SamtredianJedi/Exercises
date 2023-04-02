b0be9f05f1135576c36fe36785b986d4ededd6ed
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-02 23:41:37 +0400

    update2

--- JS/Programs/Introduction/task342.js
+++ JS/Programs/Introduction/task342.js
@@ -41,4 +41,21 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+//var a = -1.1230000;
+//var a = 0.0000009999999;
+//var a = 1.000000000;
+//var a = -4.55555;
+//var a = 5.0001000;
+var a = 1.1234000;
+
+var result = parseFloat(a.toFixed(4));
+console.log(result);
+
+
+
+
+
+
  */
