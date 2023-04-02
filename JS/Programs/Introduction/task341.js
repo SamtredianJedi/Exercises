9acb5837a249adf8d302577a6a5347f194dfb67b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-02 23:33:46 +0400

    update2

--- JS/Programs/Introduction/task341.js
+++ JS/Programs/Introduction/task341.js
@@ -30,4 +30,25 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+console.log(create_UUID());
+
+
+
+
+
  */
