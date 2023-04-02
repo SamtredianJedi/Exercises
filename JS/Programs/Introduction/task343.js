aaa9972043f6295e58e371d35e4a656455332625
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-02 23:48:36 +0400

    task

---
+++ JS/Programs/Introduction/task343.js
@@ -0,0 +1,13 @@
+// JavaScript: Currency math (add, subtract, multiply, division etc.)
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-25 with Solution
+Write a JavaScript function to make currency math (add, subtract, multiply, division etc.).
+
+Test Data :
+n1 = '$40.24', n2 = '$21.57';
+
+
+ */
