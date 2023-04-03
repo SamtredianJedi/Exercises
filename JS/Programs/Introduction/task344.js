9a1d3f4e4162053a36095f8f03a5c00a68479532
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:34:32 +0400

    task

---
+++ JS/Programs/Introduction/task344.js
@@ -0,0 +1,18 @@
+// JavaScript: Calculate the nth root of a number
+
+/**
+ * 
+
+
+JavaScript Math: Exercise-26 with Solution
+Write a JavaScript function to calculate the nth root of a number.
+
+Test Data :
+console.log(nthroot(64, 2));
+8
+console.log(nthroot(64, -2));
+0.125
+
+
+
+ */
