cbf4177d5f42f6a79505bb5dc49af77be38d24e5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:42:37 +0400

    task

---
+++ JS/Programs/Introduction/task346.js
@@ -0,0 +1,18 @@
+// JavaScript: Round up an integer value to the next multiple of 5
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-28 with Solution
+Write a JavaScript function to round up an integer value to the next multiple of 5.
+
+Test Data:
+console.log(int_round5(32));
+35
+console.log(int_round5(137));
+140
+
+
+
+ */
