a57c05413a770674557984c23649531c7a61fc19
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:43:23 +0400

    update1

--- JS/Programs/Introduction/task346.js
+++ JS/Programs/Introduction/task346.js
@@ -15,4 +15,22 @@ console.log(int_round5(137));
 
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to round up an integer value to the next multiple of 5</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
  */
