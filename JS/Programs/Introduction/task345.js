242410960edfea8b91b370812032366701c04d6a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:41:04 +0400

    update3

--- JS/Programs/Introduction/task345.js
+++ JS/Programs/Introduction/task345.js
@@ -48,6 +48,40 @@ console.log(pointDirection(1, 0, 1, 10));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Write a JavaScript function to calculate degrees between 2 points with inverse Y axis</title>
+</head>
+<body>
+​
+</body>
+
+
+
+
+
+js:
+
+
+
+
+function pointDirection(x1, y1, x2, y2) {
+    return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+}
+
+console.log(pointDirection(1, 0, 12, 0));
+console.log(pointDirection(1, 0, 1, 10));
+
+
+
 
 
 
