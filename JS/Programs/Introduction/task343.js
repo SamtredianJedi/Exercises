121f3f38bb800001c0cccb2eaa10685687f949b4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:32:56 +0400

    update3

--- JS/Programs/Introduction/task343.js
+++ JS/Programs/Introduction/task343.js
@@ -51,5 +51,46 @@ console.log(parseFloat(n1.replace(regp, '')) /         parseFloat(n2.replace(reg
 
 
 
+Live Demo:
+
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Write a JavaScript function to make currency math (add, subtract, multiply, division etc.).</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+
+var n1 = '$40.24';
+var n2 = '$21.57';
+var regp = /[^0-9.-]+/g;
+
+console.log(parseFloat(n1.replace(regp, '')) +       parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) -         parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) *         parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) /         parseFloat(n2.replace(regp, '')));
+
+
+
+
+
+
+
 
  */
