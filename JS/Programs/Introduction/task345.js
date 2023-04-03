2c3d59753ff6f2aba28f708f5a464228f6e750d3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:38:42 +0400

    task

---
+++ JS/Programs/Introduction/task345.js
@@ -0,0 +1,18 @@
+// JavaScript: Calculate degrees between 2 points with inverse Y axis
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-27 with Solution
+Write a JavaScript function to calculate degrees between 2 points with inverse Y axis.
+
+Test Data :
+console.log(pointDirection(1, 0, 12, 0));
+0
+console.log(pointDirection(1, 0, 1, 10));
+90
+
+
+
+ */
