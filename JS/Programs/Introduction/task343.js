84f532abee487fee39107f6bc0ddc4d9a8a7d51c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:31:00 +0400

    update1

--- JS/Programs/Introduction/task343.js
+++ JS/Programs/Introduction/task343.js
@@ -10,4 +10,27 @@ Test Data :
 n1 = '$40.24', n2 = '$21.57';
 
 
+
+
+
+HTML Code:
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Write a JavaScript function to make currency math (add, subtract, multiply, division etc.).</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
  */
