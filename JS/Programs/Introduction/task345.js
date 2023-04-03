a8eb361ad3177f5d3d2da1415f0d4ba2982e5f09
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:40:02 +0400

    update2

--- JS/Programs/Introduction/task345.js
+++ JS/Programs/Introduction/task345.js
@@ -36,5 +36,21 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function pointDirection(x1, y1, x2, y2) {
+    return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+}
+
+console.log(pointDirection(1, 0, 12, 0));
+console.log(pointDirection(1, 0, 1, 10));
+
+
+
+
+
+
+
+
 
  */
