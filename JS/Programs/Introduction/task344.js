57dc0da2844f818f26dc12f6280f2642dcec3078
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:36:45 +0400

    update3

--- JS/Programs/Introduction/task344.js
+++ JS/Programs/Introduction/task344.js
@@ -50,4 +50,43 @@ console.log(nthroot(64, -2));
 
 
 
+Live Demo:
+
+
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate the nth root of a number</title>
+</head>
+<body>
+​
+</body>
+
+
+
+js:
+
+
+
+
+function nthroot(x, n)
+   {
+    ng = n % 2;
+    if((ng == 1) || x<0)
+       x = -x;
+    var r = Math.pow(x, 1 / n);
+    n = Math.pow(r, n);
+  
+    if(Math.abs(x - n) < 1 && (x > 0 === n > 0))
+      return ng ? -r : r; 
+   }
+console.log(nthroot(64, 2));
+console.log(nthroot(64, -2));
+
+
+
  */
