ccb3b3a2f0ebbb938df238bae4411fbfb9dfb566
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-03 23:31:46 +0400

    update2

--- JS/Programs/Introduction/task343.js
+++ JS/Programs/Introduction/task343.js
@@ -32,5 +32,24 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+var n1 = '$40.24';
+var n2 = '$21.57';
+var regp = /[^0-9.-]+/g;
+
+console.log(parseFloat(n1.replace(regp, '')) +       parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) -         parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) *         parseFloat(n2.replace(regp, '')));
+
+console.log(parseFloat(n1.replace(regp, '')) /         parseFloat(n2.replace(regp, '')));
+
+
+
+
+
+
 
  */
