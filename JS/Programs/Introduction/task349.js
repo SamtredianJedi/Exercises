8d8d07f49cf8f3d353fa21d33931a89abffc598e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-04 23:43:22 +0400

    task

---
+++ JS/Programs/Introduction/task349.js
@@ -0,0 +1,16 @@
+// JavaScript: Get the highest number from three different numbers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-31 with Solution
+Write a JavaScript function to get the highest number from three different numbers.
+
+Test Data:
+console.log(highest_of_three(-5, 4, 2));
+4
+
+
+
+ */
