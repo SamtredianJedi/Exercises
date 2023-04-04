0016d5d81fa09f9d3bbadaa2da3a33f41b97f33b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-04 23:35:48 +0400

    update1

--- JS/Programs/Introduction/task348.js
+++ JS/Programs/Introduction/task348.js
@@ -15,4 +15,20 @@ console.log(sqrt_to_int(17));
 
 
 
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to cast a square root of a number to an integer.</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
