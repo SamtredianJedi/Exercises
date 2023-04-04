a5d1653e6b9028e1e6dea264da376bf892919ca6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-04 23:28:03 +0400

    update2

--- JS/Programs/Introduction/task346.js
+++ JS/Programs/Introduction/task346.js
@@ -32,5 +32,24 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function int_round5(num)
+{
+    return Math.ceil(num/5)*5;
+}
+
+console.log(int_round5(32));
+console.log(int_round5(137));
+console.log(int_round5(142));
+
+
+
+
+
+
+
+
+
 
  */
