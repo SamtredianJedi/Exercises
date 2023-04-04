7c51ccc1061e79fd8fcc860196fa13d0b98ab819
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-04 23:35:15 +0400

    task

---
+++ JS/Programs/Introduction/task348.js
@@ -0,0 +1,18 @@
+// JavaScript: Cast a square root of a number to an integer
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-30 with Solution
+Write a JavaScript function to cast a square root of a number to an integer.
+
+Test Data:
+console.log(sqrt_to_int(17));
+4
+
+
+
+
+ */
