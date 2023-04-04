4e113590887294012a32fbff17cb4b40044a2a6e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-04 23:31:21 +0400

    task

---
+++ JS/Programs/Introduction/task347.js
@@ -0,0 +1,15 @@
+// JavaScript: Convert a positive number to negative number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-29 with Solution
+Write a JavaScript function to convert a positive number to negative number.
+
+Test Data :
+console.log(pos_to_neg(15));
+-15
+
+
+ */
