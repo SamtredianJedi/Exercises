9e33e17fd3f857f146bfa11648c83d93e06135a4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:29:58 +0400

    update2

--- JS/Programs/Introduction/task349.js
+++ JS/Programs/Introduction/task349.js
@@ -29,5 +29,19 @@ HTML Code:
 </html>
 
 
+JavaScript Code:
+
+function highest_of_three(num1, num2, num3)
+{
+    return Math.max(num1, num2, num3);  
+}
+
+console.log(highest_of_three(-5, 4, 2));
+
+
+
+
+
+
 
  */
