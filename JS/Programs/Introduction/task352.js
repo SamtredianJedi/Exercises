dc70aa8b2c27a3d08b56484a047f5041287d4512
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:40:51 +0400

    task

---
+++ JS/Programs/Introduction/task352.js
@@ -0,0 +1,14 @@
+// JavaScript: Convert radians to degrees
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-34 with Solution
+Write a JavaScript function to convert radians to degrees.
+
+Test Data : console.log(radians_to_degrees(0.7853981633974483));
+45
+
+
+
+ */
