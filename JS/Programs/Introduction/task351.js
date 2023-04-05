7c01f003b1f9a053701c537c8f1d2b24bb8fc460
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:38:48 +0400

    update3

--- JS/Programs/Introduction/task351.js
+++ JS/Programs/Introduction/task351.js
@@ -45,4 +45,40 @@ console.log(degrees_to_radians(90));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to convert degrees to radians</title>
+</head>
+<body>
+
+</body>
+
+
+
+
+js:
+
+
+
+
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+console.log(degrees_to_radians(45));
+
+
+
+
+
  */
