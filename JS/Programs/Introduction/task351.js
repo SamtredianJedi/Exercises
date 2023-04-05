3aee01038d35896df03638242af36b5f53199657
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:36:52 +0400

    task

---
+++ JS/Programs/Introduction/task351.js
@@ -0,0 +1,18 @@
+// JavaScript: Convert an angle from degrees to radians
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-33 with Solution
+Write a JavaScript function to convert an angle from degrees to radians.
+
+Test Data :
+console.log(degrees_to_radians(45));
+0.7853981633974483
+
+Use Math.PI and the degree to radian formula to convert the angle from degrees to radians.
+
+
+
+ */
