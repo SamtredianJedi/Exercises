d7ea239dde25ba7ca086d067b6c5b3e67910ffb1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:31:32 +0400

    update3

--- JS/Programs/Introduction/task349.js
+++ JS/Programs/Introduction/task349.js
@@ -42,6 +42,39 @@ console.log(highest_of_three(-5, 4, 2));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to get the highest number from three different numbers</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+function highest_of_three(num1, num2, num3)
+{
+    return Math.max(num1, num2, num3);  
+}
+​
+console.log(highest_of_three(-5, 4, 2));
+
+
+
+
+
+
 
 
  */
