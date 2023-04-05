16b110d6e956743ff2a0f2f958b2194706e08ebd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:33:18 +0400

    task

---
+++ JS/Programs/Introduction/task350.js
@@ -0,0 +1,15 @@
+// JavaScript: Calculate the percentage of a number
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-32 with Solution
+Write a JavaScript function to calculate the percentage (%) of a number.
+
+Test Data:
+console.log(percentage(1000, 47.12));
+471.2
+
+
+
+ */
