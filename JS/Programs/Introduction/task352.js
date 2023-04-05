025012956a6b54eb988d4df5908607ab56479a32
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-05 23:41:30 +0400

    update1

--- JS/Programs/Introduction/task352.js
+++ JS/Programs/Introduction/task352.js
@@ -11,4 +11,20 @@ Test Data : console.log(radians_to_degrees(0.7853981633974483));
 
 
 
+
+
+Sample Solution-1:
+
+JavaScript Code:
+
+function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
+          
+console.log(radians_to_degrees(0.7853981633974483));
+
+
+
  */
