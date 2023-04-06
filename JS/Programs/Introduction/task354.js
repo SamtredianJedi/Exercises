7a746fb1bdd1257829bd337d614828214fecb55e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-06 18:21:04 +0400

    update3

--- JS/Programs/Introduction/task354.js
+++ JS/Programs/Introduction/task354.js
@@ -45,4 +45,34 @@ console.log(isPower_of_two(94));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function which will return values that are powers of two</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+function isPower_of_two(num)
+  {
+     return num && (num & (num - 1)) === 0;
+}
+​
+console.log(isPower_of_two(64));  
+console.log(isPower_of_two(94));
+
+
+
  */
