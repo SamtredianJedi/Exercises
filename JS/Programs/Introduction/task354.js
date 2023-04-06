f175c945d344eb60de6dc7f24480efd60c22b573
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-06 18:20:06 +0400

    update2

--- JS/Programs/Introduction/task354.js
+++ JS/Programs/Introduction/task354.js
@@ -31,4 +31,18 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function isPower_of_two(num)
+  {
+     return num && (num & (num - 1)) === 0;
+}
+
+console.log(isPower_of_two(64));  
+console.log(isPower_of_two(94));
+
+
+
+
  */
