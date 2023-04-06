2290c2ff7ebe9bf6b32cc7a793ef73913f38d997
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-06 18:15:45 +0400

    update1

--- JS/Programs/Introduction/task353.js
+++ JS/Programs/Introduction/task353.js
@@ -13,4 +13,22 @@ console.log(pythagorean(4, 3));
 5
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Write a JavaScript function for the Pythagorean theorem</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
