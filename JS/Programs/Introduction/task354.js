bff5b9c5bc905b993485f7eea7ae454e0dfc96c3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-06 18:18:39 +0400

    task

---
+++ JS/Programs/Introduction/task354.js
@@ -0,0 +1,18 @@
+// JavaScript: Return values that are powers of two
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-36 with Solution
+Write a JavaScript function which will return values that are powers of two.
+
+Test Data :
+console.log(isPower_of_two(64));
+true
+console.log(isPower_of_two(94));
+false
+
+
+
+ */
