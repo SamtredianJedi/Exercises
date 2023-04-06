600143136dd67f1f4bb539863363880a2899dc9a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-06 18:17:16 +0400

    update3

--- JS/Programs/Introduction/task353.js
+++ JS/Programs/Introduction/task353.js
@@ -44,4 +44,35 @@ console.log(pythagorean(4, 3));
 
 
 
+Live Demo:
+
+
+html:
+
+<head>
+  <meta charset="utf-8">
+  <title>Write a JavaScript function for the Pythagorean theorem</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+function pythagorean(sideA, sideB){
+  return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+}
+​
+console.log(pythagorean(4, 3));
+
+
+
+
+
+
+
+
  */
