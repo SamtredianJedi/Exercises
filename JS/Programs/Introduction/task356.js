dab8171ec966d9585d7659a1d9c3609bac83dbe8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:30:03 +0400

    update2

--- JS/Programs/Introduction/task356.js
+++ JS/Programs/Introduction/task356.js
@@ -33,4 +33,25 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function number_test(n)
+{
+   var result = (n - Math.floor(n)) !== 0; 
+   
+  if (result)
+    return 'Number has a decimal place.';
+   else
+     return 'It is a whole number.';
+  }
+
+console.log(number_test(25.66));
+
+console.log(number_test(10));
+
+
+
  */
