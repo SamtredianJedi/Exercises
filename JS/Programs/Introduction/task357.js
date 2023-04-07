9d4a64b10b42bac898728dd21a56d2355656099a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:33:09 +0400

    task

---
+++ JS/Programs/Introduction/task357.js
@@ -0,0 +1,19 @@
+// JavaScript: Print an integer with commas as thousands separators
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-39 with Solution
+Write a JavaScript function to print an integer with commas as thousands separators.
+
+Test Data:
+console.log(thousands_separators(1000));
+"1,000"
+console.log(thousands_separators(10000.23));
+"10,000.23"
+console.log(thousands_separators(100000));
+"100,000"
+
+
+
+ */
