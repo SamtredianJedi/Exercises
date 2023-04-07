25499b076d7fbdc59331ad219e6560f58b4cde23
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:27:20 +0400

    update3

--- JS/Programs/Introduction/task355.js
+++ JS/Programs/Introduction/task355.js
@@ -47,6 +47,41 @@ console.log(value_limit(15, 0, 12));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to limit a value inside a certain range.</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+function value_limit(val, min, max) {
+  return val < min ? min : (val > max ? max : val);
+}
+
+console.log(value_limit(7, 1, 12));
+console.log(value_limit(-7, 0, 12));
+console.log(value_limit(15, 0, 12));
+
+
+
+
+
+
+
+
 
 
  */
