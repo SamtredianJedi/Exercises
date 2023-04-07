12c90aac76ab07f082e96d65bd2ecbd7f617d5ae
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:30:53 +0400

    update3

--- JS/Programs/Introduction/task356.js
+++ JS/Programs/Introduction/task356.js
@@ -54,4 +54,43 @@ console.log(number_test(10));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to check if a number is a whole number or has a decimal place</title>
+</head>
+<body>
+​
+</body>
+
+
+
+
+js:
+
+
+
+function number_test(n)
+{
+   var result = (n - Math.floor(n)) !== 0; 
+   
+  if (result)
+    return 'Number has a decimal place.';
+   else
+     return 'It is a whole number.';
+  }
+​
+console.log(number_test(25.66));
+​
+console.log(number_test(10));
+
+
+
+
  */
