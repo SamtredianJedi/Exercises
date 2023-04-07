a454e1d575635021142317a8d7cecd488265cf27
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:34:56 +0400

    update3

--- JS/Programs/Introduction/task357.js
+++ JS/Programs/Introduction/task357.js
@@ -48,4 +48,37 @@ console.log(thousands_separators(100000));
 
 
 
+Live Demo:
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to print an integer with commas as thousands separators</title>
+</head>
+<body>
+​
+</body>
+
+
+js:
+
+
+
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+
+console.log(thousands_separators(1000));
+console.log(thousands_separators(10000.23));
+console.log(thousands_separators(100000));
+
+
+
+
  */
