7011543716b8d70c47f9250d1ce597e7b15570c4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:34:11 +0400

    update2

--- JS/Programs/Introduction/task357.js
+++ JS/Programs/Introduction/task357.js
@@ -32,5 +32,20 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+
+console.log(thousands_separators(1000));
+console.log(thousands_separators(10000.23));
+console.log(thousands_separators(100000));
+
+
+
 
  */
