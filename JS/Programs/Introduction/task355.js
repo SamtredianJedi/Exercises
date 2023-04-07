f7b477b124fba6176edefc6a94bb627c328ddd5d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-07 23:26:05 +0400

    update2

--- JS/Programs/Introduction/task355.js
+++ JS/Programs/Introduction/task355.js
@@ -34,4 +34,19 @@ HTML Code :
 
 
 
+
+JavaScript Code:
+
+function value_limit(val, min, max) {
+  return val < min ? min : (val > max ? max : val);
+}
+
+console.log(value_limit(7, 1, 12));
+console.log(value_limit(-7, 0, 12));
+console.log(value_limit(15, 0, 12));
+
+
+
+
+
  */
