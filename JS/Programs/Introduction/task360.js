db37995b57d962605041c0f3abc286da6fa8928c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:46:49 +0400

    update3

--- JS/Programs/Introduction/task360.js
+++ JS/Programs/Introduction/task360.js
@@ -67,5 +67,59 @@ console.log(combinations(5, 3));
 
 
 
+Live Demo:
+
+
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate the combination of n and r. The formula is : n!/(r!*(n - r)!)</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+function product_Range(a,b) {
+  var prd = a,i = a;
+ 
+  while (i++< b) {
+    prd*=i;
+  }
+  return prd;
+}
+​
+​
+function combinations(n, r) 
+{
+   if (n==r || r==0) 
+  {
+    return 1;
+  } 
+  else 
+  {
+    r=(r < n-r) ? n-r : r;
+    return product_Range(r+1, n)/product_Range(1,n-r);
+  }
+}
+​
+​
+console.log(combinations(6, 2));
+console.log(combinations(5, 3));
+
+
+
+
+
+
+
 
  */
