228a34fd9dc8938b028ab4e4f742541bcd34ac48
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:44:30 +0400

    task

---
+++ JS/Programs/Introduction/task360.js
@@ -0,0 +1,20 @@
+// JavaScript: Calculate the combination of n and r
+
+
+/**
+ * 
+
+
+JavaScript Math: Exercise-42 with Solution
+Write a JavaScript function to calculate the combination of n and r.
+The formula is : n!/(r!*(n - r)!).
+
+Test Data :
+console.log(combinations(6, 2));
+15
+console.log(combinations(5, 3));
+10
+
+
+
+ */
