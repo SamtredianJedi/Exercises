f8cf9086ac3b7ad74a55a1c6e92a33a78936a78b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:45:53 +0400

    update2

--- JS/Programs/Introduction/task360.js
+++ JS/Programs/Introduction/task360.js
@@ -34,5 +34,38 @@ html:
 
 
 
+JavaScript Code:
+
+function product_Range(a,b) {
+  var prd = a,i = a;
+ 
+  while (i++< b) {
+    prd*=i;
+  }
+  return prd;
+}
+
+
+function combinations(n, r) 
+{
+  if (n==r || r==0) 
+  {
+    return 1;
+  } 
+  else 
+  {
+    r=(r < n-r) ? n-r : r;
+    return product_Range(r+1, n)/product_Range(1,n-r);
+  }
+}
+
+
+console.log(combinations(6, 2));
+console.log(combinations(5, 3));
+
+
+
+
+
 
  */
