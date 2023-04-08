4c62a9fb1e7eca56764887a35797669d47c00b78
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:39:12 +0400

    update3

--- JS/Programs/Introduction/task358.js
+++ JS/Programs/Introduction/task358.js
@@ -45,4 +45,42 @@ random_bg_color();
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to create random background color</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+​
+random_bg_color();
+
+
+
+
+
+
  */
