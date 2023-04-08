af893b146f7c7484f6caf094992f4d8ed698f022
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:40:42 +0400

    task

---
+++ JS/Programs/Introduction/task359.js
@@ -0,0 +1,12 @@
+// JavaScript: Count the digits of an integer
+
+/**
+ * 
+ * 
+
+JavaScript Math: Exercise-41 with Solution
+Write a JavaScript function to count the digits of an integer.
+
+
+
+ */
