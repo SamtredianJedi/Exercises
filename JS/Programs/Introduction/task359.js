902a7d4a26939062be8e4ec83a9f22e79031212d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:41:52 +0400

    update2

--- JS/Programs/Introduction/task359.js
+++ JS/Programs/Introduction/task359.js
@@ -27,5 +27,26 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function digits_count(n) {
+  var count = 0;
+  if (n >= 1) ++count;
+
+  while (n / 10 >= 1) {
+    n /= 10;
+    ++count;
+  }
+
+  return count;
+}
+
+console.log(digits_count(12112));
+
+console.log(digits_count(457));
+
+
+
+
 
  */
