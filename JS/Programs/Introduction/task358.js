c0e09f48200d89056567fde085c720ee52f14371
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:36:35 +0400

    task

---
+++ JS/Programs/Introduction/task358.js
@@ -0,0 +1,10 @@
+// JavaScript: Create random background color
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-40 with Solution
+Write a JavaScript function to create random background color.
+
+
+ */
