d97fdb189dbb334355605c5d9f59a9f96e2e3230
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:38:16 +0400

    update2

--- JS/Programs/Introduction/task358.js
+++ JS/Programs/Introduction/task358.js
@@ -25,4 +25,24 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
+
+
+
+
+
  */
