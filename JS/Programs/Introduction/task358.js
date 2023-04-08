34af9df4637f3db0ee2ec81868c1561d4c2c7d77
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-08 23:37:25 +0400

    update1

--- JS/Programs/Introduction/task358.js
+++ JS/Programs/Introduction/task358.js
@@ -7,4 +7,22 @@
 Write a JavaScript function to create random background color.
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to create random background color</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
