356e9c586f03493dc23da42c8781b93e5f881486
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:35:14 +0400

    task

---
+++ JS/Programs/Introduction/task362.js
@@ -0,0 +1,13 @@
+// JavaScript: Show the Hamming numbers
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-44 with Solution
+Write a JavaScript function to show the first twenty Hamming numbers.
+
+Hamming Numbers are numbers whose only prime factors are 2, 3 and 5.
+
+
+
+ */
