4b2460660c5083ca2d0d18a9a0c0bf4ad7bee5ea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:39:51 +0400

    task

---
+++ JS/Programs/Introduction/task363.js
@@ -0,0 +1,12 @@
+// JavaScript: Subtract elements from one another in an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-45 with Solution
+Write a JavaScript function to subtract elements from one another in an array.
+
+
+
+ */
