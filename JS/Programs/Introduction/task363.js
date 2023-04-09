f93372d6aedda6782daa609cbae1a17c2b2329a7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:41:09 +0400

    update2

--- JS/Programs/Introduction/task363.js
+++ JS/Programs/Introduction/task363.js
@@ -26,4 +26,33 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function subtraction(arr) 
+ {
+  if (Object.prototype.toString.call(arr) === '[object Array]') {
+    var total = arr[0];
+    if (typeof (total) !== 'number') {
+      return false;
+    }
+    for (var i = 1, length = arr.length; i < length; i++)
+    {
+      if (typeof (arr[i]) === 'number')
+      {
+        total -= arr[i];
+      } 
+      else 
+      return false;
+    }
+    return total;
+   } 
+    else
+     return false;
+  }
+console.log(subtraction([7,3, 2,-1]));
+
+
+
+
  */
