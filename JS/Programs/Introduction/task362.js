863faf1be6f38f8899b65dd2036ae6463dab4540
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:36:37 +0400

    update1

--- JS/Programs/Introduction/task362.js
+++ JS/Programs/Introduction/task362.js
@@ -10,4 +10,21 @@ Hamming Numbers are numbers whose only prime factors are 2, 3 and 5.
 
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to show the Hamming numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
