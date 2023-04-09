af237428ef6469bb124e29d32525f02cc0b6529c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:33:40 +0400

    update3

--- JS/Programs/Introduction/task361.js
+++ JS/Programs/Introduction/task361.js
@@ -67,4 +67,50 @@ console.log(primes(15));
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to get all prime numbers from 0 to num</title>
+</head>
+<body>
+​
+</body>
+
+
+js:
+
+
+
+function primeFactorsTo(max)
+{
+    var store  = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) 
+    {
+        if (!store [i]) 
+          {
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) 
+            {
+                store[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+​
+console.log(primeFactorsTo(5));
+​
+console.log(primeFactorsTo(15));
+
+
+
+
  */
