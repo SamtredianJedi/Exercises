40c3a845f22b152ff9b4da077c3010082ca7ecb3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:31:46 +0400

    update1

--- JS/Programs/Introduction/task361.js
+++ JS/Programs/Introduction/task361.js
@@ -13,4 +13,33 @@ console.log(primeFactorsTo(15));
 [2, 3, 5, 7, 11, 13]
 
 
+
+
+Sample Solution-1:
+
+JavaScript Code:
+
+function primeFactorsTo(max)
+{
+    var store  = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) 
+    {
+        if (!store [i]) 
+          {
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) 
+            {
+                store[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
+console.log(primeFactorsTo(5));
+
+console.log(primeFactorsTo(15));
+
+
+
  */
