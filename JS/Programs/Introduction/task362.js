268e1e860177e3d16dae0005a5c5a6cb53282809
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:37:11 +0400

    update2

--- JS/Programs/Introduction/task362.js
+++ JS/Programs/Introduction/task362.js
@@ -27,4 +27,34 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function Hamming(n) {
+        var succession = [1];
+        var length = succession.length;
+        var candidate = 2;
+        while (length < n) {
+            if (isHammingNumber(candidate)) {
+                succession[length] = candidate;
+                length++;
+            }
+            candidate++;
+        }
+        return succession;
+  }
+ function isHammingNumber(num) {
+        while (num % 5 === 0) num /= 5;
+        while (num % 3 === 0) num /= 3;
+        while (num % 2 === 0) num /= 2;
+
+        return num == 1;
+    }
+
+console.log(Hamming(20));
+
+
+
  */
