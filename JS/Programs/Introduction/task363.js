d7703d0ed8bfa48b8eb231e4321ec5ec1ea9891a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:42:07 +0400

    update3

--- JS/Programs/Introduction/task363.js
+++ JS/Programs/Introduction/task363.js
@@ -55,4 +55,52 @@ console.log(subtraction([7,3, 2,-1]));
 
 
 
+Live Demo:
+
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to subtract elements from one another in array</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+function subtraction(arr) 
+ {
+  if (Object.prototype.toString.call(arr) === '[object Array]') {
+    var total = arr[0];
+    if (typeof (total) !== 'number') {
+      return false;
+    }
+    for (var i = 1, length = arr.length; i < length; i++)
+    {
+      if (typeof (arr[i]) === 'number')
+      {
+        total -= arr[i];
+      } 
+      else 
+      return false;
+    }
+    return total;
+   } 
+    else
+     return false;
+  }
+console.log(subtraction([7,3, 2,-1]));
+
+
+
+
+
  */
