cab18ed9f8891ff70a73655685ddf25b3115d6c8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-09 22:40:35 +0400

    update1

--- JS/Programs/Introduction/task363.js
+++ JS/Programs/Introduction/task363.js
@@ -9,4 +9,21 @@ Write a JavaScript function to subtract elements from one another in an array.
 
 
 
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to subtract elements from one another in array</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
  */
