3fb2d02f8ed94df3d8ce499c079c642476e38adf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-10 23:37:03 +0400

    update3

--- JS/Programs/Introduction/task364.js
+++ JS/Programs/Introduction/task364.js
@@ -42,6 +42,43 @@ console.log(div_mod(17, 4));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate the divisor and modulus of two integers</title>
+</head>
+<body>
+
+</body>
+
+
+
+js:
+
+
+
+function div_mod(a, b)
+ {
+  if (b <= 0) 
+    throw new Error("b cannot be zero. Undefined.");
+  if (isInt(a) && !isInt(b))
+    throw new Error("A or B are not integers.");
+  return [Math.floor(a / b), a % b];
+}
+function isInt(n) {
+  return n % 1 === 0;
+}
+console.log(div_mod(17, 4));
+
+
+
+
 
 
  * 
