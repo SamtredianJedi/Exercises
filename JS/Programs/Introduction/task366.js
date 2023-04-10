96d94d6c7a4280c7e972a588f990da099d58c9e8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-10 23:43:13 +0400

    task

---
+++ JS/Programs/Introduction/task366.js
@@ -0,0 +1,18 @@
+// JavaScript: Calculate the falling factorial of a number
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-48 with Solution
+Write a JavaScript function to calculate the falling factorial of a number.
+
+Let x be a real number (but usually an integer).
+Let k be a positive integer.
+Then x to the (power of) k falling is:
+
+
+
+This is called the kth falling factorial power of x.
+
+
+ */
