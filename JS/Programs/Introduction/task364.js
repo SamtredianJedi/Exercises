3a486bb334e8d66e2066c017d0817b3b23747e5d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-10 23:35:53 +0400

    update3

--- JS/Programs/Introduction/task364.js
+++ JS/Programs/Introduction/task364.js
@@ -23,5 +23,26 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function div_mod(a, b)
+ {
+  if (b <= 0) 
+    throw new Error("b cannot be zero. Undefined.");
+  if (isInt(a) && !isInt(b))
+    throw new Error("A or B are not integers.");
+  return [Math.floor(a / b), a % b];
+}
+function isInt(n) {
+  return n % 1 === 0;
+}
+console.log(div_mod(17, 4));
+
+
+
+
+
+
  * 
  */
