3cc1a64a15e8efefe08f36094d855418e30fb8a3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-10 23:34:52 +0400

    update1

--- JS/Programs/Introduction/task364.js
+++ JS/Programs/Introduction/task364.js
@@ -6,5 +6,22 @@
 Write a JavaScript function to calculate the divisor and modulus of two integers.
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate the divisor and modulus of two integers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  * 
  */
