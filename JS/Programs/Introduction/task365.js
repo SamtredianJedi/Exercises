3ab7d2f2089d3b7686f5b5c4d1d5ac7aff8a4fe7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-10 23:38:31 +0400

    task

---
+++ JS/Programs/Introduction/task365.js
@@ -0,0 +1,14 @@
+// JavaScript: Calculate the extended Euclid Algorithm or extended GCD
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-47 with Solution
+Write a JavaScript function to calculate the extended Euclid Algorithm or extended GCD.
+
+In mathematics, the Euclidean algorithm[a], or Euclid's algorithm, is an efficient method for computing the greatest common divisor (GCD) of two numbers, the largest number that divides both of them without leaving a remainder. It is named after the ancient Greek mathematician Euclid, who first described it in Euclid's Elements. It is an example of an algorithm, a step-by-step procedure for performing a calculation according to well-defined rules, and is one of the oldest algorithms in common use. It can be used to reduce fractions to their simplest form, and is a part of many other number-theoretic and cryptographic calculations.
+
+
+
+ */
