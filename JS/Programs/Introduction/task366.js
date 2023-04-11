64af969f8acdecc79c67d73395da11f503d673aa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-11 17:36:21 +0400

    update3

--- JS/Programs/Introduction/task366.js
+++ JS/Programs/Introduction/task366.js
@@ -58,4 +58,48 @@ console.log(fallingFactorial(10, 2));
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate the falling factorial of a number</title>
+</head>
+<body>
+​
+</body>
+
+
+js:
+
+
+
+function fallingFactorial(n, k) 
+{
+  var i = (n - k + 1),
+    r = 1;
+  if (n < 0) 
+  {
+    throw new Error("n must be positive.");
+  }
+  if (k > n)
+  {
+    throw new Error("k cannot be greater than n.");
+  }
+  while (i <= n) 
+  {
+    r *= i++;
+  }
+  return r;
+}
+console.log(fallingFactorial(10, 2));
+
+
+
+
  */
