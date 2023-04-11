75b791d0d10a3b4218675d970ac9d91e660ba7a7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-11 17:39:11 +0400

    update1

--- JS/Programs/Introduction/task367.js
+++ JS/Programs/Introduction/task367.js
@@ -9,4 +9,21 @@ Write a JavaScript function to calculate Lanczos approximation gamma.
 In mathematics, the Lanczos approximation is a method for computing the Gamma function numerically, published by Cornelius Lanczos in 1964. It is a practical alternative to the more popular Stirling's approximation for calculating the Gamma function with fixed precision.
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to calculate Lanczos approximation gamma</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
