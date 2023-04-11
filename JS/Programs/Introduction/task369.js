0c15fb16fa4b9c5465a50529b77343232a76672b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-11 17:48:34 +0400

    update1

--- JS/Programs/Introduction/task369.js
+++ JS/Programs/Introduction/task369.js
@@ -10,4 +10,20 @@ Write a JavaScript program to subtract two complex numbers.
 A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers and i is the imaginary unit, that satisfies the equation i2 = −1. In this expression, a is the real part and b is the imaginary part of the complex number.
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to subtract two complex numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
