187c1f234b9b39916a85b2d895e9692e1db6d89f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-11 17:35:16 +0400

    update2

--- JS/Programs/Introduction/task366.js
+++ JS/Programs/Introduction/task366.js
@@ -33,4 +33,29 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function fallingFactorial(n, k) 
+{
+  var i = (n - k + 1),
+    r = 1;
+  if (n < 0) 
+  {
+    throw new Error("n must be positive.");
+  }
+  if (k > n)
+  {
+    throw new Error("k cannot be greater than n.");
+  }
+  while (i <= n) 
+  {
+    r *= i++;
+  }
+  return r;
+}
+console.log(fallingFactorial(10, 2));
+
+
+
+
  */
