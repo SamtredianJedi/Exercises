87bbfbc9d9cea328d2ee269298e5cbee5e87ab5a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-12 23:15:04 +0400

    task

---
+++ JS/Programs/Introduction/task372.js
@@ -0,0 +1,12 @@
+// JavaScript: Check if a given number is a power of 10
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-54 with Solution
+Write a JavaScript program to check if a given number is a power of 10.
+Math.log10() and the modulo operator (%) determines if num is a power of 10.
+
+
+
+ */
