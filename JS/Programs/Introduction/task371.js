19c64aa628c76d37328833d8405bb7a259eb0ab3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-12 23:10:06 +0400

    task

---
+++ JS/Programs/Introduction/task371.js
@@ -0,0 +1,13 @@
+// JavaScript: Divide two complex numbers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-53 with Solution
+Write a JavaScript program to divide two complex numbers.
+A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers and i is the imaginary unit, that satisfies the equation i2 = −1. In this expression, a is the real part and b is the imaginary part of the complex number.
+
+
+
+ */
