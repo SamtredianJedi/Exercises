7cd8d2252ea4abc462e934cefd1f95570446834a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-12 22:58:20 +0400

    task

---
+++ JS/Programs/Introduction/task370.js
@@ -0,0 +1,13 @@
+// JavaScript: Multiply two complex numbers
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-52 with Solution
+Write a JavaScript program to multiply two complex numbers.
+A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers and i is the imaginary unit, that satisfies the equation i2 = −1. In this expression, a is the real part and b is the imaginary part of the complex number.
+
+
+ */
