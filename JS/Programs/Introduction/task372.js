41155488837ef737084cae7d332189c99eecb80b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-12 23:15:37 +0400

    update1

--- JS/Programs/Introduction/task372.js
+++ JS/Programs/Introduction/task372.js
@@ -9,4 +9,22 @@ Math.log10() and the modulo operator (%) determines if num is a power of 10.
 
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to check if a  given number is a power of 10</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
  */
