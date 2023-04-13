6423a05fba3c255df91c59dc146b3e69ed1f8961
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 22:50:11 +0400

    update2

--- JS/Programs/Introduction/task372.js
+++ JS/Programs/Introduction/task372.js
@@ -26,5 +26,18 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+const is_Power_Of_Ten = num => Math.log10(num) % 1 === 0;
+console.log(is_Power_Of_Ten(1));
+console.log(is_Power_Of_Ten(10));
+console.log(is_Power_Of_Ten(30));
+console.log(is_Power_Of_Ten(100));
+console.log(is_Power_Of_Ten(90));
+console.log(is_Power_Of_Ten(1000));
+
+
+
+
 
  */
