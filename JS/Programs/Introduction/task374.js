ecdf1e3dc4679a692b257cd88d78c9f8153dec1a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 23:02:35 +0400

    update3

--- JS/Programs/Introduction/task374.js
+++ JS/Programs/Introduction/task374.js
@@ -52,4 +52,51 @@ console.log(vol_Cuboid(3.0, -2.0, 4.0))
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Cuboid</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+js:
+
+
+const vol_Cuboid = (width, length, height) => {
+ is_Number(width, 'Width')
+ is_Number(length, 'Length')
+ is_Number(height, 'Height')
+ return (width * length * height)
+}
+const is_Number = (n, n_name = 'number') => {
+ if (typeof n !== 'number') {
+   throw new TypeError('The ' + n_name + ' is not Number type!')
+ } else if (n < 0 || (!Number.isFinite(n))) {
+   throw new Error('The ' + n_name + ' must be a positive values!')
+ }
+}
+console.log(vol_Cuboid(3.0, 2.0, 4.0))
+console.log(vol_Cuboid('3.0', 2.0, 4.0))
+console.log(vol_Cuboid(3.0, -2.0, 4.0))
+
+
+
+
+
+
  */
