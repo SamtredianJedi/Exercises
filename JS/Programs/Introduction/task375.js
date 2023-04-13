0f8606fb5a9308dd653a48ca531286d2ed9cd246
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 23:03:55 +0400

    task

---
+++ JS/Programs/Introduction/task375.js
@@ -0,0 +1,15 @@
+// JavaScript: Volume of a Cube
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-57 with Solution
+Write a JavaScript program to calculate the volume of a Cube.
+
+From Wikipedia –
+In geometry, a cube is a three-dimensional solid object bounded by six square faces, facets or sides, with three meeting at each vertex. The cube is the only regular hexahedron and is one of the five Platonic solids. It has 6 faces, 12 edges, and 8 vertices.
+
+
+
+ */
