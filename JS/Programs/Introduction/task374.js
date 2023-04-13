86ec6cc5258aa8ebb5924ba5b4cac834ec75c448
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 23:01:28 +0400

    update3

--- JS/Programs/Introduction/task374.js
+++ JS/Programs/Introduction/task374.js
@@ -28,4 +28,28 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+const vol_Cuboid = (width, length, height) => {
+ is_Number(width, 'Width')
+ is_Number(length, 'Length')
+ is_Number(height, 'Height')
+ return (width * length * height)
+}
+const is_Number = (n, n_name = 'number') => {
+ if (typeof n !== 'number') {
+   throw new TypeError('The ' + n_name + ' is not Number type!')
+ } else if (n < 0 || (!Number.isFinite(n))) {
+   throw new Error('The ' + n_name + ' must be a positive values!')
+ }
+}
+console.log(vol_Cuboid(3.0, 2.0, 4.0))
+console.log(vol_Cuboid('3.0', 2.0, 4.0))
+console.log(vol_Cuboid(3.0, -2.0, 4.0))
+
+
+
  */
