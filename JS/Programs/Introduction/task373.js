98247151d102de00dcd6f40471748644e0789ef1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 22:57:14 +0400

    update2

--- JS/Programs/Introduction/task373.js
+++ JS/Programs/Introduction/task373.js
@@ -35,4 +35,20 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+let x = 20;
+let y = 40;
+console.log("x=",x,"y=",y)
+let temp;
+temp = x;
+x = y;
+y = temp;
+console.log("After swapping:")
+console.log("x=",x,"y=",y)
+
+
+
+
  */
