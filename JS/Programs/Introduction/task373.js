9e910e98a65589e5299d77df5b44745386315eaa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 22:55:58 +0400

    task

---
+++ JS/Programs/Introduction/task373.js
@@ -0,0 +1,20 @@
+// JavaScript: Swap two variables
+
+/**
+ * 
+
+
+JavaScript Math: Exercise-55 with Solution
+Write a JavaScript program to swap two given variables.
+
+Swapping two variables refers to mutually exchanging the values of the variables. Generally, this is done with the data in memory.
+
+The simplest method to swap two variables is to use a third temporary variable :
+define swap(x, y)
+temp := x
+x := y
+y := temp
+
+
+
+ */
