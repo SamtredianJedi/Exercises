c82c594550fd978278159a106ac7b3a210a12775
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-13 23:00:11 +0400

    task

---
+++ JS/Programs/Introduction/task374.js
@@ -0,0 +1,15 @@
+// JavaScript: Volume of a Cuboid
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-56 with Solution
+Write a JavaScript program to calculate the volume of a Cuboid.
+
+From Wikipedia –
+In geometry, a cuboid is a hexahedron, a six-faced solid. Its faces are quadrilaterals. Cuboid means "like a cube", in the sense that by adjusting the length of the edges or the angles between edges and faces a cuboid can be transformed into a cube.
+
+
+
+ */
