00a61242c1ce429a5ed29a3ef4746e8b8f0860c3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:31:47 +0400

    update1

--- JS/Programs/Introduction/task376.js
+++ JS/Programs/Introduction/task376.js
@@ -11,4 +11,18 @@ A cone is a three-dimensional geometric shape that tapers smoothly from a flat b
 A cone is formed by a set of line segments, half-lines, or lines connecting a common point, the apex, to all of the points on a base that is in a plane that does not contain the apex. Depending on the author, the base may be restricted to be a circle, any one-dimensional quadratic form in the plane, any closed one-dimensional figure, or any of the above plus all the enclosed points.
 
 
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Cone</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
