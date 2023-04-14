144645464e3d7143d825e2daa875eacc4b9eefa8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:28:35 +0400

    update3

--- JS/Programs/Introduction/task375.js
+++ JS/Programs/Introduction/task375.js
@@ -49,4 +49,41 @@ console.log(volume_Cube(-3.0))
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Cube</title>
+</head>
+<body>
+
+</body>
+</html>
+
+js:
+
+
+
+const volume_Cube = (length) => {
+ is_Number(length, 'Length')
+ return (length ** 3)
+}
+const is_Number = (n, n_name = 'number') => {
+ if (typeof n !== 'number') {
+   throw new TypeError('The ' + n_name + ' is not Number type!')
+ } else if (n < 0 || (!Number.isFinite(n))) {
+   throw new Error('The ' + n_name + ' must be a positive values!')
+ }
+}
+console.log(volume_Cube(3.0))
+
+
+
  */
