acd184752b1392982159f6d608ee2ce946ed58d6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:33:39 +0400

    update3

--- JS/Programs/Introduction/task376.js
+++ JS/Programs/Introduction/task376.js
@@ -47,4 +47,45 @@ console.log(volume_Cone(-3.0, 7.0))
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Cone</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+const volume_Cone = (radius, height) => {
+  is_Number(radius, 'Radius')
+  is_Number(height, 'Height')
+  return (Math.PI * radius ** 2 * height / 3.0)
+}
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+  } else if (n < 0 || (!Number.isFinite(n))) {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+  }
+}
+console.log(volume_Cone(3.0, 7.0))
+console.log(volume_Cone('3.0', 7.0))
+console.log(volume_Cone(-3.0, 7.0))
+
+
+
+
  */
