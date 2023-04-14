ab5360812d76a6d371f12c8b0f2c5995db601617
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:49:29 +0400

    task

---
+++ JS/Programs/Introduction/task378.js
@@ -0,0 +1,17 @@
+// JavaScript: Volume of a Cylinder
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-60 with Solution
+Write a JavaScript program to calculate the volume of a Cylinder.
+
+From Wikipedia -
+A cylinder has traditionally been a three-dimensional solid, one of the most basic of curvilinear geometric shapes. In elementary geometry, it is considered a prism with a circle as its base.
+A cylinder may also be defined as an infinite curvilinear surface in various modern branches of geometry and topology. The shift in the basic meaning - solid versus surface (as in ball and sphere) - has created some ambiguity with terminology. The two concepts may be distinguished by referring to solid cylinders and cylindrical surfaces. In the literature the unadorned term cylinder could refer to either of these or to an even more specialized object, the right circular cylinder.
+
+
+
+
+ */
