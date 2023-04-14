3e50f85df292dfc14c7e033bbbe141b11666f47c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:36:56 +0400

    update1

--- JS/Programs/Introduction/task377.js
+++ JS/Programs/Introduction/task377.js
@@ -9,4 +9,20 @@ Write a JavaScript program to calculate the volume of a Pyramid.
 In geometry, a pyramid is a polyhedron formed by connecting a polygonal base and a point, called the apex. Each base edge and apex form a triangle, called a lateral face. It is a conic solid with polygonal base. A pyramid with an n-sided base has n + 1 vertices, n + 1 faces, and 2n edges. All pyramids are self-dual.
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Pyramid</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
