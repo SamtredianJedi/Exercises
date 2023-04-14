03ab4ca87951c6910135c654e0a9e300124f1280
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:31:02 +0400

    task

---
+++ JS/Programs/Introduction/task376.js
@@ -0,0 +1,14 @@
+// JavaScript: Volume of a Cone
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-58 with Solution
+Write a JavaScript program to calculate the volume of a Cone.
+
+From Wikipedia -
+A cone is a three-dimensional geometric shape that tapers smoothly from a flat base (frequently, though not necessarily, circular) to a point called the apex or vertex.
+A cone is formed by a set of line segments, half-lines, or lines connecting a common point, the apex, to all of the points on a base that is in a plane that does not contain the apex. Depending on the author, the base may be restricted to be a circle, any one-dimensional quadratic form in the plane, any closed one-dimensional figure, or any of the above plus all the enclosed points.
+
+
+ */
