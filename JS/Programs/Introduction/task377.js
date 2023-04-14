48028ecc74938cf9346bc525c4aa9f2abff7fc03
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:40:58 +0400

    update3

--- JS/Programs/Introduction/task377.js
+++ JS/Programs/Introduction/task377.js
@@ -48,4 +48,49 @@ console.log(volume_Pyramid(2.0, 3.0, '7.0'))
 console.log(volume_Pyramid(2.0, -3.0, 7.0))
 
 
+
+
+
+Live Demo:
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Pyramid</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+const volume_Pyramid = (baseLength, baseWidth, height) => {
+  is_Number(baseLength, 'BaseLength')
+  is_Number(baseWidth, 'BaseWidth')
+  is_Number(height, 'Height')
+  return (baseLength * baseWidth * height) / 3.0
+}
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+  } else if (n < 0 || (!Number.isFinite(n))) {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+  }
+}
+console.log(volume_Pyramid(2.0, 3.0, 7.0))
+console.log(volume_Pyramid(2.0, 3.0, '7.0'))
+console.log(volume_Pyramid(2.0, -3.0, 7.0))
+
+
+
+
+
+
+
+
  */
