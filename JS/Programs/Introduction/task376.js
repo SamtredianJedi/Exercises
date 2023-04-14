8a546618794c9ffbeb15f2490ef776c7ea1224b9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-14 22:32:22 +0400

    update2

--- JS/Programs/Introduction/task376.js
+++ JS/Programs/Introduction/task376.js
@@ -25,4 +25,26 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+const volume_Cone = (radius, height) => {
+  is_Number(radius, 'Radius')
+  is_Number(height, 'Height')
+  return (Math.PI * radius ** 2 * height / 3.0)
+}
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+  } else if (n < 0 || (!Number.isFinite(n))) {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+  }
+}
+console.log(volume_Cone(3.0, 7.0))
+console.log(volume_Cone('3.0', 7.0))
+console.log(volume_Cone(-3.0, 7.0))
+
+
+
  */
