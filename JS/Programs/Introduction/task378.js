6dfdbdd016a4f205b6917da96a08e7ab45719daa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 20:55:35 +0400

    update3

--- JS/Programs/Introduction/task378.js
+++ JS/Programs/Introduction/task378.js
@@ -56,4 +56,48 @@ console.log(volume_Cylinder(2.0, -5.0))
 
 
 
+Live Demo:
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Cylinder</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+
+
+const volume_Cylinder = (radius, height) => {
+  is_Number(radius, 'Radius')
+  is_Number(height, 'Height')
+  return (Math.PI * radius ** 2 * height)
+}
+​
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+  } else if (n < 0 || (!Number.isFinite(n))) {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+  }
+}
+​
+console.log(volume_Cylinder(2.0, 5.0))
+​
+
+
+
+
+
  */
