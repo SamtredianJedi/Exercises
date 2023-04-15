5568ae40fd72cdd688890f2a1964d99468a8fd31
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 20:57:48 +0400

    update1

--- JS/Programs/Introduction/task379.js
+++ JS/Programs/Introduction/task379.js
@@ -9,4 +9,21 @@ From Wikipedia -
 In geometry, a triangular prism is a three-sided prism; it is a polyhedron made of a triangular base, a translated copy, and 3 faces joining corresponding sides. A right triangular prism has rectangular sides, otherwise it is oblique. A uniform triangular prism is a right triangular prism with equilateral bases, and square sides.
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Triangular Prism</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
