832b87c74d39793de25fcb04a69621127a77713b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 21:04:34 +0400

    task

---
+++ JS/Programs/Introduction/task381.js
@@ -0,0 +1,14 @@
+// JavaScript: Volume of a Sphere
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-63 with Solution
+Write a JavaScript program to calculate the volume of a Sphere.
+
+From Wikipedia -
+A sphere is a geometrical object that is a three-dimensional analogue to a two-dimensional circle. A sphere is the set of points that are all at the same distance r from a given point in three-dimensional space.That given point is the centre of the sphere, and r is the sphere's radius. The earliest known mentions of spheres appear in the work of the ancient Greek mathematicians.
+
+
+ */
