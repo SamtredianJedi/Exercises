9a9d6b51d2b32b2ea1427f2bb57e1844eb85f5d3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 20:54:11 +0400

    update2

--- JS/Programs/Introduction/task378.js
+++ JS/Programs/Introduction/task378.js
@@ -32,4 +32,28 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+const volume_Cylinder = (radius, height) => {
+  is_Number(radius, 'Radius')
+  is_Number(height, 'Height')
+  return (Math.PI * radius ** 2 * height)
+}
+
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+  } else if (n < 0 || (!Number.isFinite(n))) {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+  }
+}
+
+console.log(volume_Cylinder(2.0, 5.0))
+console.log(volume_Cylinder('2.0', 5.0))
+console.log(volume_Cylinder(2.0, -5.0))
+
+
+
+
+
  */
