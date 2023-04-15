123e9a49bbb945595533e076a3c750102772ba4d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 21:05:09 +0400

    update1

--- JS/Programs/Introduction/task381.js
+++ JS/Programs/Introduction/task381.js
@@ -11,4 +11,21 @@ From Wikipedia -
 A sphere is a geometrical object that is a three-dimensional analogue to a two-dimensional circle. A sphere is the set of points that are all at the same distance r from a given point in three-dimensional space.That given point is the centre of the sphere, and r is the sphere's radius. The earliest known mentions of spheres appear in the work of the ancient Greek mathematicians.
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Volume of a Sphere</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
