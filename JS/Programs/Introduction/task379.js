ae6ea2106393af5a7218919827a564db8dc153c2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 20:57:17 +0400

    task

---
+++ JS/Programs/Introduction/task379.js
@@ -0,0 +1,12 @@
+// JavaScript: Volume of a Triangular Prism
+
+/**
+ * 
+ * JavaScript Math: Exercise-61 with Solution
+Write a JavaScript program to calculate the volume of a Triangular Prism.
+
+From Wikipedia -
+In geometry, a triangular prism is a three-sided prism; it is a polyhedron made of a triangular base, a translated copy, and 3 faces joining corresponding sides. A right triangular prism has rectangular sides, otherwise it is oblique. A uniform triangular prism is a right triangular prism with equilateral bases, and square sides.
+
+
+ */
