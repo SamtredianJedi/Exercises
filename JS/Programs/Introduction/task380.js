37b2425a5c0b5c78a3e0c7572589b23d635e1699
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-15 21:00:50 +0400

    task

---
+++ JS/Programs/Introduction/task380.js
@@ -0,0 +1,14 @@
+// JavaScript: Volume of a Pentagonal Prism
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-62 with Solution
+Write a JavaScript program to calculate the volume of a prism using only its height and one of its hexagonal sides.
+
+In geometry, the pentagonal prism is a prism with a pentagonal base. It is a type of heptahedron with seven faces, fifteen edges, and ten vertices.
+
+
+
+ */
