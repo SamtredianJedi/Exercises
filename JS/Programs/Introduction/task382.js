b05ea6ccd2ebbc430eacdbe5cd83ea3ffcacc747
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:35:12 +0400

    task

---
+++ JS/Programs/Introduction/task382.js
@@ -0,0 +1,14 @@
+// JavaScript: Volume of a Hemisphere
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-64 with Solution
+Write a JavaScript program to calculate the volume of a Hemisphere.
+
+Hemisphere refers to a half of a sphere:
+A sphere is a geometrical object that is a three-dimensional analogue to a two-dimensional circle. A sphere is the set of points that are all at the same distance r from a given point in three-dimensional space.[2] That given point is the centre of the sphere, and r is the sphere's radius. The earliest known mentions of spheres appear in the work of the ancient Greek mathematicians.
+
+
+
+ */
