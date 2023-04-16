108a4e20d5f50382bd0e5784751224b6b9e686ee
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:36:21 +0400

    update2

--- JS/Programs/Introduction/task382.js
+++ JS/Programs/Introduction/task382.js
@@ -27,4 +27,28 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+const volume_Hemisphere = (radius) => {
+  is_Number(radius, 'Radius')
+  return (2.0 * Math.PI * radius ** 3)/3.0
+}
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+    } 
+    else if (n < 0 || (!Number.isFinite(n))) 
+    {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+    }
+}
+console.log(volume_Hemisphere(4.0))
+console.log(volume_Hemisphere('4.0'))
+console.log(volume_Hemisphere(-4.0))
+
+
  */
