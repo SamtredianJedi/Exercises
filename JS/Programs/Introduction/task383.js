fae1593413f2026722c6fa9ecf76ff6eb5a68c4c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:39:56 +0400

    update2

--- JS/Programs/Introduction/task383.js
+++ JS/Programs/Introduction/task383.js
@@ -31,4 +31,32 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function find_lucas_number(index) {  
+  // index should not be a negative number
+  if (index >= 0)
+    {
+  // index sould be an integer
+    if (Math.floor(index) !== index) throw new TypeError('Index cannot be a Decimal')
+    let x = 2
+    let y = 1
+    for (let i = 0; i < index; i++) {
+      const temp = x + y
+      x = y
+      y = temp
+    }
+    return x
+  }
+}
+console.log(find_lucas_number(0))
+console.log(find_lucas_number(4))
+console.log(find_lucas_number(7))
+console.log(find_lucas_number(5.4))
+
+
+
  */
