9dcdee1bc7bcb1a232a8d75a4a666f07e641f4d2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:38:45 +0400

    task

---
+++ JS/Programs/Introduction/task383.js
@@ -0,0 +1,17 @@
+// JavaScript: Find Lucas number from index value
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-65 with Solution
+Write a JavaScript program to get the Nth Lucas Number.
+
+From Wikipedia-
+The Lucas numbers or Lucas series are an integer sequence named after the mathematician Francois Edouard Anatole Lucas (1842-1891), who studied both that sequence and the closely related Fibonacci numbers. Lucas numbers and Fibonacci numbers form complementary instances of Lucas sequences.
+The Lucas series has the same recursive relationship as the Fibonacci sequence, where each term is the sum of the two previous terms, but with different starting values. This produces a sequence where the ratios of successive terms approach the golden ratio, and in fact the terms themselves are roundings of integer powers of the golden ratio. The sequence also has a variety of relationships with the Fibonacci numbers, like the fact that adding any two Fibonacci numbers two terms apart in the Fibonacci sequence results in the Lucas number in between.
+The first few Lucas numbers are:
+2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, 199, 322, 521, 843, 1364, 2207, 3571, 5778, 9349 .... .
+
+
+ */
