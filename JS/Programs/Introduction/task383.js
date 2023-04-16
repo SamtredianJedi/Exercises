22f3825b772fffdecec29e3319d68a77acb24e9c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:41:16 +0400

    update3

--- JS/Programs/Introduction/task383.js
+++ JS/Programs/Introduction/task383.js
@@ -59,4 +59,50 @@ console.log(find_lucas_number(5.4))
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to find Lucas number from index value</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+function find_lucas_number(index) {  
+  // index should not be a negative number
+  if (index >= 0)
+    {
+  // index sould be an integer
+    if (Math.floor(index) !== index) throw new TypeError('Index cannot be a Decimal')
+    let x = 2
+    let y = 1
+    for (let i = 0; i < index; i++) {
+      const temp = x + y
+      x = y
+      y = temp
+    }
+    return x
+  }
+}
+console.log(find_lucas_number(0))
+console.log(find_lucas_number(4))
+console.log(find_lucas_number(7))
+console.log(find_lucas_number(5.4))
+
+
+
  */
