328b124036a5d2f3b0c74646df919e9e8c8eea3e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:42:40 +0400

    task

---
+++ JS/Programs/Introduction/task384.js
@@ -0,0 +1,19 @@
+// JavaScript: Twin prime of a prime number
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-66 with Solution
+Write a JavaScript program to find the 'twin prime' of a given prime number.
+
+From Wikipedia,
+A twin prime is a prime number that is either 2 less or 2 more than another prime number-for example, either member of the twin prime pair (41, 43). In other words, a twin prime is a prime that has a prime gap of two. Sometimes the term twin prime is used for a pair of twin primes; an alternative name for this is prime twin or prime pair.
+Usually the pair (2, 3) is not considered to be a pair of twin primes. Since 2 is the only even prime, this pair is the only pair of prime numbers that differ by one; thus twin primes are as closely spaced as possible for any other two primes.
+The first few twin prime pairs are:
+(3, 5), (5, 7), (11, 13), (17, 19), (29, 31), (41, 43), (59, 61), (71, 73), (101, 103), (107, 109), (137, 139), ....
+
+Return value: Either the twin, or -1 if n or n + 2 is not prime number.
+
+
+
+ */
