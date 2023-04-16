22d6e867e88ee2f3e1f6ac56ee2ba6b9ad325814
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-16 20:31:06 +0400

    update3

--- JS/Programs/Introduction/task381.js
+++ JS/Programs/Introduction/task381.js
@@ -28,4 +28,31 @@ HTML Code:
 </html>
 
 
+JavaScript Code:
+
+const volume_Sphere = (radius) => {
+  is_Number(radius, 'Radius')
+  return (4 / 3 * Math.PI * radius ** 3)
+}
+const is_Number = (n, n_name = 'number') => {
+  if (typeof n !== 'number') {
+    throw new TypeError('The ' + n_name + ' is not Number type!')
+    } 
+    else if (n < 0 || (!Number.isFinite(n))) 
+    {
+    throw new Error('The ' + n_name + ' must be a positive values!')
+    }
+}
+console.log(volume_Sphere(4.0))
+console.log(volume_Sphere('4.0'))
+console.log(volume_Sphere(-4.0))
+
+
+
+
+
+
+
+
+
  */
