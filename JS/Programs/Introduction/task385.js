d97200d4b89777bd23fae703382b0edfbf8c8fbe
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 13:03:53 +0400

    update3

--- JS/Programs/Introduction/task385.js
+++ JS/Programs/Introduction/task385.js
@@ -63,6 +63,65 @@ JavaScript Code:
 
 
 
+/**
+ * 
+ * 
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to sum of a geometric progression</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+
+/**
+ * https://bit.ly/3gCyxWa 
+ * first_Term: The first term of the geometric progression.
+ * common_Ratio: The common ratio of the geometric progression.
+ * num_Of_Terms: The number of terms in the progression.
+ */
+// function sumOf_Geometric_Progression(first_Term, common_Ratio, num_Of_Terms) {
+//     if (!Number.isFinite(num_Of_Terms)) {
+//         /*
+//           If the number of Terms is Infinity, the common ratio needs to be less than 1 to be a convergent geometric progression
+//           Article on Convergent Series: https://en.wikipedia.org/wiki/Convergent_series
+//         */
+//         if (Math.abs(common_Ratio) < 1) return first_Term / (1 - common_Ratio)
+//         throw new Error('The geometric progression is diverging, and its sum cannot be calculated')
+//     }
+//     if (common_Ratio === 1) return first_Term * num_Of_Terms
+//     return (first_Term * (Math.pow(common_Ratio, num_Of_Terms) - 1)) / (common_Ratio - 1)
+// }
+// console.log(sumOf_Geometric_Progression(100, 2.5, 5))
+// console.log(sumOf_Geometric_Progression(0.5, 20, Infinity))
+
+
+
+
+
+
+
+*
+/
+
+
+
+
+
 
 *
 /
