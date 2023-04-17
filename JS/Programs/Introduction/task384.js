53d5bd434d0ade256a9d4a170af4b4445d0af7a0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 12:58:21 +0400

    update3

--- JS/Programs/Introduction/task384.js
+++ JS/Programs/Introduction/task384.js
@@ -79,4 +79,73 @@ console.log("Twin prime of 61 is:"+twin_Prime(61))
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to find the twin prime number</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+function twin_Prime (n) {
+  const prime_num = test_prime(n)
+
+  if (!prime_num) {
+    return -1
+  }
+  if (!test_prime(n + 2)) {
+    return -1
+  }
+
+  return n + 2
+}
+function test_prime(n)
+{
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }
+  else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+console.log("Twin prime of 5 is:"+twin_Prime(5))
+console.log("Twin prime of 59 is:"+twin_Prime(59))
+console.log("Twin prime of 107 is:"+twin_Prime(107))
+console.log("Twin prime of 61 is:"+twin_Prime(61))
+
+
+
+
+
+
+
+
  */
