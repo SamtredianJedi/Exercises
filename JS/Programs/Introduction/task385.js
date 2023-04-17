916bdbc76324991a5bbb49b27fca15548b099363
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 13:01:46 +0400

    update2

--- JS/Programs/Introduction/task385.js
+++ JS/Programs/Introduction/task385.js
@@ -32,4 +32,37 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+/**
+ * https://bit.ly/3gCyxWa 
+ * first_Term: The first term of the geometric progression.
+ * common_Ratio: The common ratio of the geometric progression.
+ * num_Of_Terms: The number of terms in the progression.
  */
+// function sumOf_Geometric_Progression(first_Term, common_Ratio, num_Of_Terms) {
+//     if (!Number.isFinite(num_Of_Terms)) {
+//         /*
+//           If the number of Terms is Infinity, the common ratio needs to be less than 1 to be a convergent geometric progression
+//           Article on Convergent Series: https://en.wikipedia.org/wiki/Convergent_series
+//         */
+//         if (Math.abs(common_Ratio) < 1) return first_Term / (1 - common_Ratio)
+//         throw new Error('The geometric progression is diverging, and its sum cannot be calculated')
+//     }
+//     if (common_Ratio === 1) return first_Term * num_Of_Terms
+//     return (first_Term * (Math.pow(common_Ratio, num_Of_Terms) - 1)) / (common_Ratio - 1)
+// }
+// console.log(sumOf_Geometric_Progression(100, 2.5, 5))
+// console.log(sumOf_Geometric_Progression(0.5, 20, Infinity))
+
+
+
+
+
+
+
+*
+/
