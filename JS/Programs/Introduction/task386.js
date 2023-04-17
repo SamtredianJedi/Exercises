2df14a130bc820e4f352ee54d8d0bc62f2ecf97a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 13:10:38 +0400

    update2

--- JS/Programs/Introduction/task386.js
+++ JS/Programs/Introduction/task386.js
@@ -29,4 +29,27 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function sum_Of_Digits(n) {
+  if (n < 0) n = -n
+  let result = 0
+
+  while (n > 0) 
+   {
+    result += n % 10
+    n = Math.floor(n / 10)
+  }
+
+  return result
+}
+console.log(sum_Of_Digits(6098))
+console.log(sum_Of_Digits(-501))
+console.log(sum_Of_Digits(2546))
+
+
+
  */
