5ddb4fa2271fc5e6d6cf8aa87bff9e57772de6fb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 13:05:27 +0400

    task

---
+++ JS/Programs/Introduction/task386.js
@@ -0,0 +1,14 @@
+// JavaScript: Sum of the digits of a number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-68 with Solution
+Write a JavaScript program to calculate the sum of the digits of a given number.
+
+In mathematics, the digit sum of a natural number in a given number base is the sum of all its digits. For example, the digit sum of the decimal number 6098 would be 6+0+9+8=23.
+
+
+
+ */
