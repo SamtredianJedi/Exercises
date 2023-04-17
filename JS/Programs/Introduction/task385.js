9ef425ed56c2871adb74ac1f74689f670334492a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-17 12:59:59 +0400

    task

---
+++ JS/Programs/Introduction/task385.js
@@ -0,0 +1,17 @@
+// JavaScript: Sum of a geometric progression
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-67 with Solution
+Write a JavaScript program to calculate the sum of a geometric progression.
+
+From Wikipedia,
+In mathematics, a geometric series is the sum of an infinite number of terms that have a constant ratio between successive terms. For example, the series
+JavaScript Math: Geometric Series.
+is geometric, because each successive term can be obtained by multiplying the previous term by 1/2. In general, a geometric series is written as a + ar + ar2 + ar3 + ..., where a is the coefficient of each term and r is the common ratio between adjacent terms. The geometric series had an important role in the early development of calculus, is used throughout mathematics, and can serve as an introduction to frequently used mathematical tools such as the Taylor series, the complex Fourier series, and the matrix exponential.
+
+
+
+
+ */
