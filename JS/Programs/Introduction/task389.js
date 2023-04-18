72cfe528b5fbd9cbc6446242cde79b17f86a85c9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:09:05 +0400

    update2

--- JS/Programs/Introduction/task389.js
+++ JS/Programs/Introduction/task389.js
@@ -26,4 +26,26 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+const Prime_Factors = (number) => {
+  const result_factors = []
+  for (let i = 2; i * i <= number; i++) {
+    while (number % i === 0) {
+      result_factors.push(i)
+      number = Math.floor(number / i)
+    }
+  }
+  if (number > 1) {
+    result_factors.push(number)
+  }
+  return result_factors
+}
+console.log(Prime_Factors(75))
+console.log(Prime_Factors(18))
+console.log(Prime_Factors(101))
+
+
  */
