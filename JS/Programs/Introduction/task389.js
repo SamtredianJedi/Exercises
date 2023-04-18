a2d5c64f25537bb39107b973597a732dd7d20ef8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:09:56 +0400

    update3

--- JS/Programs/Introduction/task389.js
+++ JS/Programs/Introduction/task389.js
@@ -48,4 +48,47 @@ console.log(Prime_Factors(18))
 console.log(Prime_Factors(101))
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to all prime factors of a given number</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+
+const Prime_Factors = (number) => {
+  const result_factors = []
+  for (let i = 2; i * i <= number; i++) {
+    while (number % i === 0) {
+      result_factors.push(i)
+      number = Math.floor(number / i)
+    }
+  }
+  if (number > 1) {
+    result_factors.push(number)
+  }
+  return result_factors
+}
+console.log(Prime_Factors(75))
+console.log(Prime_Factors(18))
+console.log(Prime_Factors(101))
+
+
+
  */
