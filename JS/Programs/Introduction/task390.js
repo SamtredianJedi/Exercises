7b46429a23517577048aa168532ccdf964db1d3d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:12:12 +0400

    task

---
+++ JS/Programs/Introduction/task390.js
@@ -0,0 +1,16 @@
+// JavaScript: Pronic number
+
+/**
+ * 
+ * 
+
+JavaScript Math: Exercise-72 with Solution
+Write a JavaScript program to check if a given number is pronic using product of two consecutive numbers. If the number is pronic return true otherwise false.
+
+From Wikipedia –
+A pronic number is a number that is the product of two consecutive integers, that is, a number of the form n(n+1). The study of these numbers dates back to Aristotle. They are also called oblong numbers, heteromecic numbers, or rectangular numbers; however, the term "rectangular number" has also been applied to the composite numbers.
+The first few pronic numbers are:
+0, 2, 6, 12, 20, 30, 42, 56, 72, 90, 110, 132, 156, 182, 210, 240, 272, 306, 342, 380, 420, 462 ...
+
+
+ */
