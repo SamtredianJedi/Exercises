707d541a08b6f4fc57a7409cc2aaa1ea893d5dae
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:01:54 +0400

    task

---
+++ JS/Programs/Introduction/task388.js
@@ -0,0 +1,15 @@
+// JavaScript: Reverse Polish notation in mathematical expression
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-70 with Solution
+Write a JavaScript program to apply Reverse Polish notation in a given mathematical expression.
+
+From Wikipedia -
+Reverse Polish notation (RPN), also known as reverse Łukasiewicz notation, Polish postfix notation or simply postfix notation, is a mathematical notation in which operators follow their operands, in contrast to Polish notation (PN), in which operators precede their operands. It does not need any parentheses as long as each operator has a fixed number of operands. The description "Polish" refers to the nationality of logician Jan Łukasiewicz, who invented Polish notation in 1924.
+
+
+
+ */
