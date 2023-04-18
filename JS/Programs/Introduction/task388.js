19038f0abf5f861bf4923db9ca34980befe69f32
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:05:04 +0400

    update3

--- JS/Programs/Introduction/task388.js
+++ JS/Programs/Introduction/task388.js
@@ -66,4 +66,67 @@ console.log(RPN_calculation("5 6 + 2 *"))
 console.log(RPN_calculation("5 3 4 * +"))
 
 
+
+
+
+
+Live Demo:
+
+
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to reverse Polish notation in mathematical expression</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+const RPN_calculation = (math_expr) => {
+  const operators = {
+    '+': (x, y) => x + y,
+    '-': (x, y) => x - y,
+    '*': (x, y) => x * y,
+    '/': (x, y) => y / x
+  }
+  const expr_part = math_expr.split(' ')
+  const data = []
+
+  expr_part.forEach((expr_part) => {
+    const operator = operators[expr_part]
+    if (typeof operator === 'function') 
+    {
+      const x = data.pop()
+      const y = data.pop()
+      const result = operator(x, y)
+      data.push(result)
+    } 
+    else 
+    {
+      data.push(parseFloat(expr_part))
+    }
+  })
+  return data.pop()
+}
+console.log(RPN_calculation("5 8 *"))
+console.log(RPN_calculation("5 6 + 2 *"))
+console.log(RPN_calculation("5 3 4 * +"))
+
+
+
+
+
+
+
+
  */
