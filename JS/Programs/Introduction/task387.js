3d30bf01e4d7b47daa585e7f56aa34fbcfcc4dea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 22:59:26 +0400

    update2

--- JS/Programs/Introduction/task387.js
+++ JS/Programs/Introduction/task387.js
@@ -27,4 +27,30 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function sieve_Of_Eratosthenes(n) {
+  const sieve = []
+  const primes_set = []
+  for (let i = 2; i <= n; ++i) {
+    if (!sieve[i]) 
+     {  
+       primes_set.push(i)
+       for (let j = i << 1; j <= n; j += i) 
+        { 
+        sieve[j] = true
+      }
+    }
+  }
+  return primes_set
+}
+console.log(sieve_Of_Eratosthenes(5))
+console.log(sieve_Of_Eratosthenes(11))
+console.log(sieve_Of_Eratosthenes(30))
+
+
+
  */
