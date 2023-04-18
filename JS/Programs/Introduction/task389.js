244e5bd1f17f3889ab76b4825393bd0a9367dd3c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-18 23:08:03 +0400

    task

---
+++ JS/Programs/Introduction/task389.js
@@ -0,0 +1,12 @@
+// JavaScript: All prime factors of a given number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-71 with Solution
+Write a JavaScript program to print all prime factors of a given number.
+
+
+
+ */
