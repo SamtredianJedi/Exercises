275e9f277f666a51e98e1d89ac76f786fe9ccad6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-19 23:40:58 +0400

    update2

--- JS/Programs/Introduction/task392.js
+++ JS/Programs/Introduction/task392.js
@@ -36,4 +36,19 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function test(n){
+    return (parseInt(n, 16).toString(2)).padStart(8, '0');
+}
+console.log(test("7B316"));
+console.log(test("6F16"));
+console.log(test("4B2A16"));
+
+
+
  */
