c79f19447f7b0b80ad9dd57dda54301612685ebd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-19 23:31:12 +0400

    update2

--- JS/Programs/Introduction/task390.js
+++ JS/Programs/Introduction/task390.js
@@ -32,4 +32,23 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+const is_Pronic = (n) => {
+  if (n === 0) {
+    return true
+  }
+  const sqrt = Math.sqrt(n)
+  return sqrt % 1 !== 0 && Math.ceil(sqrt) * Math.floor(sqrt) === n
+}
+
+console.log(is_Pronic(6))
+console.log(is_Pronic(110))
+console.log(is_Pronic(200))
+
+
+
+
+
  */
