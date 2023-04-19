514dd9aa97059b5d00983689b75e2cf3d028d245
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-19 23:34:14 +0400

    task

---
+++ JS/Programs/Introduction/task391.js
@@ -0,0 +1,17 @@
+// JavaScript: Check sum of consecutive positive integers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-73 with Solution
+Write a JavaScript program to check if a given positive integer can be expressed as sum of two or more consecutive positive integers.
+
+Sample Data:
+33 can be represented as 11 + 22
+10 = 1+2+3+4
+but 8 cannot be represented in this way.
+
+
+
+ */
