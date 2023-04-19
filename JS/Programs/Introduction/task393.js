aa6c49377bb54dfcb4f43bafac01caa04de7baea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-19 23:44:06 +0400

    task

---
+++ JS/Programs/Introduction/task393.js
@@ -0,0 +1,14 @@
+// JavaScript: Swap three numbers
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-75 with Solution
+Write a JavaScript program to swap three numbers without a third variable.
+
+Test Data:
+(100, 200, 300) -> 300, 100, 200
+
+
+
+ */
