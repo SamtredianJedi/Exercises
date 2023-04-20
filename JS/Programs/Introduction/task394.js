d8ef2db3e9ea1b4161609c876997272daac85c78
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:40:47 +0400

    update2

--- JS/Programs/Introduction/task394.js
+++ JS/Programs/Introduction/task394.js
@@ -30,4 +30,27 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function test1(a, b)
+{
+    do 
+    {
+        let temp = (~a) & b;
+         a = a ^ b;
+         b = temp << 1;
+    }while (b != 0)
+    return a;
+}
+
+console.log(test1(200, 100));
+console.log(test1(200, 300));
+console.log(test1(-200, -300));
+console.log(test1(200, 200));
+
+
+
  */
