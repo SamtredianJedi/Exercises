92133661465a18423a10cb39e81c9ce6d751c343
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:41:51 +0400

    update3

--- JS/Programs/Introduction/task394.js
+++ JS/Programs/Introduction/task394.js
@@ -53,4 +53,45 @@ console.log(test1(200, 200));
 
 
 
+
+Live Demo:
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Subtract without arithmetic operators</title>
+</head>
+<body>
+
+</body>
+</html>
+
+js:
+
+function test1(a, b)
+{
+    do 
+    {
+        let temp = (~a) & b;
+         a = a ^ b;
+         b = temp << 1;
+    }while (b != 0)
+    return a;
+}
+
+console.log(test1(200, 100));
+console.log(test1(200, 300));
+console.log(test1(-200, -300));
+console.log(test1(200, 200));
+
+
+
+
+
+
+
  */
