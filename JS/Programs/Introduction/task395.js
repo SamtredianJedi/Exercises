640fb3e73bbe7da6673f622c50b5b22a480f0233
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:43:49 +0400

    task

---
+++ JS/Programs/Introduction/task395.js
@@ -0,0 +1,14 @@
+// JavaScript: Two positive integers as string, return their sum
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-77 with Solution
+Write a JavaScript program to given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Test Data:
+("123", "123") -> 246
+("45", "100") -> 145
+
+
+ */
