eb1a0a0d3bfc6b701be7948ae1c7ce58b51580aa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:34:56 +0400

    update3

--- JS/Programs/Introduction/task393.js
+++ JS/Programs/Introduction/task393.js
@@ -59,4 +59,57 @@ console.log("z = "+result[2]);
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Swap three numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+function test(x, y, z)
+{
+    x = x + y + z;
+    y = x - (y + z);
+    z = x - (y + z);
+    x = x - (y + z);
+    return [x, y, z];  
+}
+ 
+x = 100; 
+y = 200; 
+z = 300;
+console.log("Before swapping:")
+console.log("x = "+x);
+console.log("y = "+y);
+console.log("z = "+z);
+result = test(x, y, z);
+console.log("After swapping:");
+console.log("x = "+result[0]);
+console.log("y = "+result[1]);
+console.log("z = "+result[2]);
+
+
+
+
+
+
+
+
+
+
  */
