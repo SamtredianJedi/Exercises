4ceea419582794e3ad73dec9fce2a723236feecb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:38:01 +0400

    task

---
+++ JS/Programs/Introduction/task394.js
@@ -0,0 +1,17 @@
+// JavaScript: Subtract without arithmetic operators
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-76 with Solution
+Write a JavaScript program to subtract one integer from another, without using any arithmetic operators such as -, %, /, +, etc.
+
+Test Data:
+(200, 100) -> 100
+(200, 300) -> -100
+(-200, -300) -> 100
+(200, 200) -> 0
+
+
+ */
