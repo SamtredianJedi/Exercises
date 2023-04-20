52cf2097516cc771413eb48fee7f63ad79e8ecca
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:49:53 +0400

    task

---
+++ JS/Programs/Introduction/task396.js
@@ -0,0 +1,25 @@
+// JavaScript: Nth Tetrahedral Number
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-78 with Solution
+Write a JavaScript program that takes an integer n and return the nth Tetrahedral number.
+
+A tetrahedral number, or triangular pyramidal number, is a figurate number that represents a pyramid with a triangular base and three sides, called a tetrahedron. The formula for the nth tetrahedral number is represented by the 3rd rising factorial of n divided by the factorial of 3:
+
+
+
+The tetrahedral numbers are:
+1, 4, 10, 20, 35, 56, 84, 120, 165, 220, ...
+Test Data:
+(1) -> 1
+(2) -> 4
+(3) -> 10
+(4) -> 20
+(5) -> 35
+
+
+ */
