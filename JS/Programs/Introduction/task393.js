5ef6e71d026a5cbf24611960149d7303023e12c8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:33:42 +0400

    update2

--- JS/Programs/Introduction/task393.js
+++ JS/Programs/Introduction/task393.js
@@ -29,4 +29,34 @@ HTML Code:
 </html>
 
 
+
+
+JavaScript Code:
+
+function test(x, y, z)
+{
+    x = x + y + z;
+    y = x - (y + z);
+    z = x - (y + z);
+    x = x - (y + z);
+    return [x, y, z];  
+}
+ 
+x = 100; 
+y = 200; 
+z = 300;
+console.log("Before swapping:")
+console.log("x = "+x);
+console.log("y = "+y);
+console.log("z = "+z);
+result = test(x, y, z);
+console.log("After swapping:");
+console.log("x = "+result[0]);
+console.log("y = "+result[1]);
+console.log("z = "+result[2]);
+
+
+
+
+
  */
