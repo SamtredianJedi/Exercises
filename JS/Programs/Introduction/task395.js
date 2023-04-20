d09439219c8bbe586cfb16983688c418642261e1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-20 18:47:38 +0400

    update3

--- JS/Programs/Introduction/task395.js
+++ JS/Programs/Introduction/task395.js
@@ -62,4 +62,60 @@ console.log(test("45", "100"));
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Two positive integers as string, return their sum</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+js:
+
+
+
+
+function test(n1, n2) {
+    var str = '';
+    var carry = 0;
+    var num1 = n1.length -1;
+    var num2 = n2.length -1;
+    var sum_of_two
+​
+    while(num1 >= 0 || num2 >= 0){
+        sum_of_two = (parseInt(n1[num1]) || 0) + (parseInt(n2[num2]) || 0) 
+        sum_of_two += carry ? 1:0
+        
+        if(sum_of_two > 9){
+            carry = 1;
+            str = sum_of_two %10 + str 
+        }else{
+            str = sum_of_two  + str;
+            carry = 0
+        }
+        num1--;
+        num2--
+    }
+    if(carry){str = 1+ str}
+    return str
+};
+console.log(test("123", "123"));
+console.log(test("45", "100"));
+​
+
+
+
  */
