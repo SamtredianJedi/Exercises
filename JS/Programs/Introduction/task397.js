f05aee34a889d21c47aa3eb1c842ba9f6c25b055
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:02:40 +0400

    update2

--- JS/Programs/Introduction/task397.js
+++ JS/Programs/Introduction/task397.js
@@ -32,4 +32,30 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+ return n * (n + 1) / 2;
+}
+n = 1
+console.log("Triangular number = "+n)
+console.log("Number of dots based on the triangular number of the sequence = "+test(n));
+n = 2
+console.log("Triangular number = "+n)
+console.log("Number of dots based on the triangular number of the sequence = "+test(n));
+n = 3
+console.log("Triangular number = "+n)
+console.log("Number of dots based on the triangular number of the sequence = "+test(n));
+n = 7
+console.log("Triangular number = "+n)
+console.log("Number of dots based on the triangular number of the sequence = "+test(n));
+n = 11
+console.log("Triangular number = "+n)
+console.log("Number of dots based on the triangular number of the sequence = "+test(n));
+
+
+
  */
