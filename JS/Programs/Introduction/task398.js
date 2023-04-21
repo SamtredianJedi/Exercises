68d861920c716f84646dbf2cadc14365d906d5aa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:05:23 +0400

    task

---
+++ JS/Programs/Introduction/task398.js
@@ -0,0 +1,14 @@
+// JavaScript: Check an integer is Repdigit number or not
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-80 with Solution
+Write a JavaScript program to check whether a given integer is a Repdigit or not.
+
+From Wikipedia -
+In recreational mathematics, a repdigit or sometimes monodigit is a natural number composed of repeated instances of the same digit in a positional number system (often implicitly decimal). The word is a portmanteau of repeated and digit. Examples are 11, 666, 4444, and 999999.
+
+
+
+ */
