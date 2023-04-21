dc88960acc0abdbdc932d602377006e08366f1ed
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 22:58:47 +0400

    update2

--- JS/Programs/Introduction/task396.js
+++ JS/Programs/Introduction/task396.js
@@ -40,4 +40,30 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+return n*(n+1)*(n+2)/6
+}
+n = 1
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 2
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 3
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 4
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 5
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+
+
+
  */
