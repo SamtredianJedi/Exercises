995b16437ee92eeae7281e59ce9b164dc1fce398
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 22:59:38 +0400

    update3

--- JS/Programs/Introduction/task396.js
+++ JS/Programs/Introduction/task396.js
@@ -66,4 +66,47 @@ console.log("nth Tetrahedral number = "+test(n));
 
 
 
+
+Live Demo:
+
+html:
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Nth Tetrahedral Number</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+js:
+
+
+
+function test(n) {
+return n*(n+1)*(n+2)/6
+}
+n = 1
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 2
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 3
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 4
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+n = 5
+console.log("n = "+n)
+console.log("nth Tetrahedral number = "+test(n));
+
+
+
+
  */
