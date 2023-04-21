223fb1f983f12753dba58c5f0a2f6608f81644f5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:06:28 +0400

    update2

--- JS/Programs/Introduction/task398.js
+++ JS/Programs/Introduction/task398.js
@@ -30,4 +30,31 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+	return new Set('' + n).size === 1
+}
+
+n = 11
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 66
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 336
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 444
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 1151
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+
+
+
  */
