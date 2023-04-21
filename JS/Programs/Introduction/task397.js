01464ac1031fb3e5bc144eb21861aaca5e780572
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:01:34 +0400

    task

---
+++ JS/Programs/Introduction/task397.js
@@ -0,0 +1,17 @@
+// JavaScript: Sequence number from the given Triangular Number
+
+
+
+/**
+ * 
+
+JavaScript Math: Exercise-79 with Solution
+Write a JavaScript program program to get the number of dots based on the triangular number of the sequence.
+
+From Wikipedia -
+A triangular number or triangle number counts objects arranged in an equilateral triangle. Triangular numbers are a type of figurate number, other examples being square numbers and cube numbers. The nth triangular number is the number of dots in the triangular arrangement with n dots on each side, and is equal to the sum of the n natural numbers from 1 to n. The sequence of triangular numbers, starting with the 0th triangular number, is
+0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210, 231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496, 528, 561, 595, 630, ...
+
+
+
+ */
