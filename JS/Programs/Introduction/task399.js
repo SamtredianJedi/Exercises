402151e6f762e91785fc1054e9a279c7bb428925
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:10:20 +0400

    task

---
+++ JS/Programs/Introduction/task399.js
@@ -0,0 +1,18 @@
+// JavaScript: Multiply every digit of a number three times
+
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-81 with Solution
+Write a JavaScript program to multiply every digit of a number three times.
+
+Test Data:
+(11) -> 11
+(66) -> 216216
+(336) -> 2727216
+(444) -> 646464
+(1151) -> 111251
+
+
+ */
