1da56aa8e80932bc110ba7ce4dc3d8aababb4ff3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-21 23:07:15 +0400

    update3

--- JS/Programs/Introduction/task398.js
+++ JS/Programs/Introduction/task398.js
@@ -57,4 +57,54 @@ console.log("Check the said number is Repdigit: "+test(n));
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Check an integer is Repdigit number or not</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+
+function test(n) {
+	return new Set('' + n).size === 1
+}
+
+n = 11
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 66
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 336
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 444
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+n = 1151
+console.log("n = "+n)
+console.log("Check the said number is Repdigit: "+test(n));
+
+
+
+
  */
