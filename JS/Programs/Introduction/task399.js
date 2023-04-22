d47545301179d2c47eb7969275aef1016a4dc26c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:28:22 +0400

    update1

--- JS/Programs/Introduction/task399.js
+++ JS/Programs/Introduction/task399.js
@@ -15,4 +15,25 @@ Test Data:
 (1151) -> 111251
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to multiply every digit of a number three times</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
+
  */
