a25f89db2f60c74e6547fc052bd3c4a32e61e2df
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:34:36 +0400

    task

---
+++ JS/Programs/Introduction/task400.js
@@ -0,0 +1,17 @@
+// JavaScript: Mean of all digits of a number
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-82 with Solution
+Write a JavaScript program to calculate the mean of all the digits of a given number.
+
+What is a Mean?
+In mathematics, the mean represents the simple average of two or more numbers. There are several methods available for computing the mean of a set of numbers, including the arithmetic mean method, which uses the sum of the numbers in the series, and the geometric mean method, which is the average of a set of products.
+
+
+
+ */
