54f29e4a38be1aae5f941576491156d6969ae75b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:40:55 +0400

    update2

--- JS/Programs/Introduction/task401.js
+++ JS/Programs/Introduction/task401.js
@@ -25,4 +25,25 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(nums) {
+  for (let n = 1; n<= nums.length + 1; n++) {
+    if (nums.indexOf(n) === -1) 
+      return n;
+  }
+}
+
+nums = [1,2,3,5,6,7]
+console.log("nums = "+nums)
+console.log("Missing number of the said array: "+test(nums));
+nums = [2,3,4,5]
+console.log("nums= "+nums)
+console.log("Missing number of the said array: "+test(nums));
+
+
+
  */
