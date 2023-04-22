359af694840f4a623b5cc9642c66fc09be6cb2c3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:41:58 +0400

    update3

--- JS/Programs/Introduction/task401.js
+++ JS/Programs/Introduction/task401.js
@@ -46,4 +46,47 @@ console.log("Missing number of the said array: "+test(nums));
 
 
 
+
+
+Live Demo:
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to missing number from an array</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+function test(nums) {
+  for (let n = 1; n<= nums.length + 1; n++) {
+    if (nums.indexOf(n) === -1) 
+      return n;
+  }
+}
+
+nums = [1,2,3,5,6,7]
+console.log("nums = "+nums)
+console.log("Missing number of the said array: "+test(nums));
+nums = [2,3,4,5]
+console.log("nums= "+nums)
+console.log("Missing number of the said array: "+test(nums));
+
+
+
+
+
+
+
+
  */
