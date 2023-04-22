3cf6755bbdcb06728eaf3b6fe491ca96ed9782cf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:43:41 +0400

    task

---
+++ JS/Programs/Introduction/task402.js
@@ -0,0 +1,16 @@
+// JavaScript: Sum of the Two highest Numbers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-84 with Solution
+Write a JavaScript program to calculate the sum of the two highest positive numbers in a given array.
+
+Test Data:
+([1,2,6,3,4,5,6,7]) ->13
+([2,3,4,5]) -> 9
+
+
+
+ */
