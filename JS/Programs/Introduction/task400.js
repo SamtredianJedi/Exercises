84e6b3549e02d0fcd25d3a27696a37c27c28acd3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:36:27 +0400

    update2

--- JS/Programs/Introduction/task400.js
+++ JS/Programs/Introduction/task400.js
@@ -29,4 +29,33 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+function test(n) {
+	let arr_str = String(Math.abs(n)).split('');
+	return arr_str.reduce((a,b)=>a+Number(b),0)/arr_str.length;
+}
+
+n = 11
+console.log("n = "+n)
+console.log("Mean of all digits of the said number: "+test(n));
+n = 66
+console.log("n = "+n)
+console.log("Mean of all digits of the said number: "+test(n));
+n = 336
+console.log("n = "+n)
+console.log("Mean of all digits of the said number: "+test(n));
+n = 444
+console.log("n = "+n)
+console.log("Mean of all digits of the said number: "+test(n));
+n = 1151
+console.log("n = "+n)
+console.log("Mean of all digits of the said number: "+test(n));
+
+
+
  */
