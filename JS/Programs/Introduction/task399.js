83e4a67aa1d1949159fd6d30313fd2e7f350ef16
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-22 16:30:48 +0400

    update2

--- JS/Programs/Introduction/task399.js
+++ JS/Programs/Introduction/task399.js
@@ -33,6 +33,30 @@ HTML Code:
 
 
 
+JavaScript Code:
+
+function test(n) {
+	return +[...String(n)].map(x => x*x*x).join('');
+}
+n = 11
+console.log("n = "+n)
+console.log("Multiply every digit of the said number three times: "+test(n));
+n = 66
+console.log("n = "+n)
+console.log("Multiply every digit of the said number three times: "+test(n));
+n = 336
+console.log("n = "+n)
+console.log("Multiply every digit of the said number three times: "+test(n));
+n = 444
+console.log("n = "+n)
+console.log("Multiply every digit of the said number three times: "+test(n));
+n = 1151
+console.log("n = "+n)
+console.log("Multiply every digit of the said number three times: "+test(n));
+
+
+
+
 
 
 
