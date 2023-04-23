7d608a0d64060e3352e47f27947fc98c40431e8b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:34:07 +0400

    task

---
+++ JS/Programs/Introduction/task405.js
@@ -0,0 +1,18 @@
+// JavaScript: Check a number is Sastry number or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-87 with Solution
+Write a JavaScript program that checks whether a number (positive integer) is a Sastry number or not.
+
+The number N is a Sastry number when it is concatenated with N+1 and gives a perfect squares. Some Sastry numbers are 183, 328, 528, 715, 6099, 13224, 40495, 106755, 453288, 2066115, 2975208, 22145328, 28027683, 110213248...
+
+Test Data:
+(27) -> false
+(328) -> true
+(28027683) -> true
+
+
+ */
