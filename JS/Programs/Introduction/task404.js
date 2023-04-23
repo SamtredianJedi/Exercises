5787901f2ff8c8b5297b279852549b7b841a2afa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:26:27 +0400

    update2

--- JS/Programs/Introduction/task404.js
+++ JS/Programs/Introduction/task404.js
@@ -31,4 +31,33 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+function test(text) {
+  var text_len = text.length;
+  if (text_len % 2 != 0) 
+  {
+    let start = (text_len - 1) / 2;
+    return text.slice(start, start + 1)
+  } 
+  else 
+  {
+    let start = text_len / 2 - 1;
+    return text.slice(start, start + 2);
+  }
+ }
+text = "abcd"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+text = "abc"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+text = "JavaScript"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+
+
+
  */
