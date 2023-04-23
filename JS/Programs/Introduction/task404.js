7fc822fa9a0a4a5d6e2a84d7cf9975651435842f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:31:38 +0400

    update3

--- JS/Programs/Introduction/task404.js
+++ JS/Programs/Introduction/task404.js
@@ -60,4 +60,55 @@ console.log("Middle character(s) of the said string: "+test(text))
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Middle character(s) of a string</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+function test(text) {
+  var text_len = text.length;
+  if (text_len % 2 != 0) 
+  {
+    let start = (text_len - 1) / 2;
+    return text.slice(start, start + 1)
+  } 
+  else 
+  {
+    let start = text_len / 2 - 1;
+    return text.slice(start, start + 2);
+  }
+ }
+text = "abcd"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+text = "abc"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+text = "JavaScript"
+console.log("Original string: "+text);
+console.log("Middle character(s) of the said string: "+test(text))
+
+
+
+
+
+
+
  */
