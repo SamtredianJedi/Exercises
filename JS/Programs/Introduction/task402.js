d5d787db65673d73ecc62ecbc69f6d90456d5028
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:19:28 +0400

    update3

--- JS/Programs/Introduction/task402.js
+++ JS/Programs/Introduction/task402.js
@@ -51,4 +51,45 @@ console.log("Sum of the two highest numbers of the said array: "+test(nums));
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Sum of the Two highest Numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+
+function test(nums) {
+  return nums.filter((el) => el >= 0)
+    .sort((x, y) => x - y)
+    .slice(-2)
+    .reduce((acc, el) => acc + el);
+}
+nums = [1,2,6,3,4,5,6,7]
+console.log("nums = "+nums)
+console.log("Sum of the two highest numbers of the said array: "+test(nums));
+nums = [2,3,4,5]
+console.log("nums = "+nums)
+console.log("Sum of the two highest numbers of the said array: "+test(nums));
+
+
+
+
+
  */
