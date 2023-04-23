00657daa8deb0f913b9a4be2d8afa2b686bb0759
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:20:59 +0400

    task

---
+++ JS/Programs/Introduction/task403.js
@@ -0,0 +1,15 @@
+// JavaScript: Sum of the main diagonal elements of a square matrix
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-85 with Solution
+Write a JavaScript program to compute the sum of the main diagonal elements of a square matrix.
+
+In mathematics, a matrix (plural matrices) is a rectangular array or table of numbers, symbols, or expressions, arranged in rows and columns, which is used to represent a mathematical object or a property of such an object.
+An m × n matrix: the m rows are horizontal and the n columns are vertical
+
+
+ */
