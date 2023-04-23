92e8d8b3ff953fc969dc9565126ef8826399e090
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:22:40 +0400

    update2

--- JS/Programs/Introduction/task403.js
+++ JS/Programs/Introduction/task403.js
@@ -30,4 +30,26 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(nums) {
+    return nums.reduce((x,y,i)=>x+y[i],0);
+}
+nums = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+       ]
+console.log("Sum of the main diagonal elements of the said square matrix: "+test(nums));
+nums = [
+        [-1, -2],
+        [-4, -5]
+       ]
+console.log("Sum of the main diagonal elements of the said square matrix: "+test(nums));
+
+
+
  */
