f27a81354a7e03054f812a2f3ae9af09cea9c8a8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:18:08 +0400

    update2

--- JS/Programs/Introduction/task402.js
+++ JS/Programs/Introduction/task402.js
@@ -31,4 +31,24 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function test(nums) {
+  return nums.filter((el) => el >= 0)
+    .sort((x, y) => x - y)
+    .slice(-2)
+    .reduce((acc, el) => acc + el);
+}
+nums = [1,2,6,3,4,5,6,7]
+console.log("nums = "+nums)
+console.log("Sum of the two highest numbers of the said array: "+test(nums));
+nums = [2,3,4,5]
+console.log("nums = "+nums)
+console.log("Sum of the two highest numbers of the said array: "+test(nums));
+
+
+
  */
