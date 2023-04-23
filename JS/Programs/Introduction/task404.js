0d4e63353d7b12e7252314449b5fed84dccad50d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-23 23:25:09 +0400

    task

---
+++ JS/Programs/Introduction/task404.js
@@ -0,0 +1,17 @@
+// JavaScript: Middle character(s) of a string
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-86 with Solution
+Write a JavaScript program to get the middle character(s) from a given string.
+
+Test Data:
+("abcd") -> "bc"
+("abc") -> "b"
+("JavaScript") -> "Sc"
+
+
+
+ */
