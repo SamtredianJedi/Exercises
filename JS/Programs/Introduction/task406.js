d5c446fd3ac29922a89617f50f9d30fabd38b729
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:44:38 +0400

    task

---
+++ JS/Programs/Introduction/task406.js
@@ -0,0 +1,16 @@
+// JavaScript: Check to given vectors are orthogonal or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-88 with Solution
+Write a JavaScript program to check whether two vectors are orthogonal or not.
+
+Two vectors are orthogonal if they are perpendicular to each other. i.e. the dot product of the two vectors is zero.
+Let u and v are two vectors. The vectors u, v will be orthogonal if they are perpendicular, i.e., they form a right angle, or if the dot product they yield is zero.
+u⊥v or u•v=0
+
+
+
+ */
