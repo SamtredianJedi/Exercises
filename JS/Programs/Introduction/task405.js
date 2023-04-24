9e1a1b2549436f0c89e7b72ffa0d831eea62aca2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:42:13 +0400

    update2

--- JS/Programs/Introduction/task405.js
+++ JS/Programs/Introduction/task405.js
@@ -32,4 +32,24 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(n){
+	return Number.isInteger(Math.sqrt(`${n}${n + 1}`))
+}
+n = 27
+console.log("Original string: "+n);
+console.log("Check the said number is Sastry number or not: "+test(n))
+n = 328
+console.log("Original string: "+n);
+console.log("Check the said number is Sastry number or not: "+test(n))
+n = 28027683
+console.log("Original string: "+n);
+console.log("Check the said number is Sastry number or not: "+test(n))
+
+
+
  */
