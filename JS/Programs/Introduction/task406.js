ca6c3eb35fbeb97e3bbe4a10d1f705bb75d298e4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:46:19 +0400

    update2

--- JS/Programs/Introduction/task406.js
+++ JS/Programs/Introduction/task406.js
@@ -31,4 +31,34 @@ HTML Code:
 
 
 
+
+
+JavaScript Code:
+
+function test(nums1, nums2) {
+  var total = 0
+  for(var i=0;i<nums1.length;i++){
+    total+=(nums1[i] *nums2[i]);
+  }
+  return total ==0;
+}
+
+nums1 = [2, 4, 1]
+nums2 = [2, 1, -8]
+console.log("Original vectors:")
+console.log(nums1+"\n"+nums2)
+console.log("Check the said vectors are orthogonal or not: "+test(nums1, nums2))
+nums1 = [1, 0, 1]
+nums2 = [0, 0, 1]
+console.log("Original vectors:")
+console.log(nums1+"\n"+nums2)
+console.log("Check the said vectors are orthogonal or not: "+test(nums1, nums2))
+nums1 = [1, 0, 0]
+nums2 = [0, 1, 0]
+console.log("Original vectors:")
+console.log(nums1+"\n"+nums2)
+console.log("Check the said vectors are orthogonal or not: "+test(nums1, nums2))
+
+
+
  */
