37697f157bff7f8f17af273443778dce996b11bf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:50:39 +0400

    task

---
+++ JS/Programs/Introduction/task407.js
@@ -0,0 +1,11 @@
+// JavaScript: Sum of oddly divisible numbers from a range
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-89 with Solution
+Write a JavaScript program that takes three arguments x, y, n and calculates the sum of the numbers oddly divided by n from the range x, y inclusive.
+
+
+
+ */
