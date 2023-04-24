9e32c6063fddc1d3615ea19e1da54c7f7955a384
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:51:34 +0400

    update1

--- JS/Programs/Introduction/task407.js
+++ JS/Programs/Introduction/task407.js
@@ -8,4 +8,21 @@ Write a JavaScript program that takes three arguments x, y, n and calculates the
 
 
 
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Sum of oddly divisible numbers from a range</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
