743055d9ae2a3528dc1faf451fdbf4356407719f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:53:08 +0400

    update3

--- JS/Programs/Introduction/task407.js
+++ JS/Programs/Introduction/task407.js
@@ -50,4 +50,47 @@ console.log("Divisible number: "+n)
 console.log("Sum of oddly divisible numbers from a the said range: "+test(x, y, n))
 
 
+Live Demo:
+
+
+html:
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Sum of oddly divisible numbers from a range</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+
+function test(x,y,n) {
+	let num_sum = 0;
+	for (let i = x; i <= y; i++) {
+		num_sum += i % n === 0 ? i : 0;
+	}
+  return num_sum
+}
+x = 1
+y = 5
+n = 3
+console.log("Range: "+x+"-"+y)
+console.log("Divisible number: "+n)
+console.log("Sum of oddly divisible numbers from a the said range: "+test(x, y, n))
+x = 100
+y = 1000
+n = 5
+console.log("Range: "+x+"-"+y)
+console.log("Divisible number: "+n)
+console.log("Sum of oddly divisible numbers from a the said range: "+test(x, y, n))
+
+
+
  */
