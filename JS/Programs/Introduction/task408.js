87507ec0a176c15b6f35d2aaf632d39a8394c8a0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-24 15:55:42 +0400

    task

---
+++ JS/Programs/Introduction/task408.js
@@ -0,0 +1,14 @@
+// JavaScript: Test if a number is a Harshad Number or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-90 with Solution
+Write a JavaScript program to check whether a number is a Harshad Number or not.
+
+In recreational mathematics, a harshad number in a given number base, is an integer that is divisible by the sum of its digits when written in that base.
+Example: Number 200 is a Harshad Number because the sum of digits 2 and 0 and 0 is 2 (2+0+0) and 200 is divisible by 2. Number 171 is a Harshad Number because the sum of digits 1 and 7 and 1 is 9(1+7+1) and 171 is divisible by 9.
+
+
+ */
