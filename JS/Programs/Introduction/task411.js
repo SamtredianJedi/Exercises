ac3933af77c683b12a5e744acdb62a122c7d745e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:50:02 +0400

    task

---
+++ JS/Programs/Introduction/task411.js
@@ -0,0 +1,16 @@
+// JavaScript: Check downward trend, array of integers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-93 with Solution
+Write a JavaScript program to check if an array of integers has a downward trend or not.
+
+Test Data:
+([1, 3, 4, 7, 9, 10, 11]) -> false
+([11, 10, 9, 7, 4, 3, 2, 0]) -> true
+([1, 0, -2, -3, -12]) -> true
+
+
+ */
