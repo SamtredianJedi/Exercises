da5b719e9c1af5d4b4b321874defbdf8a4a6ab9e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:45:55 +0400

    task

---
+++ JS/Programs/Introduction/task410.js
@@ -0,0 +1,15 @@
+// JavaScript: Iterated Cube Root
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-92 with Solution
+Write a Python program that takes a positive integer and calculates the cube root of the number until it is less than three. Return the number of steps to complete this process.
+
+Test Data:
+(27) -> 2
+(10000) -> 2
+(-100) -> "Not a positive number!"
+
+
+ */
