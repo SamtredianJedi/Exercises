2e74e3044059855f71c0780fb278f906029ff3cd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:42:32 +0400

    task

---
+++ JS/Programs/Introduction/task409.js
@@ -0,0 +1,12 @@
+// JavaScript: Sum of all odds in a matrix
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-91 with Solution
+Write a JavaScript program to calculate the sum of all odd elements in a square matrix.
+
+
+
+ */
