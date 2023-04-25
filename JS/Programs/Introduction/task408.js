5d10c065f282b30a2378d0e2f382cba14317b656
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:38:33 +0400

    update1

--- JS/Programs/Introduction/task408.js
+++ JS/Programs/Introduction/task408.js
@@ -11,4 +11,23 @@ In recreational mathematics, a harshad number in a given number base, is an inte
 Example: Number 200 is a Harshad Number because the sum of digits 2 and 0 and 0 is 2 (2+0+0) and 200 is divisible by 2. Number 171 is a Harshad Number because the sum of digits 1 and 7 and 1 is 9(1+7+1) and 171 is divisible by 9.
 
 
+
+
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Test if a number is a Harshad Number or not</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
