7e213014b8f7d16d50163a032912a4d4d0af1dcd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:39:36 +0400

    update2

--- JS/Programs/Introduction/task408.js
+++ JS/Programs/Introduction/task408.js
@@ -30,4 +30,35 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+  n = n.toString();
+  var total = 0;
+  for (var i=0; i<n.length; i++)
+  {
+    total+= parseInt(n[i]);
+  }
+  n = parseInt(n);
+  if (n%total===0)
+  {
+    return true;
+  }
+  return false;
+}
+n = 113
+console.log("n = "+n)
+console.log("Test the said number is Harshad Number or not! "+test(n))
+n = 171
+console.log("n = "+n)
+console.log("Test the said number is Harshad Number or not! "+test(n))
+n = 200
+console.log("n = "+n)
+console.log("Test the said number is Harshad Number or not! "+test(n))
+
+
+
  */
