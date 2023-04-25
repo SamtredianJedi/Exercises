a85610cf647c9c066ff8e8c749b3380e69a7e48d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:44:38 +0400

    update3

--- JS/Programs/Introduction/task409.js
+++ JS/Programs/Introduction/task409.js
@@ -46,4 +46,43 @@ console.log("Sum of all odds of the said matrix: "+test(nums));
 
 
 
+
+
+
+Live Demo:
+
+html:
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Sum of all odds in a matrix</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+js:
+
+
+
+function test(nums) {
+    return nums.reduce((b,a) => [...b,...a], []).reduce((b,a) => !(a%2) ? b: a+b , 0)
+}
+nums = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+       ]
+console.log("Sum of all odds of the said matrix: "+test(nums));
+nums = [
+        [-1, -2],
+        [-4, -5]
+       ]
+console.log("Sum of all odds of the said matrix: "+test(nums));
+​
+
+
  */
