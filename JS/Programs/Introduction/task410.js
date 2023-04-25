ed8ce2d88c274c7c1446e5938c5b91b3715a5298
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-25 23:48:18 +0400

    update3

--- JS/Programs/Introduction/task410.js
+++ JS/Programs/Introduction/task410.js
@@ -57,4 +57,55 @@ console.log("Number of steps to complete the said process: "+test(n));
 
 
 
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Iterated Cube Root</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+function test(n)
+  {
+	ctr = 0
+	while(n >= 3)
+      {
+	  n =  n ** (1./3.)
+	  ctr = ctr + 1
+      }
+      if (n < 0)
+        return 'Not a positive number!';
+        else 
+          return ctr;
+      } 
+console.log("Iterated Cube Root:");
+n = 27
+console.log("n = "+n)
+console.log("Number of steps to complete the said process: "+test(n));
+n = 10000
+console.log("n = "+n)
+console.log("Number of steps to complete the said process: "+test(n));
+n = -100
+console.log("n = "+n)
+console.log("Number of steps to complete the said process: "+test(n));
+
+
+
+
+
+
+
+
  */
