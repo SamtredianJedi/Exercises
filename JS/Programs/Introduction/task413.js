0e7ef4ddb5ead6dcd50605ede4977c07c115d0f5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:34:49 +0400

    update2

--- JS/Programs/Introduction/task413.js
+++ JS/Programs/Introduction/task413.js
@@ -31,4 +31,28 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function test(text) {
+return text
+    .split(" ")
+    .map((word) =>
+      !(word.length % 2) ? word[word.length-1] : word[Math.floor(word.length / 2)]
+    )
+    .join(", ");
+}
+text = "Number of even numbers and their sum"
+console.log("Original string: "+text)
+result = test(text)
+console.log("Number of even numbers and their sum: "+result);
+text = "JavaScript Math and Numbers- Exercises Practice Solution"
+console.log("Original string: "+text)
+result = test(text)
+console.log("Number of even numbers and their sum: "+result);
+
+
+
 */
