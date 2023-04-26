8aebd37e544217d2474c80f8cee697fc4a578d0b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:35:59 +0400

    update3

--- JS/Programs/Introduction/task413.js
+++ JS/Programs/Introduction/task413.js
@@ -55,4 +55,49 @@ console.log("Number of even numbers and their sum: "+result);
 
 
 
+
+
+Live Demo:
+
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Last and middle character from a string</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+js:
+
+
+
+function test(text) {
+return text
+    .split(" ")
+    .map((word) =>
+      !(word.length % 2) ? word[word.length-1] : word[Math.floor(word.length / 2)]
+    )
+    .join(", ");
+}
+text = "Number of even numbers and their sum"
+console.log("Original string: "+text)
+result = test(text)
+console.log("Number of even numbers and their sum: "+result);
+text = "JavaScript Math and Numbers- Exercises Practice Solution"
+console.log("Original string: "+text)
+result = test(text)
+console.log("Number of even numbers and their sum: "+result);
+
+
+
 */
