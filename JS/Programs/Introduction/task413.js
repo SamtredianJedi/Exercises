9d7565789f28ec5382e1c4928cd537047b6bfb65
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:33:04 +0400

    task

---
+++ JS/Programs/Introduction/task413.js
@@ -0,0 +1,16 @@
+// JavaScript: Last and middle character from a string
+
+/** 
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-95 with Solution
+Write a JavaScript program that takes text and returns the last character of each word if it is even in length. It also returns the middle character if it is odd in length.
+
+Test Data:
+("Number of even numbers and their sum") -> "r, f, n, b, n, e, u"
+("JavaScript Math and Numbers- Exercises Practice Solution") -> "t, h, n, -, c, e, n"
+
+
+
+*/
