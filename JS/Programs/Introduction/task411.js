7904ef550751278a930d4bb1abf3035deceeb0b9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:27:10 +0400

    update3

--- JS/Programs/Introduction/task411.js
+++ JS/Programs/Introduction/task411.js
@@ -52,4 +52,49 @@ console.log(" process: "+test(nums))
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Check downward trend, array of integers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+js:
+
+
+
+function test(nums) {
+ return nums.every((item,i)=> item > nums[i+1] || item === nums[nums.length -1])
+}
+nums = [1, 3, 4, 7, 9, 10, 11]
+console.log("n = "+nums)
+console.log(" process: "+test(nums));
+nums = [11, 10, 9, 7, 4, 3, 2, 0]
+console.log("n = "+nums)
+console.log(" process: "+test(nums));
+nums = [1, 0, -2, -3, -12]
+console.log("n = "+nums)
+console.log(" process: "+test(nums))
+
+
+
+
+
+
+
  */
