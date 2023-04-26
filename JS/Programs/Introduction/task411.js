21b82efec9b3d06dd7438b3662516daab1f8ae96
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:26:08 +0400

    update2

--- JS/Programs/Introduction/task411.js
+++ JS/Programs/Introduction/task411.js
@@ -32,4 +32,24 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(nums) {
+ return nums.every((item,i)=> item > nums[i+1] || item === nums[nums.length -1])
+}
+nums = [1, 3, 4, 7, 9, 10, 11]
+console.log("n = "+nums)
+console.log(" process: "+test(nums));
+nums = [11, 10, 9, 7, 4, 3, 2, 0]
+console.log("n = "+nums)
+console.log(" process: "+test(nums));
+nums = [1, 0, -2, -3, -12]
+console.log("n = "+nums)
+console.log(" process: "+test(nums))
+
+
+
  */
