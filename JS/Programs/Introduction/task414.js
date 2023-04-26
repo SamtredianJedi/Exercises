5099a03732d5800372b981ca837a17dae25272e1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:37:43 +0400

    task

---
+++ JS/Programs/Introduction/task414.js
@@ -0,0 +1,17 @@
+// JavaScript: Number of trailing zeroes in a factorial
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-96 with Solution
+Write a JavaScript program that calculates the factorial of a number and returns the number of trailing zeroes.
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. The factorial of n also equals the product of n with the next smaller factorial:
+n! = n x (n-1) x (n-2) x (n-3)x.....x 3 x 2 x 1
+= n x (n-1)!
+For example 4! = 4 x 3! = 4 x 3 x 2 x 1 =24
+The value of 0! is 1, according to the convention for an empty product.
+
+
+ */
