b21132ccf106469e958f0c532e6d114da55cde15
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-26 23:29:00 +0400

    task

---
+++ JS/Programs/Introduction/task412.js
@@ -0,0 +1,15 @@
+// JavaScript: Even, odd numbers and their sum in an array
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-94 with Solution
+Write a JavaScript program to calculate the sum and count of even and odd numbers in an array.
+
+Test Data:
+([1,2,3,4,5,6,7]) -> 3,12, 4,16
+([2,3,5,1,2,0,3,4,2,3,4)] -> 6,14, 5,15
+
+
+
+ */
