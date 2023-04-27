1a7e28723d6060c51b96415958eed95aff781cb9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-27 23:17:25 +0400

    update2

--- JS/Programs/Introduction/task415.js
+++ JS/Programs/Introduction/task415.js
@@ -37,4 +37,31 @@ HTML Code:
 </html>
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+  s = 1;
+  e = 26;
+  result = "";
+  while ((n -= s) >= 0) {
+        result = String.fromCharCode(parseInt((n % e) / s) + 65) + result;
+        s = e;
+        e *= 26;
+    }
+  return result;
+}
+n = 4
+console.log("n = " + n)
+console.log("Excel column title related with the said column number: "+test(n));
+n = 27
+console.log("n = " + n)
+console.log("Excel column title related with the said column number: "+test(n));
+n = 151
+console.log("n = " + n)
+console.log("Excel column title related with the said column number: "+test(n)); 
+
+
  */
