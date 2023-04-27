291a6281b2b2ffdf949486a113d99b3b185d0234
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-27 23:10:55 +0400

    update2

--- JS/Programs/Introduction/task414.js
+++ JS/Programs/Introduction/task414.js
@@ -33,4 +33,38 @@ HTML Code:
 </html>
 
 
+
+
+
+
+
+JavaScript Code:
+
+function test(n) {
+    ctr = 0
+    i = 5
+    while (n/i>=1){
+        ctr += Math.floor(n/i)
+        i *= 5
+    }
+    return ctr;
+};
+n = 4
+console.log("n = " + n)
+console.log("Number of trailing zeroes in the said factorial: "+test(n));
+n = 9
+console.log("n = " + n)
+console.log("Number of trailing zeroes in the said factorial: "+test(n));
+n = 10
+console.log("n = " + n)
+console.log("Number of trailing zeroes in the said factorial: "+test(n));
+n = 23
+console.log("n = " + n)
+console.log("Number of trailing zeroes in the said factorial: "+test(n));
+n = 35
+console.log("n = " + n)
+console.log("Number of trailing zeroes in the said factorial: "+test(n));
+
+
+
  */
