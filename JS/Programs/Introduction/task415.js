2e685312e98193709e18e92537e91ce6f474f6cf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-27 23:15:57 +0400

    task

---
+++ JS/Programs/Introduction/task415.js
@@ -0,0 +1,21 @@
+// JavaScript: Excel column title related with a column number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-97 with Solution
+Write a JavaScript program to retrieve the Excel column title associated with a given column number (integer value).
+
+For example:
+1 -> A
+2 -> B
+3 -> C
+...
+26 -> Z
+27 -> AA
+28 -> AB
+...
+
+
+ */
