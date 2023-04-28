3e310cecaafb4aee7d51c625368e1d158ad7f12b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:38:49 +0400

    update2

--- JS/Programs/Introduction/task417.js
+++ JS/Programs/Introduction/task417.js
@@ -32,4 +32,31 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+function test(n) {
+  if (isNaN(n) || n === 0) return 0;  
+  total = n;
+    while (n >= 10) {        
+        total = 0;
+        while (n != 0) {
+            total += Math.floor(n % 10);
+            n = Math.floor(n / 10);
+        }
+        n = total;
+    }
+    
+    return total;
+};  
+n = 49
+console.log("n = " +n)
+console.log("Add digits of the said number until there is only one digit: "+test(n));
+n = 9
+console.log("Original text: " +n)
+console.log("Add digits of the said number until there is only one digit: "+test(n));
+
+
  */
