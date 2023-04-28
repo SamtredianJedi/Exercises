07f5a04af381d2b721e1aad1402977a52b6bcce7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:36:42 +0400

    task

---
+++ JS/Programs/Introduction/task417.js
@@ -0,0 +1,16 @@
+// JavaScript: Add digits until there is only one digit
+
+/**
+ * 
+ * 
+ * JavaScript Math: Exercise-99 with Solution
+Write a JavaScript program to add repeatedly all the digits of a given non-negative number until the result has only one digit.
+
+For example:
+Input: 39
+Output: 3
+Explanation: The formula is like: 3 + 9 = 12, 1 + 2 = 3.
+
+
+
+ */
