1f8ba63ee4b8e57ea59d6844f60d657a496d92bb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:45:26 +0400

    update3

--- JS/Programs/Introduction/task418.js
+++ JS/Programs/Introduction/task418.js
@@ -33,4 +33,28 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function test(n) {
+  map = [5, 3, 2];
+  for (let i = 0; i < map.length && n > 1; i++) {
+    while (n % map[i] === 0) n /= map[i];
+  }
+  return n === 1;
+};  
+n = 12
+console.log("n = " +n)
+console.log("Check the said number is an ugly number? "+test(n));
+n = 18
+console.log("Original text: " +n)
+console.log("Check the said number is an ugly number? "+test(n));
+n = 19
+console.log("Original text: " +n)
+console.log("Check the said number is an ugly number? "+test(n));
+
+
  */
