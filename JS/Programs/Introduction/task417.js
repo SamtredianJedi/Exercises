666c1d1b1c18010dde4276e0cfca4737736b7c27
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:39:57 +0400

    update3

--- JS/Programs/Introduction/task417.js
+++ JS/Programs/Introduction/task417.js
@@ -59,4 +59,54 @@ console.log("Original text: " +n)
 console.log("Add digits of the said number until there is only one digit: "+test(n));
 
 
+
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript program to Add digits until there is only one digit</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+function test(n) {
+  if (isNaN(n) || n === 0) return 0;  
+  total = n;
+    while (n >= 10) {        
+        total = 0;
+        while (n != 0) {
+            total += Math.floor(n % 10);
+            n = Math.floor(n / 10);
+        }
+        n = total;
+    }
+    
+    return total;
+};  
+n = 49
+console.log("n = " +n)
+console.log("Add digits of the said number until there is only one digit: "+test(n));
+n = 9
+console.log("Original text: " +n)
+console.log("Add digits of the said number until there is only one digit: "+test(n));
+
+
+
+
  */
