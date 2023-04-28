c059335feceb749f9773f482bdf98aa88912494b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:31:45 +0400

    update2

--- JS/Programs/Introduction/task416.js
+++ JS/Programs/Introduction/task416.js
@@ -38,4 +38,31 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function test(text) {
+    text = text.toUpperCase()
+    var cl_no = 0
+    var len = text.length;
+    for (var i = 0; i < len; i++) {
+        cl_no += (Math.pow(26, (len - i - 1)) * (text.charCodeAt(i) - 64));
+    }
+    return cl_no;
+}
+text = "C"
+console.log("Original text: " +text)
+console.log("Excel column title related with the said column number: "+test(text));
+text = "AD"
+console.log("Original text: " +text)
+console.log("Excel column title related with the said column number: "+test(text));
+text = "ZX"
+console.log("Original text: " +text)
+console.log("Excel column title related with the said column number: "+test(text)); 
+
+
+
  */
