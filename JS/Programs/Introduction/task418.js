af873688c465285c5da317285ffb9b3e7261ee2a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-28 13:43:21 +0400

    task

---
+++ JS/Programs/Introduction/task418.js
@@ -0,0 +1,15 @@
+// JavaScript: Check a given number is an ugly number or not
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-100 with Solution
+Write a JavaScript program to check if a given number is ugly.
+
+Ugly numbers are positive numbers whose only prime factors are 2, 3 or 5. The first 10 ugly numbers are:
+1, 2, 3, 4, 5, 6, 8, 9, 10, 12, ...
+Note: 1 is typically treated as an ugly number.
+
+
+ */
