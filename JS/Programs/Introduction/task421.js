df0dd002dd52ad3e6fffec7f746de1c0252c0417
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-29 23:30:23 +0400

    task

---
+++ JS/Programs/Introduction/task421.js
@@ -0,0 +1,17 @@
+// JavaScript: Product of two non-negative integers as strings
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-103 with Solution
+Write a JavaScript program to calculate the product of non-negative integers n1 and n2 represented as strings. The product is also returned as a string.
+
+Test Data:
+("11", "10") -> "110"
+("17", "19") -> "323"
+("1", "0") -> "0"
+("0", "0") -> "0"
+
+
+ */
