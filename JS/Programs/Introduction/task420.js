a8a72cd5514ab11e000857414e2324eb7f3c0fe7
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-29 23:26:25 +0400

    update2

--- JS/Programs/Introduction/task420.js
+++ JS/Programs/Introduction/task420.js
@@ -33,4 +33,32 @@ HTML Code:
 </html>
 
 
+
+
+Solution-1
+
+JavaScript Code:
+
+function test(num) {
+    if (n<1)
+      return false;
+    var ctr = 0;
+    for (var i = 1; i <= n; i *= 10) {
+        var d = i * 10;
+        ctr += parseInt(n / d) * i +
+            Math.min(Math.max(n % d - i + 1, 0), i);
+    }
+    return ctr;
+}
+n = 11
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+n = 305
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+n = 0
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+
+
  */
