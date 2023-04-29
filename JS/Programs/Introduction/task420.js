f0e758627146c915e50eb5416aeb1687b81858fe
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-29 23:25:18 +0400

    task

---
+++ JS/Programs/Introduction/task420.js
@@ -0,0 +1,18 @@
+// JavaScript: Count total number of 1s from 1 to N
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-102 with Solution
+Write a JavaScript program to count the number of times the digit 1 appears in 1 to a given number.
+
+Example : 11, 1 appears 4 time between 1 to 11 [1->1, 10->1 ,11-.2]
+
+Test Data:
+(11) -> 4
+(305) -> 161
+(0) -> false
+
+
+ */
