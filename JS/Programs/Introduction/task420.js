3b64ccc93c660ec610cc47f914b3a15cc6f48339
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-29 23:27:16 +0400

    update3

--- JS/Programs/Introduction/task420.js
+++ JS/Programs/Introduction/task420.js
@@ -61,4 +61,52 @@ console.log("n = " +n)
 console.log("Total number of 1s from 1 to n: "+test(n));
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Count total number of 1s from 1 to N</title>
+</head>
+<body>
+
+</body>
+</html>
+
+js:
+
+
+
+
+function test(num) {
+    if (n<1)
+      return false;
+    var ctr = 0;
+    for (var i = 1; i <= n; i *= 10) {
+        var d = i * 10;
+        ctr += parseInt(n / d) * i +
+            Math.min(Math.max(n % d - i + 1, 0), i);
+    }
+    return ctr;
+}
+n = 11
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+n = 305
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+n = 0
+console.log("n = " +n)
+console.log("Total number of 1s from 1 to n: "+test(n));
+
+
+
  */
