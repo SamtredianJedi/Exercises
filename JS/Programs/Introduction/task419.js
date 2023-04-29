7d78c292303d6c5e47a436017c27f9e559a36ea4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-29 23:21:27 +0400

    task

---
+++ JS/Programs/Introduction/task419.js
@@ -0,0 +1,15 @@
+// JavaScript: Find the nth ugly number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-101 with Solution
+Write a JavaScript program to find the nth ugly number.
+
+Ugly numbers are positive numbers whose only prime factors are 2, 3 or 5. The first 10 ugly numbers are:
+1, 2, 3, 4, 5, 6, 8, 9, 10, 12, ...
+Note: 1 is typically treated as an ugly number.
+
+
+ */
