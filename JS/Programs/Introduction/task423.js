dd70f9b0c0134babe060f4b96bdc93234cc0c158
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-30 17:10:43 +0400

    update2

--- JS/Programs/Introduction/task423.js
+++ JS/Programs/Introduction/task423.js
@@ -40,6 +40,21 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+var is_array = function(input) {
+  if (toString.call(input) === "[object Array]")
+    return true;
+  return false;   
+    };
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
+
+
+
  * 
  * 
  * 
