16e43e611d0992f589d08c9066b7b04d74a11f62
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-30 17:04:09 +0400

    task

---
+++ JS/Programs/Introduction/task422.js
@@ -0,0 +1,18 @@
+// JavaScript: Distinct ways to climb the staircase
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Math: Exercise-104 with Solution
+A staircase consists of N steps, and you are given the choice of climbing one step at a time or two steps at a time.
+Write a JavaScript program to find distinct ways to climb the staircase.
+
+Example:
+Number of stairs = 2
+There are 2 ways to climb the stairs: (1,1) and (2)
+Number of stairs = 3
+There are 3 ways to climb the stairs: (1,1,1), (2,1) and (1,2)
+
+
+ */
