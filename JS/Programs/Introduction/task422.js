77bb11d219f8d956ea6b7fbc84e6509006a2e2f1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-30 17:05:11 +0400

    update2

--- JS/Programs/Introduction/task422.js
+++ JS/Programs/Introduction/task422.js
@@ -34,4 +34,30 @@ HTML Code:
 
 
 
+
+
+
+Solution-1
+
+JavaScript Code:
+
+function test(n) {
+    var x = 0,
+        y = 1;
+    while(n-->0){
+        y = x + y;
+        x = y - x;
+    }
+    return b;
+ }
+
+n = 2
+console.log("Number of stairs = " +n)
+console.log("Number of ways to climb the said number of stairs: "+test(n));
+n = 3
+console.log("Number of stairs = " +n)
+console.log("Number of ways to climb the said number of stairs: "+test(n));
+
+
+
  */
