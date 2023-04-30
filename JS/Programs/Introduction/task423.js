2a2028448a3ff545b5968486118164e4c9364e36
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-30 17:11:19 +0400

    es6version

--- JS/Programs/Introduction/task423.js
+++ JS/Programs/Introduction/task423.js
@@ -55,6 +55,21 @@ console.log(is_array([1, 2, 4, 0]));
 
 
 
+
+
+
+
+ES6 Version:
+
+var is_array = input => {
+  if (toString.call(input) === "[object Array]")
+    return true;
+  return false;   
+    };
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
+
+
  * 
  * 
  * 
