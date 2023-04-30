1185353f149c59e48f21ca1f1b34bfa08bbc500c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-04-30 17:09:44 +0400

    task

---
+++ JS/Programs/Introduction/task423.js
@@ -0,0 +1,29 @@
+// JavaScript array - Exercises, Practice, Solution
+
+
+
+/**
+ * 
+ * JavaScript: Get the first element of an array
+ * 
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-1 with Solution
+Write a JavaScript function to check whether an 'input' is an array or not.
+
+Test Data:
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
+false
+true
+
+
+
+
+ * 
+ * 
+ * 
+ * 
+ */
