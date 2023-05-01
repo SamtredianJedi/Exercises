43a7de108d1b7a29d3e519019a70f6cd513bea1e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-01 23:28:57 +0400

    task

---
+++ JS/Programs/Introduction/task425.js
@@ -0,0 +1,19 @@
+// JavaScript: Get the first element of an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-3 with Solution
+Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+
+Test Data:
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+[1, 2, 4, 0]
+[1, 2, [4, 0]]
+
+
+
+ */
