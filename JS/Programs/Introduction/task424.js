7822d26f24012751c08d239e78e6f70a8c114703
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-01 23:22:47 +0400

    update2

--- JS/Programs/Introduction/task424.js
+++ JS/Programs/Introduction/task424.js
@@ -15,4 +15,20 @@ console.log(array_Clone([1, 2, [4, 0]]));
 [1, 2, [4, 0]]
 
 
+
+
+
+
+Sample Solution-1:
+
+JavaScript Code:
+
+var array_Clone = function(arra1) {
+ return arra1.slice(0);
+    };
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+
+
+
  */
