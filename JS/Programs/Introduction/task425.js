f682e9988342f2cb264b2560de738f28b4dbd766
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-01 23:31:50 +0400

    update3

--- JS/Programs/Introduction/task425.js
+++ JS/Programs/Introduction/task425.js
@@ -59,4 +59,28 @@ console.log(first([7, 9, 0, -2],-3));
 
 
 
+
+
+
+ES6 Version:
+
+var first =  (array, n) => {
+      if (array == null) 
+      return void 0;
+    if (n == null) 
+      return array[0];
+    if (n < 0)
+      return [];
+    return array.slice(0, n);
+  };
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+
+
+
  */
