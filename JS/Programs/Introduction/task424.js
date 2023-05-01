9dd7e76c2d8a83f79e701be939b8ddba8c31ad0e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-01 23:22:07 +0400

    task

---
+++ JS/Programs/Introduction/task424.js
@@ -0,0 +1,18 @@
+// JavaScript: Clone an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-2 with Solution
+Write a JavaScript function to clone an array.
+
+Test Data:
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+[1, 2, 4, 0]
+[1, 2, [4, 0]]
+
+
+ */
