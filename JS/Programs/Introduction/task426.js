bd61ce7af31ad5843fa30696d4d544126401c421
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:29:29 +0400

    update2

--- JS/Programs/Introduction/task426.js
+++ JS/Programs/Introduction/task426.js
@@ -31,4 +31,24 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+var last =  function(array, n) {
+  if (array == null) 
+    return void 0;
+  if (n == null) 
+     return array[array.length - 1];
+  return array.slice(Math.max(array.length - n, 0));  
+  };
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+
+
+
  */
