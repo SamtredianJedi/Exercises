6b530173b0f98f6f95438fb076ba90c39c1414ac
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:34:46 +0400

    update4

--- JS/Programs/Introduction/task427.js
+++ JS/Programs/Introduction/task427.js
@@ -57,4 +57,35 @@ console.log(myColor.join('+'));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JavaScript Array Join</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+js:
+
+
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
+
+
+
  */
