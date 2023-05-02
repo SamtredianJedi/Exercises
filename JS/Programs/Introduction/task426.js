cae7e677f93cefad1f35545ca8ce9316746b38b2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:28:08 +0400

    task

---
+++ JS/Programs/Introduction/task426.js
@@ -0,0 +1,17 @@
+// JavaScript: Get the last element of an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-4 with Solution
+Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+
+Test Data :
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+
+
+ */
