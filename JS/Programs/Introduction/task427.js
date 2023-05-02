c5b374b883dbc64be46c6d8d7325766dac3f5ac6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:32:24 +0400

    task

---
+++ JS/Programs/Introduction/task427.js
@@ -0,0 +1,18 @@
+// JavaScript: Join all elements of an array into a string
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-5 with Solution
+Write a simple JavaScript program to join all elements of the following array into a string.
+
+Expected Output:
+"Red,Green,White,Black"
+"Red,Green,White,Black"
+"Red+Green+White+Black"
+
+
+
+ */
