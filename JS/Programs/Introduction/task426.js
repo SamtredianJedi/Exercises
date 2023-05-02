44eeb59e3b51284407253536424d91c9b7e96962
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:30:52 +0400

    update4

--- JS/Programs/Introduction/task426.js
+++ JS/Programs/Introduction/task426.js
@@ -68,4 +68,44 @@ console.log(last([7, 9, 0, -2], 3));
 console.log(last([7, 9, 0, -2], 6));
 
 
+
+
+Live Demo:
+
+html:
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Get last and last n elements of an array</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+js:
+
+
+
+
+var last =  function(array, n) {
+  if (array == null) 
+    return void 0;
+  if (n == null) 
+     return array[array.length - 1];
+  return array.slice(Math.max(array.length - n, 0));  
+  };
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+
+
+
+
  */
