8799ac9c2462e673260e961cb019857e36d2a22c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:33:27 +0400

    update2

--- JS/Programs/Introduction/task427.js
+++ JS/Programs/Introduction/task427.js
@@ -31,4 +31,17 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
+
+
+
  */
