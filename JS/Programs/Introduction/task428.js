d53077da1741ab94961c728bd48b65bbab56d11f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:37:29 +0400

    update2

--- JS/Programs/Introduction/task428.js
+++ JS/Programs/Introduction/task428.js
@@ -26,4 +26,29 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+const num=window.prompt();
+const str = num.toString();
+const result = [str[0]];
+  
+for(let x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x]);
+     }
+    else
+     {
+      result.push(str[x]);
+     }
+  }
+console.log(result.join(''));
+
+
+
  */
