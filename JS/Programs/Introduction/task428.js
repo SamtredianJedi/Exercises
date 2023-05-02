10c793f4fd6c3015d31779ddac0a4ddb0493df86
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:36:21 +0400

    task

---
+++ JS/Programs/Introduction/task428.js
@@ -0,0 +1,13 @@
+// JavaScript: Insert dashes (-) between each two even digits of a specific number
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-6 with Solution
+Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
+
+
+
+ */
