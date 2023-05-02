4a5c53755305c08ac54f086b5bd77cb4fa1984d8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-02 23:30:03 +0400

    update3

--- JS/Programs/Introduction/task426.js
+++ JS/Programs/Introduction/task426.js
@@ -51,4 +51,21 @@ console.log(last([7, 9, 0, -2],6));
 
 
 
+
+
+
+
+ES6 Version:
+
+var last = function last(array, n) {
+  if (array == null) return void 0;
+  if (n == null) return array[array.length - 1];
+  return array.slice(Math.max(array.length - n, 0));
+};
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2], 3));
+console.log(last([7, 9, 0, -2], 6));
+
+
  */
