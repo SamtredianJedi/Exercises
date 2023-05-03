ab1f767fbc5d04dc04ea72cdad0663f697bab557
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:34:53 +0400

    update2

--- JS/Programs/Introduction/task430.js
+++ JS/Programs/Introduction/task430.js
@@ -31,4 +31,31 @@ html:
 
 
 
+
+
+
+JavaScript Code:
+
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var mf = 1;
+var m = 0;
+var item;
+for (var i=0; i<arr1.length; i++)
+{
+        for (var j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+}
+console.log(item+" ( " +mf +" times ) ") ;
+
+
+
  */
