93d013c344239ba653bfed9341ca0da60ece30e5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:33:16 +0400

    task

---
+++ JS/Programs/Introduction/task430.js
@@ -0,0 +1,16 @@
+// JavaScript: Find the most frequent item of an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-8 with Solution
+Write a JavaScript program to find the most frequent item in an array.
+
+Sample array: var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+Sample Output: a ( 5 times )
+
+
+
+ */
