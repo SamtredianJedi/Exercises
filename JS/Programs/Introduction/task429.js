f88ff866fdc7a3d494e747a9d460312cdf068749
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:29:09 +0400

    task

---
+++ JS/Programs/Introduction/task429.js
@@ -0,0 +1,14 @@
+// JavaScript: Sort the items of an array
+
+/**
+ * 
+ * 
+ * JavaScript Array: Exercise-7 with Solution
+Write a JavaScript program to sort the items of an array.
+
+Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8
+
+
+
+ */
