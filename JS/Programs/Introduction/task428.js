1b33346c916bfccd3a5e0270e9d46bd37e727b7c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:27:15 +0400

    update4

--- JS/Programs/Introduction/task428.js
+++ JS/Programs/Introduction/task428.js
@@ -71,4 +71,32 @@ console.log(result.join(''));
 
 
 
+Live Demo:
+
+
+
+
+js:
+
+
+var num=window.prompt();
+var str = num.toString();
+var result = [str[0]];
+  
+for(var x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x]);
+     }
+    else
+     {
+      result.push(str[x]);
+     }
+  }
+console.log(result.join(''));
+
+
+
+
  */
