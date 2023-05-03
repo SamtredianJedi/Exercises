6c5a42f0f98cb159ebb1f271051f6e6dca4b47d3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:34:21 +0400

    update1

--- JS/Programs/Introduction/task430.js
+++ JS/Programs/Introduction/task430.js
@@ -13,4 +13,22 @@ Sample Output: a ( 5 times )
 
 
 
+
+
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Write a JavaScript program to find the most frequent item of an array. - w3resource</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
  */
