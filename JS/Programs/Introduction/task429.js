2bb301c1223eba34663e55f96a0a7b41fc37becd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:31:55 +0400

    update4

--- JS/Programs/Introduction/task429.js
+++ JS/Programs/Introduction/task429.js
@@ -101,4 +101,62 @@ console.log(arr2);
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Write a JavaScript program to sort the items of an array</title>
+</head>
+<body>
+</body>
+</html>
+
+
+js:
+
+
+
+
+var arr1=[-3,8,7,6,5,-4,3,2,1];
+var arr2=[];
+var min=arr1[0];
+var pos;
+var max=arr1[0];
+for (i=0; i<arr1.length; i++)
+{
+        if (max<arr1[i]) max=arr1[i];
+}
+
+for (var i=0;i<arr1.length;i++)
+{
+        for (var j=0;j<arr1.length;j++)
+        {
+                if (arr1[j]!="x")
+                {
+                        if (min>arr1[j]) 
+                        {
+                                min=arr1[j];
+                                pos=j;
+                        }
+                }
+        }
+        arr2[i]=min;
+        arr1[pos]="x";
+        min=max;
+}
+console.log(arr2);
+
+
+
+
+
+
  */
