d253db9a4da44d92673bd100236e02de9cc8e234
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-03 20:36:01 +0400

    update3

--- JS/Programs/Introduction/task430.js
+++ JS/Programs/Introduction/task430.js
@@ -58,4 +58,31 @@ console.log(item+" ( " +mf +" times ) ") ;
 
 
 
+
+
+
+ES6 Version:
+
+const arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+let mf = 1;
+let m = 0;
+let item;
+for (let i=0; i<arr1.length; i++)
+{
+        for (let j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+}
+console.log(`${item} ( ${mf} times ) `) ;
+
+
+
  */
