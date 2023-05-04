6a213164c2e174cc386b7e3a8592247956c63103
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-04 22:03:10 +0400

    update4

--- JS/Programs/Introduction/task431.js
+++ JS/Programs/Introduction/task431.js
@@ -89,4 +89,55 @@ console.log(result.join(''));
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Swap the case of each character of a string</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+
+
+
+js:
+
+
+
+var str = 'c';
+var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+var result = [];
+  
+  for(var x=0; x<str.length; x++)
+  {
+    if(UPPER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toUpperCase());
+    }
+    else 
+    {
+      result.push(str[x]);
+    }
+  }
+console.log(result.join(''));
+
+
+
  */
