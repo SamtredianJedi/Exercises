6af3d41e5f0c89d2b831246dbe37124f38a293ea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-04 21:59:10 +0400

    task

---
+++ JS/Programs/Introduction/task431.js
@@ -0,0 +1,12 @@
+// JavaScript: Swap the case of each character of a string, upper case to lower and vice versa
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-9 with Solution
+Write a JavaScript program that accepts a string as input and swaps the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+
+
+ */
