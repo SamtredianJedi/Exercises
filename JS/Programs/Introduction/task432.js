3ecdbc9bb427c180067737343b083c69eb6ac84e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-04 22:04:32 +0400

    task

---
+++ JS/Programs/Introduction/task432.js
@@ -0,0 +1,25 @@
+// JavaScript: Print the elements of an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-10 with Solution
+Write a JavaScript program that prints the elements of the following array.
+
+Note : Use nested for loops.
+Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+Sample Output :
+"row 0"
+" 1"
+" 2"
+" 1"
+" 24"
+"row 1"
+------
+------
+
+
+
+ */
