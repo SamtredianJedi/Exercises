b55aae169ee9929e9e2b93f527c1901a359dc16a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-04 22:05:35 +0400

    update2

--- JS/Programs/Introduction/task432.js
+++ JS/Programs/Introduction/task432.js
@@ -39,4 +39,23 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+// a sample 2-D array 
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+ 
+for (var i in a) 
+{
+   console.log("row " + i);
+   for (var j in a[i]) 
+     {
+      console.log(" " + a[i][j]);
+     }
+}
+
+
+
  */
