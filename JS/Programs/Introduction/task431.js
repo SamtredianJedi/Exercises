af4c971470ba8622de80302ab69c26de524abd6a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-04 22:01:59 +0400

    update3

--- JS/Programs/Introduction/task431.js
+++ JS/Programs/Introduction/task431.js
@@ -57,4 +57,36 @@ console.log(result.join(''));
 
 
 
+
+
+
+
+ES6 Version:
+
+var str = 'c';
+const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const result = [];
+  
+  for(let x=0; x<str.length; x++)
+  {
+    if(UPPER.includes(str[x]))
+    {
+      result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.includes(str[x]))
+    {
+      result.push(str[x].toUpperCase());
+    }
+    else 
+    {
+      result.push(str[x]);
+    }
+  }
+console.log(result.join(''));
+
+
+
+
+
  */
