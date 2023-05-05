66cb2728854440e03eecc758e09b208da1005c21
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:09:51 +0400

    task

---
+++ JS/Programs/Introduction/task435.js
@@ -0,0 +1,12 @@
+// JavaScript: Add items in a blank array and display the items
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-13 with Solution
+Write a JavaScript program to add items to a blank array and display them.
+
+
+
+ */
