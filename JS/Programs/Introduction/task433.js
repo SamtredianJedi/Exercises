4337230476b986f1ddf79323376c372bed988aee
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:02:27 +0400

    update2

--- JS/Programs/Introduction/task433.js
+++ JS/Programs/Introduction/task433.js
@@ -25,4 +25,23 @@ HTML Code:
 
 
 
+
+
+
+
+
+JavaScript Code:
+
+function sum_sq(array) {
+  var sum = 0, 
+      i = array.length;
+  while (i--) 
+   sum += Math.pow(array[i], 2);
+  return sum;
+}
+ 
+console.log(sum_sq([0,1,2,3,4])); 
+
+
+
  */
