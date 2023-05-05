3247c22b48e03d049b894e2ec22d3e83f3b363a2
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:00:51 +0400

    update1

--- JS/Programs/Introduction/task433.js
+++ JS/Programs/Introduction/task433.js
@@ -8,4 +8,21 @@
 Write a JavaScript program to find the sum of squares of a numerical vector.
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Sum of squares of a numeric vector</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
  */
