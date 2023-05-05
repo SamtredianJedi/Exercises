0d7f4b28ae720fd027518d108b57b5e7fd06673e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:05:21 +0400

    task

---
+++ JS/Programs/Introduction/task434.js
@@ -0,0 +1,12 @@
+// JavaScript: Compute the sum and product of an array of integers
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-12 with Solution
+Write a JavaScript program to compute the sum and product of an array of integers.
+
+
+
+ */
