3c2916a67f4feb38d643784f309e87ae9ef03f3d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:10:56 +0400

    update1

--- JS/Programs/Introduction/task435.js
+++ JS/Programs/Introduction/task435.js
@@ -9,4 +9,28 @@ Write a JavaScript program to add items to a blank array and display them.
 
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JS Bin</title>
+<style>
+body {padding-top:50px} 
+</style> 
+</head>
+<body>
+<input type="text" id="text1"></input>
+<input type="button" id="button1" value="Add" onclick="add_element_to_array();"></input>
+<input type="button" id="button2" value="Display" onclick="display_array();"></input>
+<div id="Result"></div> 
+</body>
+</html>
+
+
+
  */
