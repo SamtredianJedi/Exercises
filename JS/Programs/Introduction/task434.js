1be53f72a26633fc3d3629337d3ec55e1e2806ff
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-05 18:07:41 +0400

    update4

--- JS/Programs/Introduction/task434.js
+++ JS/Programs/Introduction/task434.js
@@ -65,4 +65,25 @@ console.log(`Sum : ${s} Product :  ${p}`);
 
 
 
+
+
+Live Demo:
+
+
+
+
+var array = [1, 2, 3, 4, 5, 6],
+    s = 0,
+    p = 1,
+    i;
+for (i = 0; i < array.length; i += 1) 
+   {
+    s += array[i];
+    p *= array[i];
+    }
+console.log('Sum : '+s + ' Product :  ' +p); 
+
+
+
+
  */
