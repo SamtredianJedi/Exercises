72b8410c413ffd7998177d61bf0dc224efe60455
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:42:47 +0400

    task

---
+++ JS/Programs/Introduction/task437.js
@@ -0,0 +1,21 @@
+// JavaScript: Display the colors entered in an array by a specific format
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-15 with Solution
+Write a JavaScript program to display the colors in the following way.
+
+Here is the sample array:
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["th","st","nd","rd"]
+Output
+"1st choice is Blue ."
+"2nd choice is Green."
+"3rd choice is Red."
+- - - - - - - - - - - - -
+
+
+
+ */
