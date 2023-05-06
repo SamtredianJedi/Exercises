2e07bf7eebea23fe954a923e696d60f7c6831be3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:41:07 +0400

    update3

--- JS/Programs/Introduction/task436.js
+++ JS/Programs/Introduction/task436.js
@@ -55,4 +55,34 @@ console.log([...new Set(nums)])
 
 
 
+
+
+
+Live Demo:
+
+
+
+function removeDuplicates(num) {
+  var x,
+      len=num.length,
+      out=[],
+      obj={};
+ 
+  for (x=0; x<len; x++) {
+    obj[num[x]]=0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  return out;
+}
+var Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+result = removeDuplicates(Mynum);
+console.log(Mynum);
+console.log(result);
+
+
+
+
+
  */
