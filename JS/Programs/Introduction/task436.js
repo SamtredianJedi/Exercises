a3b366060b05900cf89830ba842aa9d54027b99b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:39:50 +0400

    update1

--- JS/Programs/Introduction/task436.js
+++ JS/Programs/Introduction/task436.js
@@ -9,4 +9,33 @@ Write a JavaScript program to remove duplicate items from an array (ignore case 
 
 
 
+
+
+Sample Solution-1:
+
+JavaScript Code:
+
+function removeDuplicates(num) {
+  var x,
+      len=num.length,
+      out=[],
+      obj={};
+ 
+  for (x=0; x<len; x++) {
+    obj[num[x]]=0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  return out;
+}
+var Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+result = removeDuplicates(Mynum);
+console.log(Mynum);
+console.log(result);
+
+
+
+
+
  */
