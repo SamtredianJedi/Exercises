9fbe0946c8cd9849a14c8746a6a10f6283d023d5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:35:33 +0400

    update3

--- JS/Programs/Introduction/task435.js
+++ JS/Programs/Introduction/task435.js
@@ -59,4 +59,34 @@ function display_array()
 }
 
 
+
+
+
+
+ES6 Version:
+
+let x = 0;
+const array = Array();
+
+function add_element_to_array()
+{
+ array[x] = document.getElementById("text1").value;
+ alert(`Element: ${array[x]} Added at index ${x}`);
+ x++;
+ document.getElementById("text1").value = "";
+}
+
+function display_array()
+{
+   let e = "<hr/>";   
+    
+   for (let y=0; y<array.length; y++)
+   {
+     e += `Element ${y} = ${array[y]}<br/>`;
+   }
+   document.getElementById("Result").innerHTML = e;
+}
+
+
+
  */
