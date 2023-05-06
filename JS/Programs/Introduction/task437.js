1f2133b0d2783cfb98d66ecaab36c7c28f344340
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:45:12 +0400

    update4

--- JS/Programs/Introduction/task437.js
+++ JS/Programs/Introduction/task437.js
@@ -88,5 +88,27 @@ console.log(output);
 }
 
 
+Live Demo:
+
+
+
+
+var color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+
+function Ordinal(n)
+ {
+ var o = ["th","st","nd","rd"],
+ x = n%100;
+ return x+(o[(x-20)%10]||o[x]||o[0]);
+ }
+
+for(n = 0; n < color.length; n++){
+ var ordinal = n + 1;
+
+ var output = (Ordinal(ordinal) + " choice is " + color[n] + ".");
+
+console.log(output);
+}
+
 
  */
