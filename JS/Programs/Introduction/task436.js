d58786adaf6ae14458c7e7cfd96a7056911ccab6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-06 19:37:50 +0400

    task

---
+++ JS/Programs/Introduction/task436.js
@@ -0,0 +1,12 @@
+// JavaScript: Remove duplicate items from an array, ignore case sensitivity
+
+
+/**
+ * 
+ * 
+ * JavaScript Array: Exercise-14 with Solution
+Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity).
+
+
+
+ */
