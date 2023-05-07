6575b6293c51ffb62dfe87c6175c033d0cbd11d3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-07 23:42:41 +0400

    task

---
+++ JS/Programs/Introduction/task440.js
@@ -0,0 +1,20 @@
+// JavaScript: Perform a binary search within an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-18 with Solution
+Write a JavaScript program to perform a binary search.
+
+Note : A binary search or half-interval search algorithm finds the position of a specified input value within an array sorted by key value.
+
+
+Sample array:
+var items = [1, 2, 3, 4, 5, 7, 8, 9];
+Expected Output:
+console.log(binary_Search(items, 1)); //0
+console.log(binary_Search(items, 5)); //4
+
+
+ */
