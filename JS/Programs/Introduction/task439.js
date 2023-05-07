755a3c68e742923a4eef9be0f2a198221b90e3cc
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-07 23:40:19 +0400

    update3

--- JS/Programs/Introduction/task439.js
+++ JS/Programs/Introduction/task439.js
@@ -53,4 +53,32 @@ console.log(shuffle(myArray));
 
 
 
+
+
+
+ES6 Version:
+
+function shuffle(arra1) {
+    let ctr = arra1.length;
+    let temp;
+    let index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
+const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(shuffle(myArray));
+
+
+
  */
