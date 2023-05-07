88a2b6510262f22252af0b481331faab03b6f370
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-07 23:35:08 +0400

    update2

--- JS/Programs/Introduction/task438.js
+++ JS/Programs/Introduction/task438.js
@@ -25,4 +25,41 @@ HTML Code:
 
 
 
+
+
+
+
+
+JavaScript Code:
+
+function leap_year_range(st_year, end_year) {
+        var year_range = [];
+        for (var i = st_year; i <= end_year; i++)
+        {
+             year_range.push(i);
+        }
+        var new_array = [];
+
+  year_range.forEach(
+   function(year)
+    { 
+       if (test_LeapYear(year)) 
+       new_array.push(year);
+    });
+
+  return new_array;
+     }
+
+function test_LeapYear(year) {
+   if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+                return year;
+        } else {
+                return false;
+        }
+}
+
+console.log(leap_year_range(2000,2012));
+
+
+
  */
