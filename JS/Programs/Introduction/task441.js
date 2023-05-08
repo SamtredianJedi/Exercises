a1eb7db71a29f6af2ad1dd6c51a1a7117737ad82
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:32:29 +0400

    update3

--- JS/Programs/Introduction/task441.js
+++ JS/Programs/Introduction/task441.js
@@ -76,4 +76,48 @@ console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
 
 
 
+
+
+
+
+
+ES6 Version:
+
+function Arrays_sum(array1, array2) 
+{
+  const result = [];
+  let ctr = 0;
+  let x=0;
+
+  if (array1.length === 0) 
+   return "array1 is empty";
+  if (array2.length === 0) 
+   return "array2 is empty";   
+
+ while (ctr < array1.length && ctr < array2.length) 
+  {
+    result.push(array1[ctr] + array2[ctr]);
+    ctr++;
+  }
+
+ if (ctr === array1.length) 
+ {
+    for (x = ctr; x < array2.length; x++)   {
+      result.push(array2[x]);
+    }
+  } 
+  else
+  {
+  for (x = ctr; x < array1.length; x++) 
+    {
+      result.push(array1[x]);
+    }
+  }
+  return result;
+}
+
+console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
+
+
+
  */
