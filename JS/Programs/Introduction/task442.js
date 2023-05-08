673c25fa9ef7b0ed1280a482a64c8192778b4ac5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:37:14 +0400

    update4

--- JS/Programs/Introduction/task442.js
+++ JS/Programs/Introduction/task442.js
@@ -86,5 +86,52 @@ function find_duplicate_in_array(arra1) {
     console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
 
 
+
+
+
+    Live Demo:
+
+
+
+
+    html:
+
+    <head>
+<meta charset="utf-8">
+<title>Find duplicate values in a JavaScript array</title>
+</head>
+<body>
+</body>
+
+
+
+js:
+
+
+
+
+function find_duplicate_in_array(arra1) {
+        var object = {};
+        var result = [];
+
+        arra1.forEach(function (item) {
+          if(!object[item])
+              object[item] = 0;
+            object[item] += 1;
+        })
+
+        for (var prop in object) {
+           if(object[prop] >= 2) {
+               result.push(prop);
+           }
+        }
+
+        return result;
+
+    }
+
+    console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
+
+
     
  */
