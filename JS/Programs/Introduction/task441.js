fd464787e04171142df44416ac4f9deae4c7641e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:30:16 +0400

    task

---
+++ JS/Programs/Introduction/task441.js
@@ -0,0 +1,18 @@
+// JavaScript: Compute the sum of each individual index value of two or more arrays
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-19 with Solution
+There are two arrays with individual values. Write a JavaScript program to compute the sum of each individual index value in the given array.
+
+Sample array:
+array1 = [1,0,2,3,4];
+array2 = [3,5,6,7,8,13];
+Expected Output:
+[4, 5, 8, 10, 12, 13]
+
+
+ */
