261bf454590698e25de2aebd3fc217457c695a6d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:35:51 +0400

    update2

--- JS/Programs/Introduction/task442.js
+++ JS/Programs/Introduction/task442.js
@@ -26,4 +26,34 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function find_duplicate_in_array(arra1) {
+        var object = {};
+        var result = [];
+
+        arra1.forEach(function (item) {
+          if(!object[item])
+              object[item] = 0;
+            object[item] += 1;
+        })
+
+        for (var prop in object) {
+           if(object[prop] >= 2) {
+               result.push(prop);
+           }
+        }
+
+        return result;
+
+    }
+
+    console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
+
+
+    
  */
