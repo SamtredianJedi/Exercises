41900f107964424523d8cefa7a9fe0cf1ed345e9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:33:20 +0400

    update4

--- JS/Programs/Introduction/task441.js
+++ JS/Programs/Introduction/task441.js
@@ -120,4 +120,63 @@ console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
 
 
 
+
+
+Live Demo:
+
+
+
+
+html:
+
+
+<head>
+<meta charset="utf-8">
+<title>compute the sum of each individual index value from two arrays</title>
+</head>
+<body>
+</body>
+
+
+js:
+
+
+
+
+function Arrays_sum(array1, array2) 
+{
+  var result = [];
+  var ctr = 0;
+  var x=0;
+
+  if (array1.length === 0) 
+   return "array1 is empty";
+  if (array2.length === 0) 
+   return "array2 is empty";   
+
+ while (ctr < array1.length && ctr < array2.length) 
+  {
+    result.push(array1[ctr] + array2[ctr]);
+    ctr++;
+  }
+
+ if (ctr === array1.length) 
+ {
+    for (x = ctr; x < array2.length; x++)   {
+      result.push(array2[x]);
+    }
+  } 
+  else
+  {
+  for (x = ctr; x < array1.length; x++) 
+    {
+      result.push(array1[x]);
+    }
+  }
+  return result;
+}
+
+console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13]));
+
+
  */
