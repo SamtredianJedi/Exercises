88adda7ce26fc4232c80987edc1aa51780437b26
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:27:56 +0400

    update3

--- JS/Programs/Introduction/task440.js
+++ JS/Programs/Introduction/task440.js
@@ -66,4 +66,38 @@ console.log(binary_Search(items, 5));
 
 
 
+
+
+
+
+ES6 Version:
+
+function binary_Search(items, value){
+    let firstIndex  = 0;
+    let lastIndex   = items.length - 1;
+    let middleIndex = Math.floor((lastIndex + firstIndex)/2);
+
+    while(items[middleIndex] != value && firstIndex < lastIndex)
+    {
+       if (value < items[middleIndex])
+        {
+            lastIndex = middleIndex - 1;
+        } 
+      else if (value > items[middleIndex])
+        {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
+    }
+
+    return (items[middleIndex] != value) ? -1 : middleIndex;
+}
+const items = [1, 2, 3, 4, 5, 7, 8, 9];
+console.log(binary_Search(items, 1));   
+console.log(binary_Search(items, 5));
+
+
+
+
+
  */
