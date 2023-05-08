750cee0557c1420598bb3eb1fe10d0bceb7d0fad
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-08 23:36:22 +0400

    update3

--- JS/Programs/Introduction/task442.js
+++ JS/Programs/Introduction/task442.js
@@ -55,5 +55,36 @@ function find_duplicate_in_array(arra1) {
     console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
 
 
+
+
+
+
+
+
+    ES6 Version:
+
+function find_duplicate_in_array(arra1) {
+        const object = {};
+        const result = [];
+
+        arra1.forEach(item => {
+          if(!object[item])
+              object[item] = 0;
+            object[item] += 1;
+        })
+
+        for (const prop in object) {
+           if(object[prop] >= 2) {
+               result.push(prop);
+           }
+        }
+
+        return result;
+
+    }
+
+    console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
+
+
     
  */
