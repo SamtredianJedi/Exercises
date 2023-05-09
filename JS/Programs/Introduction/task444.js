f4c5d3560ed832cc66ff221e51b99f418dae03a4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:13:33 +0400

    update4

--- JS/Programs/Introduction/task444.js
+++ JS/Programs/Introduction/task444.js
@@ -103,4 +103,44 @@ console.log(union([1, 2, 3], [100, 2, 1, 10]));
 
 
 
+
+
+
+
+Live Demo:
+
+
+js:
+
+
+
+
+
+function union(arra1, arra2) {
+  
+  if ((arra1 == null) || (arra2==null)) 
+    return void 0;
+  
+  var obj = {};
+ 
+  for (var i = arra1.length-1; i >= 0; -- i)
+     obj[arra1[i]] = arra1[i];
+ 
+  for (var i = arra2.length-1; i >= 0; -- i)
+     obj[arra2[i]] = arra2[i];
+ 
+  var res = [];
+ 
+  for (var n in obj)
+  {
+  
+    if (obj.hasOwnProperty(n)) 
+      res.push(obj[n]);
+  }
+ 
+  return res;
+}
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
+
+
  */
