c51ec38f6cc4c93005474b4ddd5775db25fb92a0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:07:49 +0400

    update2

--- JS/Programs/Introduction/task443.js
+++ JS/Programs/Introduction/task443.js
@@ -33,4 +33,32 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+var flatten = function(a, shallow,r){
+  if(!r){ r = []}
+   
+if (shallow) {
+  return r.concat.apply(r,a);
+  }
+      
+   for(var i=0; i<a.length; i++){
+        if(a[i].constructor == Array){
+            flatten(a[i],shallow,r);
+        }else{
+            r.push(a[i]);
+        }
+    }
+    return r;
+}
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+
+
+
  */
