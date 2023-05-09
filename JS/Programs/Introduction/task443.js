5f460e5bc48cfae4abe497f80ecba386945e1dab
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:06:31 +0400

    task

---
+++ JS/Programs/Introduction/task443.js
@@ -0,0 +1,18 @@
+// JavaScript: Flatten a nested array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-21 with Solution
+Write a JavaScript program to flatten a nested (any depth) array. If you pass shallow, the array will only be flattened to a single level.
+
+Sample Data:
+console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+[1, 2, 3, 4, 5, 6]
+console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+[1, 2, 3, [[4]], 5, 6]
+
+
+
+ */
