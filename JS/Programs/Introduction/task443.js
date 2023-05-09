3a486055dc8fab9ffa111c8d67f9724e61208538
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:09:21 +0400

    update4

--- JS/Programs/Introduction/task443.js
+++ JS/Programs/Introduction/task443.js
@@ -90,4 +90,47 @@ console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
 console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
 
 
+
+
+Live Demo:
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Flatten a nested array</title>
+</head>
+<body>
+</body>
+
+
+js:
+
+
+
+
+
+var flatten = function(a, shallow,r){
+  if(!r){ r = []}
+   
+if (shallow) {
+  return r.concat.apply(r,a);
+  }
+      
+   for(var i=0; i<a.length; i++){
+        if(a[i].constructor == Array){
+            flatten(a[i],shallow,r);
+        }else{
+            r.push(a[i]);
+        }
+    }
+    return r;
+}
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+
+
  */
