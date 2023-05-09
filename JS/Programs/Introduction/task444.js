effceea93ffc5d79de97324e74ba532a4a553768
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:10:54 +0400

    task

---
+++ JS/Programs/Introduction/task444.js
@@ -0,0 +1,16 @@
+// JavaScript: Compute the union of two arrays
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-22 with Solution
+Write a JavaScript program to compute the union of two arrays.
+
+Sample Data:
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
+[1, 2, 3, 10, 100]
+
+
+ */
