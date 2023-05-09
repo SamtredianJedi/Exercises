1d85339d0e378aaef8ca21d813064a0378571e72
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:15:11 +0400

    task

---
+++ JS/Programs/Introduction/task445.js
@@ -0,0 +1,20 @@
+// JavaScript: Find the difference of two arrays
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-23 with Solution
+Write a JavaScript function to find the difference between two arrays.
+
+Test Data:
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["3", "10", "100"]
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+["6"]
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["3", "10", "100"]
+
+
+ */
