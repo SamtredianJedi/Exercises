dbe7c88fa231b874260752e0a1ecea6d9ae653dd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:08:28 +0400

    update3

--- JS/Programs/Introduction/task443.js
+++ JS/Programs/Introduction/task443.js
@@ -61,4 +61,33 @@ console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
 
 
 
+
+
+
+
+
+ES6 Version:
+
+const flatten = (a, shallow, r) => {
+  if(!r){ r = []}
+   
+if (shallow) {
+  return r.concat(...a);
+  }
+      
+   for(let i=0; i<a.length; i++){
+        if(a[i].constructor == Array){
+            flatten(a[i],shallow,r);
+        }else{
+            r.push(a[i]);
+        }
+    }
+    return r;
+};
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+
+console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+
+
  */
