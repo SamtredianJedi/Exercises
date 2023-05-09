d208ebd18c8f1d3d3651ba04a89bd8efa5919119
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:16:10 +0400

    update2

--- JS/Programs/Introduction/task445.js
+++ JS/Programs/Introduction/task445.js
@@ -33,4 +33,29 @@ HTML Code:
 </html>
 
 
+
+
+
+
+
+JavaScript Code:
+
+function differenceOf2Arrays (array1, array2) {
+var temp = [];
+array1 = array1.toString().split(',').map(Number);
+array2 = array2.toString().split(',').map(Number);
+
+for (var i in array1) {
+if(array2.indexOf(array1[i]) === -1) temp.push(array1[i]);
+}
+for(i in array2) {
+if(array1.indexOf(array2[i]) === -1) temp.push(array2[i]);
+}
+return temp.sort((a,b) => a-b);
+}
+
+console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
+console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+
+
  */
