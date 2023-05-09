7e223cc4b7deb29aae040f5d3001506fce1e57fb
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-09 19:11:28 +0400

    update1

--- JS/Programs/Introduction/task444.js
+++ JS/Programs/Introduction/task444.js
@@ -13,4 +13,23 @@ console.log(union([1, 2, 3], [100, 2, 1, 10]));
 [1, 2, 3, 10, 100]
 
 
+
+
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JS Bin</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
  */
