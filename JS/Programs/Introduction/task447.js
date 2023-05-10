be7650b9509347e2ef959fcd926073045288ad1c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-10 23:40:24 +0400

    task

---
+++ JS/Programs/Introduction/task447.js
@@ -0,0 +1,20 @@
+// JavaScript: Sort the specified array of objects by title value
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-25 with Solution
+Write a JavaScript function to sort the following array of objects by title value.
+
+Sample object:
+
+var library = [ 
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+   ];
+
+   
+ */
