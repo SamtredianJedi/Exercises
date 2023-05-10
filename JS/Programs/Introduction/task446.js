4937754e33cfe7bd4630de6c82d208b2a2d7d08f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-10 23:38:16 +0400

    update4

--- JS/Programs/Introduction/task446.js
+++ JS/Programs/Introduction/task446.js
@@ -82,4 +82,37 @@ console.log(filter_array([NaN, 0, 15, false, -22, '',undefined, 47, null]));
 
 
 
+
+
+
+
+
+Live Demo:
+
+
+
+
+js:
+
+
+function filter_array(test_array) {
+    let index = -1;
+    const arr_length = test_array ? test_array.length : 0;
+    let resIndex = -1;
+    const result = [];
+
+    while (++index < arr_length) {
+        const value = test_array[index];
+
+        if (value) {
+            result[++resIndex] = value;
+        }
+    }
+
+    return result;
+}
+console.log(filter_array([NaN, 0, 15, false, -22, '',undefined, 47, null]));
+
+
+
  */
