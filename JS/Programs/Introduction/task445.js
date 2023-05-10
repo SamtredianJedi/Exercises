869d61a2812a5f09310485bc83cb50cc47027f39
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-10 23:32:44 +0400

    update3

--- JS/Programs/Introduction/task445.js
+++ JS/Programs/Introduction/task445.js
@@ -58,4 +58,30 @@ console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
 console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
 
 
+
+
+ES6 Version:
+
+function differenceOf2Arrays (array1, array2) {
+const temp = [];
+array1 = array1.toString().split(',').map(Number);
+array2 = array2.toString().split(',').map(Number);
+
+for (var i in array1) {
+if(!array2.includes(array1[i])) temp.push(array1[i]);
+}
+for(i in array2) {
+if(!array1.includes(array2[i])) temp.push(array2[i]);
+}
+return temp.sort((a,b) => a-b);
+}
+
+console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
+console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+
+
+
+
+
+
  */
