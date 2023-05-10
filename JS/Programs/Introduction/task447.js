3fc32014791f492735c3e15c045e5743d8bda60c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-10 23:42:16 +0400

    update2

--- JS/Programs/Introduction/task447.js
+++ JS/Programs/Introduction/task447.js
@@ -35,4 +35,28 @@ var library = [
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+var library = [ 
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+   ];
+
+function compare_to_sort(x,y) 
+ {
+  if (x.title < y.title)
+    return -1;
+  if (x.title > y.title)
+    return 1;
+  return 0;
+ }
+
+console.log(library.sort(compare_to_sort));
+
+
  */
