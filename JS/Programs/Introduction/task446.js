48a883872e284220dd47308cea0262c3ba5ab968
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-10 23:35:09 +0400

    task

---
+++ JS/Programs/Introduction/task446.js
@@ -0,0 +1,15 @@
+// JavaScript: Remove null, 0, blank, false, undefined and NaN values from an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ JavaScript Array: Exercise-24 with Solution
+Write a JavaScript function to remove. 'null', '0', '""', 'false', 'undefined' and 'NaN' values from an array.
+
+Sample array: [NaN, 0, 15, false, -22, '',undefined, 47, null]
+Expected result: [15, -22, 47]
+
+
+ */
