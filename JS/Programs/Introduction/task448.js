2e7dc36ab79e0684a44971f22aface0a307ba6a0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-11 17:42:39 +0400

    info

---
+++ JS/Programs/Introduction/task448.js
@@ -0,0 +1,14 @@
+// JavaScript: Find a pair of elements from an specified array whose sum equals a specific target number
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-26 with Solution
+Write a JavaScript program to find a pair of elements (indices of the two numbers) in a given array whose sum equals a specific target number.
+
+Input: numbers= [10,20,10,40,50,60,70], target=50
+Output: 2, 3
+
+
+ */
