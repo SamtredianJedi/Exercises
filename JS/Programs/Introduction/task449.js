821496e164be82bb865f32937385be20d1f0f38a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-11 18:41:20 +0400

    task

---
+++ JS/Programs/Introduction/task449.js
@@ -0,0 +1,16 @@
+// JavaScript: Retrieve the value of a specified property from all elements in an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-27 with Solution
+Write a JavaScript function to retrieve the value of a given property from all elements in an array.
+
+Sample array: [NaN, 0, 15, false, -22, '',undefined, 47, null]
+Expected result: [15, -22, 47]
+
+
+
+ */
