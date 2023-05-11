1cdf4aff2cc93086027b88dab5ab64ed18fee9a9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-11 18:15:43 +0400

    update3

--- JS/Programs/Introduction/task448.js
+++ JS/Programs/Introduction/task448.js
@@ -53,6 +53,34 @@ console.log(twoSum([10,20,10,40,50,60,70],50));
 
 
 
+ES6 Version:
+
+function twoSum(nums, target_num) {
+  const map = [];
+  const indexnum = [];
+  
+  for (let x = 0; x < nums.length; x++)
+  {
+  if (map[nums[x]] != null)
+  {
+  var index = map[nums[x]];
+  indexnum[0] = index;
+  indexnum[1] = x;
+  break;
+  }
+  else
+  {
+  map[target_num - nums[x]] = x;
+  }
+  }
+  return indexnum;
+  }
+console.log(twoSum([10,20,10,40,50,60,70],50));
+
+
+
+
+
 
 
 
