0a54bc46b92a1eb9479ff2ff4c76b227b840b7f9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-11 18:43:55 +0400

    update3

--- JS/Programs/Introduction/task449.js
+++ JS/Programs/Introduction/task449.js
@@ -67,4 +67,40 @@ console.log(property_value(library, 'author'));
 
 
 
+
+
+
+
+ES6 Version:
+
+const library = [ 
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+   ];
+
+function property_value(array, property_key) {
+    const arr = [];
+    let index = -1;
+    const arrlen = array.length;
+    let array_items;
+
+    while (++index < arrlen) {
+		array_items = array[index];
+
+  if (array_items.hasOwnProperty(property_key)) 
+      {
+	 arr[arr.length] = array_items[property_key];
+      }
+	}
+
+    return arr;
+}
+
+console.log(property_value(library, 'title'));
+
+console.log(property_value(library, 'author'));
+
+
+
  */
