c26619e4fbdea6fb5dc27aa4e339ca0e3f0053a1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-11 18:46:19 +0400

    task

---
+++ JS/Programs/Introduction/task450.js
@@ -0,0 +1,18 @@
+// JavaScript: Find the longest common starting substring in a set of strings
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-28 with Solution
+Write a JavaScript function to find the longest common starting substring in a set of strings.
+
+Sample array : console.log(longest_common_starting_substring(['go', 'google']));
+Expected result : "go"
+
+
+
+ */
