f4e745e184b63dedaaa6d50353bde4bc67fb9551
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-12 23:24:42 +0400

    update3

--- JS/Programs/Introduction/task450.js
+++ JS/Programs/Introduction/task450.js
@@ -57,4 +57,28 @@ console.log(longest_common_starting_substring(['abcd', '1234']));
 
 
 
+
+
+ES6 Version:
+
+function longest_common_starting_substring(arr1){
+  const arr= arr1.concat().sort();
+  const a1= arr[0];
+  const a2= arr[arr.length-1];
+  const L= a1.length;
+  let i= 0;
+  while(i< L && a1.charAt(i)=== a2.charAt(i)) i++;
+  return a1.substring(0, i);
+}
+
+console.log(longest_common_starting_substring(['go', 'google'])); 
+
+console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); 
+
+console.log(longest_common_starting_substring(['abcd', '1234']));
+
+
+
+
+
  */
