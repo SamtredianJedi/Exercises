38cac640d6c2fd8ee7b3cd7690850e291b65b130
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-12 23:31:32 +0400

    task

---
+++ JS/Programs/Introduction/task452.js
@@ -0,0 +1,18 @@
+// JavaScript: Merge two arrays and removes all duplicates elements
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-30 with Solution
+Write a JavaScript function that merges two arrays and removes all duplicate elements.
+
+Test data:
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+console.log(merge_array(array1, array2));
+[3, 2, 30, 1]
+
+
+
+ */
