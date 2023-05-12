ab8a84e7ad86133798c18077b2dab6645e104a1c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-12 23:26:55 +0400

    task

---
+++ JS/Programs/Introduction/task451.js
@@ -0,0 +1,18 @@
+// JavaScript: Fill an array with values on supplied bounds
+
+
+/**
+ * 
+ * 
+
+
+JavaScript Array: Exercise-29 with Solution
+Write a JavaScript function to fill an array with values (numeric, string with one character) within supplied bounds.
+
+Test Data:
+console.log(num_string_range('a', "z", 2));
+["a", "c", "e", "g", "i", "k", "m", "o", "q", "s", "u", "w", "y"]
+
+
+
+ */
