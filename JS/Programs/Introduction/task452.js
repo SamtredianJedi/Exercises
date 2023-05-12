119a13bd9b4ac6aa2e93518424166813c86934d4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-12 23:33:05 +0400

    update4

--- JS/Programs/Introduction/task452.js
+++ JS/Programs/Introduction/task452.js
@@ -67,4 +67,37 @@ console.log(merge_array(array1, array2));
 
 
 
+
+
+
+
+ES6 Version:
+
+function merge_array(array1, array2) {
+    const result_array = [];
+    const arr = array1.concat(array2);
+    let len = arr.length;
+    const assoc = {};
+
+    while(len--) {
+        const item = arr[len];
+
+        if(!assoc[item]) 
+        { 
+            result_array.unshift(item);
+            assoc[item] = true;
+        }
+    }
+
+    return result_array;
+}
+
+
+const array1 = [1, 2, 3];
+
+const array2 = [2, 30, 1];
+
+console.log(merge_array(array1, array2));
+
+
  */
