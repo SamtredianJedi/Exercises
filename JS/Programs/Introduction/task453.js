8b80866714428c0b72d12651848dc5f4ad4bbda4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:38:36 +0400

    task

---
+++ JS/Programs/Introduction/task453.js
@@ -0,0 +1,15 @@
+// JavaScript: Remove a specific element from an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-31 with Solution
+Write a JavaScript function to remove a specific element from an array.
+
+Test data:
+console.log(remove_array_element([2, 5, 9, 6], 5));
+[2, 9, 6]
+
+
+ */
