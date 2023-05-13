77d0613cc2f595374d2456d97502d85931929c82
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:40:59 +0400

    update4

--- JS/Programs/Introduction/task453.js
+++ JS/Programs/Introduction/task453.js
@@ -67,4 +67,46 @@ function remove_array_element(array, n)
 console.log(remove_array_element([2, 5, 9, 6], 5));
 
 
+
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to remove a specific element from an array</title>
+</head>
+<body>
+​
+</body>
+
+
+
+js:
+
+
+
+
+
+
+function remove_array_element(array, n)
+ {
+   var index = array.indexOf(n);
+   if (index > -1) {
+    array.splice(index, 1);
+}
+   return array;
+ }
+​
+console.log(remove_array_element([2, 5, 9, 6], 5));
+
+
+
  */
