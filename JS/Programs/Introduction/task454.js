3dcfe77c63ed15f5cc20d42b62d5c521c394c269
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:42:34 +0400

    task

---
+++ JS/Programs/Introduction/task454.js
@@ -0,0 +1,20 @@
+// JavaScript: Find if an array contains a specific element
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-32 with Solution
+Write a JavaScript function to find an array containing a specific element.
+
+Test data:
+arr = [2, 5, 9, 6];
+console.log(contains(arr, 5));
+[True]
+
+
+
+
+
+ */
