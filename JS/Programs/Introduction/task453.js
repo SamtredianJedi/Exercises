6e183b24a751754d6601804d154aab93d0a6de6c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:39:11 +0400

    update1

--- JS/Programs/Introduction/task453.js
+++ JS/Programs/Introduction/task453.js
@@ -12,4 +12,21 @@ console.log(remove_array_element([2, 5, 9, 6], 5));
 [2, 9, 6]
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to remove a specific element from an array</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
  */
