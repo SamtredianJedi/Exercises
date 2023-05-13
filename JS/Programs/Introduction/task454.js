bc88ecad19993ec8c4a336fb2e6b36b29a9e9aae
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:45:00 +0400

    update4

--- JS/Programs/Introduction/task454.js
+++ JS/Programs/Introduction/task454.js
@@ -74,4 +74,30 @@ console.log(contains(arr, 5));
 
 
 
+
+
+
+
+Live Demo:
+
+
+js:
+
+
+
+
+function contains(arr, element) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+arr = [2, 5, 9, 6];
+console.log(contains(arr, 5));
+
+
+
+
  */
