72ec843228d316efdfb9d890f6284ec737796571
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:46:38 +0400

    task

---
+++ JS/Programs/Introduction/task555.js
@@ -0,0 +1,10 @@
+// JavaScript: Empty an array keeping the original
+
+/**
+ * 
+ * 
+ * JavaScript Array: Exercise-33 with Solution
+Write a JavaScript script to empty an array while keeping the original.
+
+
+ */
