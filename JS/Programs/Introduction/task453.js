6e7d5ed95e6b1ef9622e40f0712f7be5bfa5f8b9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-13 23:39:40 +0400

    update2

--- JS/Programs/Introduction/task453.js
+++ JS/Programs/Introduction/task453.js
@@ -29,4 +29,23 @@ HTML Code:
 </html>
 
 
+
+
+
+
+
+JavaScript Code:
+
+function remove_array_element(array, n)
+ {
+   var index = array.indexOf(n);
+   if (index > -1) {
+    array.splice(index, 1);
+}
+   return array;
+ }
+
+console.log(remove_array_element([2, 5, 9, 6], 5));
+
+
  */
