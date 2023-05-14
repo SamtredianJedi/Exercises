ee2bc2b9b25fd0005da6a4bb3f8d2a83744e793c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-14 23:27:52 +0400

    update2

--- JS/Programs/Introduction/task557.js
+++ JS/Programs/Introduction/task557.js
@@ -25,4 +25,25 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+
+
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
+
+
+
  */
