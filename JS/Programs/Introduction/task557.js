67be8310ea461e170bd4b042c106e01e2af6c153
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-14 23:28:27 +0400

    update4

--- JS/Programs/Introduction/task557.js
+++ JS/Programs/Introduction/task557.js
@@ -46,4 +46,26 @@ console.log(random_item(items));
 
 
 
+
+
+
+
+ES6 Version:
+
+
+
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+const items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
+
+
+
+
  */
