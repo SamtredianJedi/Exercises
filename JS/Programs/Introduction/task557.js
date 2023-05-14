2f46c8ba647740d1b01f219e7da6ff6a951df991
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-14 23:26:39 +0400

    task

---
+++ JS/Programs/Introduction/task557.js
@@ -0,0 +1,11 @@
+// JavaScript: Get a random item from an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-35 with Solution
+Write a JavaScript function to get random items from an array.
+
+
+ */
