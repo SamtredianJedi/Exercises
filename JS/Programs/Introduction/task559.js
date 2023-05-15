1f4b83e4e6efb01f90124a6f97d77235c4a5f0ae
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-15 19:26:48 +0400

    task

---
+++ JS/Programs/Introduction/task559.js
@@ -0,0 +1,18 @@
+// JavaScript: Create a specified number of elements and pre-filled string value arrayJavaScript: Create a specified number of elements and pre-filled string value array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-37 with Solution
+Write a JavaScript function to create a specified number of elements with a pre-filled string value array.
+
+Test Data:
+console.log(array_filled(3, 'default value'));
+["default value", "default value", "default value"]
+console.log(array_filled(4, 'password'));
+["password", "password", "password", "password"]
+
+
+ */
