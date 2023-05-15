72544e634d334eb6053c0fa81c1dc1ea30eb3aea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-15 19:20:35 +0400

    task

---
+++ JS/Programs/Introduction/task558.js
@@ -0,0 +1,18 @@
+// JavaScript: Create a specified number of elements and pre-filled numeric value array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-36 with Solution
+Write a JavaScript function to create a specified number of elements with a pre-filled numeric value array.
+
+Test Data:
+console.log(array_filled(6, 0));
+[0, 0, 0, 0, 0, 0]
+console.log(array_filled(4, 11));
+[11, 11, 11, 11]
+
+
+
+ */
