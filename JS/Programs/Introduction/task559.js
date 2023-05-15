a622f9d1e2e55a9575cbf97c9009009f4170ff6a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-15 19:28:40 +0400

    update3

--- JS/Programs/Introduction/task559.js
+++ JS/Programs/Introduction/task559.js
@@ -51,4 +51,20 @@ console.log(array_filled(4, 'password'));
 
 
 
+
+
+
+
+ES6 Version:
+
+function array_filled(n, val)
+{
+    return Array(...Array(n)).map(String.prototype.valueOf,val);
+}
+
+console.log(array_filled(3, 'default value'));
+console.log(array_filled(4, 'password'));
+Live Demo:
+
+
  */
