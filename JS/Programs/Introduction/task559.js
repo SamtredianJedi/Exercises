651cd9f507669034778cf08e25d977d4147fd76d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-15 19:28:05 +0400

    update2

--- JS/Programs/Introduction/task559.js
+++ JS/Programs/Introduction/task559.js
@@ -35,4 +35,20 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code :
+
+function array_filled(n, val)
+{
+    return Array.apply(null, Array(n)).map(String.prototype.valueOf,val);
+}
+
+console.log(array_filled(3, 'default value'));
+console.log(array_filled(4, 'password'));
+
+
+
  */
