905b1d48711d98b10c8d24599b3809d8eae43618
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-15 19:24:48 +0400

    update4

--- JS/Programs/Introduction/task558.js
+++ JS/Programs/Introduction/task558.js
@@ -70,4 +70,31 @@ console.log(array_filled(4, 11));
 
 
 
+
+
+
+
+Live Demo:
+
+
+
+function array_filled(n, val)
+{
+    return Array.apply(null, Array(n)).map(Number.prototype.valueOf,val);
+}
+
+console.log(array_filled(6, 0));
+console.log(array_filled(4, 11));
+
+
+
+
+
+
+
+
+js:
+
+
+
  */
