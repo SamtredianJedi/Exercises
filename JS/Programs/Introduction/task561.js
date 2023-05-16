31a683159e48650c273fbba3ff1a7463a054e588
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:42:41 +0400

    update4

--- JS/Programs/Introduction/task561.js
+++ JS/Programs/Introduction/task561.js
@@ -73,4 +73,31 @@ console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
 
 
 
+
+
+
+
+Live Demo:
+
+
+js:
+
+
+
+
+function filter_array_values(arr) {
+  arr = arr.filter(isEligible);
+  return arr;
+}
+​
+function isEligible(value) {
+  if(value !== false || value !== null || value !== 0 || value !== "") {
+    return value;
+  }
+}
+​
+console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+
+
+
  */
