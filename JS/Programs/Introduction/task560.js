227108a073f32744469de057c93a89455817c614
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:38:20 +0400

    update4

--- JS/Programs/Introduction/task560.js
+++ JS/Programs/Introduction/task560.js
@@ -90,4 +90,42 @@ console.log(move([10, 20, 30, 40, 50], -1, -2));
 
 
 
+
+
+
+
+
+Live Demo :
+
+
+js:
+
+
+
+
+
+function move(arr, old_index, new_index) {
+    while (old_index < 0) {
+        old_index += arr.length;
+    }
+    while (new_index < 0) {
+        new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length;
+        while ((k--) + 1) {
+            arr.push(undefined);
+        }
+    }
+     arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);  
+   return arr;
+}
+
+console.log(move([10, 20, 30, 40, 50], 0, 2));
+
+console.log(move([10, 20, 30, 40, 50], -1, -2));
+
+
+
+
  */
