19cbd4a4407246785608cb432c701d0f4b4973ce
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:41:25 +0400

    update2

--- JS/Programs/Introduction/task561.js
+++ JS/Programs/Introduction/task561.js
@@ -31,4 +31,25 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+function filter_array_values(arr) {
+  arr = arr.filter(isEligible);
+  return arr;
+}
+
+function isEligible(value) {
+  if(value !== false || value !== null || value !== 0 || value !== "") {
+    return value;
+  }
+}
+
+console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+
+
+
  */
