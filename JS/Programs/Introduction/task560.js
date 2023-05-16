f5f34517d9fc55b2842e4bbea3c616483dc40d34
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:35:35 +0400

    update1

--- JS/Programs/Introduction/task560.js
+++ JS/Programs/Introduction/task560.js
@@ -14,4 +14,22 @@ console.log(move([10, 20, 30, 40, 50], -1, -2));
 [10, 20, 30, 50, 40]
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to move an array element from one position to another.</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
