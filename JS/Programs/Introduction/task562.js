0b22fb3bc96f8068fd4922d7040a8c89dd3fe9e3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:43:59 +0400

    task

---
+++ JS/Programs/Introduction/task562.js
@@ -0,0 +1,18 @@
+// JavaScript: Generate an array of specified length, the content of the array is integer numbers, increase by one from starting
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-40 with Solution
+Write a JavaScript function to generate an array of integer numbers, increasing one from the starting position, of a specified length.
+
+Test Data :
+console.log(array_range(1, 4));
+[1, 2, 3, 4]
+console.log(array_range(-6, 4));
+[-6, -5, -4, -3]
+
+
+ */
