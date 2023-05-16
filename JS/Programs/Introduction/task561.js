4cb11c11919679546cd2f15d376e149f1bcf8eea
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-16 23:40:19 +0400

    task

---
+++ JS/Programs/Introduction/task561.js
@@ -0,0 +1,16 @@
+// JavaScript: Filter false, null, 0 and blank values from an array
+
+/**
+ * 
+ * 
+
+JavaScript Array: Exercise-39 with Solution
+Write a JavaScript function to filter false, null, 0 and blank values from an array.
+
+Test Data :
+console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+[58, "abcd", true]
+
+
+
+ */
