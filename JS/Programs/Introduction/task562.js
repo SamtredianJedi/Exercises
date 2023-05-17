e9543f582275a39675791ed5e8e9edda746fcfde
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-17 22:50:10 +0400

    update4

--- JS/Programs/Introduction/task562.js
+++ JS/Programs/Introduction/task562.js
@@ -153,4 +153,36 @@ console.log(array_range(-6, 4));
 
 
 
+Live Demo:
+
+
+js:
+
+
+function array_range(start, len) 
+     {
+		var arr = new Array(len);
+		for (var i = 0; i < len; i++, start++) 
+        {
+			arr[i] = start;
+		}
+      		return arr;
+}
+
+
+console.log(array_range(1, 4));
+
+console.log(array_range(-6, 4));
+
+
+
+
+
+
+
+
+
+
+
+
  */
