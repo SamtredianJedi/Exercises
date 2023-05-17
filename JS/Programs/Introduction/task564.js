8993462ef1ceed44aab3011924ad8f1014d8d403
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-17 22:56:08 +0400

    task

---
+++ JS/Programs/Introduction/task564.js
@@ -0,0 +1,20 @@
+// JavaScript: Find the unique elements from two arrays
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-42 with Solution
+Write a JavaScript function to find unique elements in two arrays.
+
+Test Data :
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["1", "2", "3", "10", "100"]
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+["1", "2", "3", "4", "5", "6"]
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+["1", "2", "3", "10", "100"]
+
+
+
+ */
