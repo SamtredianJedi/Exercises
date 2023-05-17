e119158289952a60289c4187f7665fcfa66bae71
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-17 22:53:02 +0400

    update2

--- JS/Programs/Introduction/task563.js
+++ JS/Programs/Introduction/task563.js
@@ -36,4 +36,37 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function rangeBetwee(start, end) 
+{
+  
+		if (start > end) {
+			var arr = new Array(start - end + 1);
+			for (var i = 0; i < arr.length; i++, start--) {
+				resarrult[i] = start;
+			}
+			return arr;
+		} 
+       else 
+       {
+			var arro = new Array(end-start+1);
+         
+		for (var j = 0; j < arro.length; j++, start++) 
+        {
+			arro[j] = start;
+		}
+      		return arro;
+		}
+	}
+
+console.log(rangeBetwee(4, 7));
+console.log(rangeBetwee(-4, 7));
+
+
+
  */
