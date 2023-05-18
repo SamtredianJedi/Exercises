50de9b567960f5d3ee6dd42818f1df262770b596
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:19:29 +0400

    update2

--- JS/Programs/Introduction/task565.js
+++ JS/Programs/Introduction/task565.js
@@ -36,4 +36,31 @@ console.log(unzip([['a', 1, true], ['b', 2]]));
 
 
 
+
+
+
+
+Live Demo :
+
+
+js:
+
+
+
+
+//Source:https://bit.ly/3hEZdCl
+//unzip
+const unzip = arr =>
+  arr.reduce(
+    (acc, val) => (val.forEach((v, i) => acc[i].push(v)), acc),
+    Array.from({
+      length: Math.max(...arr.map(x => x.length))
+    }).map(x => [])
+  );
+console.log(unzip([['a', 1, true], ['b', 2, false]]));
+console.log(unzip([['a', 1, true], ['b', 2]]));
+
+
+
+
  */
