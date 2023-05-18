69afa93b5a59fbecbed7fae7f75dd763679ab067
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:30:48 +0400

    update1

--- JS/Programs/Introduction/task568.js
+++ JS/Programs/Introduction/task568.js
@@ -16,4 +16,31 @@ WARNING: This function's execution time increases exponentially with each array 
 
 
 
+
+
+
+
+
+JavaScript Code :
+
+//Source:https://bit.ly/3hEZdCl
+const permutations = arr => {
+  if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+  return arr.reduce(
+    (acc, item, i) =>
+      acc.concat(
+        permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+          item,
+          ...val,
+        ])
+      ),
+    []
+  );
+};
+console.log(permutations([1, 33, 5]));
+console.log(permutations([1, 3, 5, 7]));
+console.log(permutations([2, 4]));
+
+
+
  */
