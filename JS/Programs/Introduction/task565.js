d61ca607f7a654c94f4b790ff279e99d7e9a2f60
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:17:20 +0400

    task

---
+++ JS/Programs/Introduction/task565.js
@@ -0,0 +1,18 @@
+// JavaScript: Ungrouping the elements in an array produced by zip
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-43 with Solution
+Write a JavaScript function to create an array of arrays, ungrouping the elements in an array produced by zip.
+
+Use Math.max(), Function.prototype.apply() to get the longest subarray in the array, Array.prototype.map() to make each element an array.
+Use Array.prototype.reduce() and Array.prototype.forEach() to map grouped values to individual arrays.
+
+
+
+ */
