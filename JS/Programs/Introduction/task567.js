11849f58b036ab970d9ba329bc41cfdb307e2d62
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:27:15 +0400

    task

---
+++ JS/Programs/Introduction/task567.js
@@ -0,0 +1,17 @@
+// JavaScript: Find all unique values in an array
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-45 with Solution
+Write a JavaScript program to find all the unique values in a set of numbers.
+
+Create a new Set() from the given array to discard duplicated values.
+Use the spread operator (...) to convert it back to an array
+
+
+
+ */
