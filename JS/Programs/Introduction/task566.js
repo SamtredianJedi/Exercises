78e1dd0f4dbb5ea9ef86181ed7ec6abbf7e1d7db
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:25:54 +0400

    update2

--- JS/Programs/Introduction/task566.js
+++ JS/Programs/Introduction/task566.js
@@ -36,4 +36,31 @@ console.log(indexOn([
 
 
 
+
+
+
+
+
+Live Demo :
+
+
+
+
+//Source:https://bit.ly/3hEZdCl
+//Add the key-value pair to the object.
+const indexOn = (arr, key) =>
+  arr.reduce((obj, v) => {
+    const { [key]: id, ...data } = v;
+    obj[id] = data;
+    return obj;
+  }, {});
+ 
+console.log(indexOn([
+  { id: 10, name: 'apple' },
+  { id: 20, name: 'orange' }
+], x => x.id));
+
+
+
+
  */
