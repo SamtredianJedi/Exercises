140c2e36def45d973cc6ed89475dce5944d59993
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:24:34 +0400

    task

---
+++ JS/Programs/Introduction/task566.js
@@ -0,0 +1,17 @@
+// JavaScript: Creates an object from an array, using the specified key and excluding it from each value
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-44 with Solution
+Write a JavaScript function to create an object from an array, using the specified key and excluding it from each value.
+
+Use Array.prototype.reduce() to create an object from arr.
+Use object destructuring to get the value of the given key and the associated data and add the key-value pair to the object.
+
+
+
+ */
