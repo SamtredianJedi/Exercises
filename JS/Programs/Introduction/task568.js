d787b0d6d81ad4496c4b2b1d967cba4743c41168
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:31:38 +0400

    update2

--- JS/Programs/Introduction/task568.js
+++ JS/Programs/Introduction/task568.js
@@ -43,4 +43,55 @@ console.log(permutations([2, 4]));
 
 
 
+
+
+
+
+Live Demo :
+
+
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Find all unique values in an array</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+
+
+
+//Source:https://bit.ly/3hEZdCl
+const permutations = arr => {
+  if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+  return arr.reduce(
+    (acc, item, i) =>
+      acc.concat(
+        permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+          item,
+          ...val,
+        ])
+      ),
+    []
+  );
+};
+console.log(permutations([1, 33, 5]));
+console.log(permutations([1, 3, 5, 7]));
+console.log(permutations([2, 4]));
+
+
+
  */
