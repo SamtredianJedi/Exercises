e24252415b4700e23c8888d387eb7a2af7c00862
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:28:15 +0400

    update2

--- JS/Programs/Introduction/task567.js
+++ JS/Programs/Introduction/task567.js
@@ -14,4 +14,39 @@ Use the spread operator (...) to convert it back to an array
 
 
 
+
+
+
+
+Live Demo :
+
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Number of trailing zeroes in a factorial</title>
+</head>
+<body>
+
+</body>
+</html>
+
+js:
+
+
+
+
+const unique_Elements = arr => [...new Set(arr)];
+console.log(unique_Elements([1, 2, 2, 3, 4, 4, 5]));
+console.log(unique_Elements([1, 2, 3, 4, 5]));
+console.log(unique_Elements([1, -2, -2, 3, 4, -5, -6, -5]));
+
+
+
+
  */
