c5cb61ca1184f5f0e75f2cd93dc97dd8b7ed4888
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:18:25 +0400

    update1

--- JS/Programs/Introduction/task565.js
+++ JS/Programs/Introduction/task565.js
@@ -15,4 +15,25 @@ Use Array.prototype.reduce() and Array.prototype.forEach() to map grouped values
 
 
 
+
+
+
+
+
+JavaScript Code :
+
+//Source:https://bit.ly/3hEZdCl
+//unzip
+const unzip = arr =>
+  arr.reduce(
+    (acc, val) => (val.forEach((v, i) => acc[i].push(v)), acc),
+    Array.from({
+      length: Math.max(...arr.map(x => x.length))
+    }).map(x => [])
+  );
+console.log(unzip([['a', 1, true], ['b', 2, false]]));
+console.log(unzip([['a', 1, true], ['b', 2]]));
+
+
+
  */
