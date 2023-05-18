ad781df100c6bd53457f2f37c4d2090c329cded8
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-18 13:25:10 +0400

    update1

--- JS/Programs/Introduction/task566.js
+++ JS/Programs/Introduction/task566.js
@@ -14,4 +14,26 @@ Use object destructuring to get the value of the given key and the associated da
 
 
 
+
+
+
+
+JavaScript Code :
+
+//Source:https://bit.ly/3hEZdCl
+//Add the key-value pair to the object.
+const indexOn = (arr, key) =>
+  arr.reduce((obj, v) => {
+    const { [key]: id, ...data } = v;
+    obj[id] = data;
+    return obj;
+  }, {});
+ 
+console.log(indexOn([
+  { id: 10, name: 'apple' },
+  { id: 20, name: 'orange' }
+], x => x.id));
+
+
+
  */
