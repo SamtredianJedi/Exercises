ba8b217e90abc1116545a234d5eb3aa81e1d15cf
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:01:50 +0400

    task

---
+++ JS/Programs/Introduction/task569.js
@@ -0,0 +1,20 @@
+// JavaScript: Remove all falsy values from an object or array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-47 with Solution
+Write a JavaScript program to remove all false values from an object or array.
+
+Use recursion.
+Initialize the iterable data, using Array.isArray(), Array.prototype.filter() and Boolean for arrays in order to avoid sparse arrays.
+Use Object.keys() and Array.prototype.reduce() to iterate over each key with an appropriate initial value.
+Use Boolean to determine the truthiness of each key's value and add it to the accumulator if it's truthy.
+Use typeof to determine if a given value is an object and call the function again to deeply compact it.
+
+
+
+
+ */
