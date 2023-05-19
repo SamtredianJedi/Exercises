4fc7c9562258c0bb3e7a7cd5492c1b2b1985db2d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:13:31 +0400

    task

---
+++ JS/Programs/Introduction/task572.js
@@ -0,0 +1,18 @@
+// JavaScript: Sum all numbers of an array of mixed data types
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-50 with Solution
+Write a JavaScript program that takes an array with mixed data type and calculates the sum of all numbers.
+
+Test Data:
+
+([2, "11", 3, "a2", false, 5, 7, 1]) -> 18
+([2, 3, 0, 5, 7, 8, true, false]) -> 25
+
+
+
+ */
