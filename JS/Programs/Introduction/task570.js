70ca6ba667345950632aad7e583021263a00c3f6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:07:58 +0400

    update2

--- JS/Programs/Introduction/task570.js
+++ JS/Programs/Introduction/task570.js
@@ -15,4 +15,29 @@ Test Data:
 
 
 
+
+
+
+
+JavaScript Code :
+
+function test(arr_nums) 
+{
+   for (n of arr_nums)
+   {
+    if (n == 1 | n > 2 & n % 2 == 0) 
+      return false;
+  }
+  return true;
+}
+
+nums = [2, 3, 5, 7]
+console.log("Original array of integers: "+nums)
+console.log("In the said array check every numbers are prime or not! "+test(nums));
+nums = [2, 3, 5, 7, 8]
+console.log("Original array of integers: "+nums)
+console.log("In the said array check every numbers are prime or not! "+test(nums));
+
+
+
  */
