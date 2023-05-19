6967946234588634531bd50be10758e5b6b42604
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:11:03 +0400

    update1

--- JS/Programs/Introduction/task571.js
+++ JS/Programs/Introduction/task571.js
@@ -14,4 +14,24 @@ Test Data:
 (2,3,0,5,7,8,-2,-4) -> 0
 
 
+
+
+
+
+JavaScript Code :
+
+function test(arr_nums) 
+ {
+  return arr_nums.sort((x,y) => y - x)[arr_nums.length-3]
+}
+
+nums = [2, 3, 5, 7, 1]
+console.log("Original array of numbers: "+nums)
+console.log("Third smallest number of the said array of numbers: "+test(nums));
+nums = [2, 3, 0, 5, 7, 8, -2, -4]
+console.log("Original array of numbers: "+nums)
+console.log("Third smallest number of the said array of numbers: "+test(nums));
+
+
+
  */
