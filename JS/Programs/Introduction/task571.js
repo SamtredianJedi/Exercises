d7d3c02af252c81fa9e7235444cce8d528d82eda
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:10:29 +0400

    task

---
+++ JS/Programs/Introduction/task571.js
@@ -0,0 +1,17 @@
+// JavaScript: Third smallest number of an array of numbers
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-49 with Solution
+Write a JavaScript program that takes an array of numbers and returns the third smallest number.
+
+Test Data:
+
+(2,3,5,7,1) -> 3
+(2,3,0,5,7,8,-2,-4) -> 0
+
+
+ */
