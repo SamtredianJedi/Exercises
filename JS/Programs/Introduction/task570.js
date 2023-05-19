5fd77db7e019c64c262bea39e31052199134b7c6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:07:19 +0400

    task

---
+++ JS/Programs/Introduction/task570.js
@@ -0,0 +1,18 @@
+// JavaScript: Check every numbers are prime or not in an array
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-48 with Solution
+Write a JavaScript program that takes an array of integers and returns false if every number is not prime. Otherwise, return true.
+
+Test Data:
+
+([2,3,5,7]) -> true
+([2,3,5,7,8]) -> false
+
+
+
+ */
