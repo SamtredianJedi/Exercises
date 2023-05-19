eb2003aad2f3f50a38a0b82eaca905b0198985d1
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-19 23:12:01 +0400

    update2

--- JS/Programs/Introduction/task571.js
+++ JS/Programs/Introduction/task571.js
@@ -34,4 +34,48 @@ console.log("Third smallest number of the said array of numbers: "+test(nums));
 
 
 
+
+
+
+
+
+Live Demo :
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Third smallest number of an array of numbers</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+
+
+
+function test(arr_nums) 
+ {
+  return arr_nums.sort((x,y) => y - x)[arr_nums.length-3]
+}
+​
+nums = [2, 3, 5, 7, 1]
+console.log("Original array of numbers: "+nums)
+console.log("Third smallest number of the said array of numbers: "+test(nums));
+nums = [2, 3, 0, 5, 7, 8, -2, -4]
+console.log("Original array of numbers: "+nums)
+console.log("Third smallest number of the said array of numbers: "+test(nums));
+​
+
+
+
  */
