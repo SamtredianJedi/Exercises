b7de3c7d4695b500c38cadd0d286f0e134fda292
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:16:58 +0400

    update2

--- JS/Programs/Introduction/task574.js
+++ JS/Programs/Introduction/task574.js
@@ -47,4 +47,57 @@ console.log("Find all indexes of NaN of the said array: "+result);
 
 
 
+
+
+
+
+Live Demo :
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Find all indexes of NaN in an array</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+js:
+
+
+
+
+function test(arr_nums){
+    return arr_nums.map(function(el, i){
+        if(isNaN(el)) 
+          return i;
+        return false;
+    }).filter(function(el){
+        return el;
+    });
+}
+arr_nums = [2, NaN, 8, 16, 32]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+arr_nums = [2, 4, NaN, 16, 32, NaN]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+arr_nums = [2, 4, 16, 32]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+​
+
+
+
  */
