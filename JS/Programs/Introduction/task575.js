52475aa6f824d9e381bfbbf7d537fcced96528fa
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:19:58 +0400

    update3

--- JS/Programs/Introduction/task575.js
+++ JS/Programs/Introduction/task575.js
@@ -63,4 +63,20 @@ console.log("Number of arrays inside the said array: "+test(arr_nums));
 
 
 
+
+
+
+
+JavaScript Code-2:
+
+function test(arr_nums){
+    return arr_nums.filter(n=> n.length).length;  
+}
+arr_nums = [2,8,[6],3,3,5,3,4,[5,4]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+arr_nums = [2,8,[6,3,3],[4],5,[3,4,[5,4]]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+
+
+
  */
