28c73b91f00a72b1a0623de63509ffb14f829ef6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:15:46 +0400

    task

---
+++ JS/Programs/Introduction/task574.js
@@ -0,0 +1,19 @@
+// JavaScript: Find all indexes of NaN in an array
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-52 with Solution
+Write a JavaScript program to get all the indexes where NaN is found in a given array of numbers and NaN.
+
+Test Data:
+
+([2, NaN, 8, 16, 32]) -> [1]
+([2, 4, NaN, 16, 32, NaN]) -> [2,5]
+([2, 4, 16, 32]) ->[]
+
+
+
+ */
