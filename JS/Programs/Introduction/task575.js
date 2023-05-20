61e89be40387d8aab0a95e1c6c2211b6e1f8079d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:18:42 +0400

    update1

--- JS/Programs/Introduction/task575.js
+++ JS/Programs/Introduction/task575.js
@@ -13,4 +13,17 @@ Test Data:
 ([2,8,[6,3,3],[4],5,[3,4,[5,4]]]) -> 3
 
 
+
+JavaScript Code-1:
+
+function test(arr_nums){
+	return arr_nums.filter(n=>Array.isArray(n)).length;
+}
+arr_nums = [2,8,[6],3,3,5,3,4,[5,4]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+arr_nums = [2,8,[6,3,3],[4],5,[3,4,[5,4]]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+
+
+
  */
