6d0e912929e6eb89e71f604115df1f888f34dc74
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:18:17 +0400

    task

---
+++ JS/Programs/Introduction/task575.js
@@ -0,0 +1,16 @@
+// JavaScript: Number of arrays inside an array
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-53 with Solution
+Write a JavaScript program to count the number of arrays inside a given array.
+
+Test Data:
+
+([2,8,[6],3,3,5,3,4,[5,4]]) -> 2
+([2,8,[6,3,3],[4],5,[3,4,[5,4]]]) -> 3
+
+
+ */
