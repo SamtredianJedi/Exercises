a9b33a1538fd0354682712872526da1ae2830682
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:16:16 +0400

    update1

--- JS/Programs/Introduction/task574.js
+++ JS/Programs/Introduction/task574.js
@@ -16,4 +16,35 @@ Test Data:
 
 
 
+
+
+
+
+JavaScript Code :
+
+function test(arr_nums){
+    return arr_nums.map(function(el, i){
+        if(isNaN(el)) 
+          return i;
+        return false;
+    }).filter(function(el){
+        return el;
+    });
+}
+arr_nums = [2, NaN, 8, 16, 32]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+arr_nums = [2, 4, NaN, 16, 32, NaN]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+arr_nums = [2, 4, 16, 32]
+console.log("Original array: "+arr_nums)
+result = test(arr_nums)
+console.log("Find all indexes of NaN of the said array: "+result);
+
+
+
+
  */
