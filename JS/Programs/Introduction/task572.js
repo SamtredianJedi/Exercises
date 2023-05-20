1e5783327c65addd6f0fd942f83e18b900b01201
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:08:02 +0400

    update2

--- JS/Programs/Introduction/task572.js
+++ JS/Programs/Introduction/task572.js
@@ -39,4 +39,49 @@ console.log("Sum all numbers of the said array: "+test(arr_mix));
 
 
 
+
+
+Live Demo :
+
+
+html:
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Sum all numbers of an array of mixed data types</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+
+js:
+
+
+
+
+function test(arr_mix) {
+  var total = 0;
+  for(var i=0;i<=arr_mix.length;i++){
+    if (typeof arr_mix[i] === "number")
+      total=total+arr_mix[i];
+  }
+return total; 
+}
+
+arr_mix = [2, "11", 3, "a2", false, 5, 7, 1]
+console.log("Original array: "+arr_mix)
+console.log("Sum all numbers of the said array: "+test(arr_mix));
+arr_mix = [2, 3, 0, 5, 7, 8, true, false]
+console.log("Original array: "+arr_mix)
+console.log("Sum all numbers of the said array: "+test(arr_mix));
+
+
+
+
  */
