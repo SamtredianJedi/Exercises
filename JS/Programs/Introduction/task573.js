2aae963df6f7f6c382afae380ad4b7225c9b46f6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:09:59 +0400

    update1

--- JS/Programs/Introduction/task573.js
+++ JS/Programs/Introduction/task573.js
@@ -18,4 +18,33 @@ A factor chain is an array in which the previous element is a factor of the next
 
 
 
+
+
+JavaScript Code :
+
+function test(nums) { 
+  for (var i = 0; i < nums.length - 1; i++) 
+  {
+    if (nums[i+1] % nums[i] != 0) {
+      return false;            
+    }
+  }
+  return true;
+}
+nums = [2, 4, 8, 16, 32]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+nums = [2, 4, 16, 32, 64]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+nums = [2, 4, 16, 32, 68]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+
+
+
+
  */
