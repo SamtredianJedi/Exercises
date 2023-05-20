3b81c19833659b055c43cde30b8d8372af876995
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:19:31 +0400

    update2

--- JS/Programs/Introduction/task575.js
+++ JS/Programs/Introduction/task575.js
@@ -26,4 +26,41 @@ console.log("Number of arrays inside the said array: "+test(arr_nums));
 
 
 
+
+
+Live Demo :
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Number of arrays inside an array</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+js:
+
+
+
+
+function test(arr_nums){
+  return arr_nums.filter(n=>Array.isArray(n)).length;
+}
+arr_nums = [2,8,[6],3,3,5,3,4,[5,4]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+arr_nums = [2,8,[6,3,3],[4],5,[3,4,[5,4]]]
+console.log("Number of arrays inside the said array: "+test(arr_nums));
+​
+
+
+
  */
