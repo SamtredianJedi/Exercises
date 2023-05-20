d5e2546a01f585d2bba5a27d83ce0f7fa7e5f0f5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:09:30 +0400

    task

---
+++ JS/Programs/Introduction/task573.js
@@ -0,0 +1,21 @@
+// JavaScript: Check if an array is a factor chain
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Array: Exercise-51 with Solution
+Write a JavaScript program to check if an array is a factor chain or not.
+
+A factor chain is an array in which the previous element is a factor of the next consecutive element. The following is a factor chain:
+[2, 4, 8, 16, 32]
+// 2 is a factor of 4
+// 4 is a factor of 8
+// 8 is a factor of 16
+// 16 is a factor of 32
+
+
+
+ */
