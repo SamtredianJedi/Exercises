24ff85e33991f2ab8b9d28a49ec7fa0f220b502e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-20 23:10:46 +0400

    update2

--- JS/Programs/Introduction/task573.js
+++ JS/Programs/Introduction/task573.js
@@ -47,4 +47,54 @@ console.log(test(nums));
 
 
 
+
+
+Live Demo :
+
+
+html:
+
+
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to Check if an array is a factor chain</title>
+</head>
+<body>
+​
+</body>
+</html>
+
+
+js:
+
+
+
+
+function test(nums) { 
+  for (var i = 0; i < nums.length - 1; i++) 
+  {
+    if (nums[i+1] % nums[i] != 0) {
+      return false;            
+    }
+  }
+  return true;
+}
+nums = [2, 4, 8, 16, 32]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+nums = [2, 4, 16, 32, 64]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+nums = [2, 4, 16, 32, 68]
+console.log("Original array: ",nums)
+console.log("Check the said array is a factor chain or not?");
+console.log(test(nums));
+
+
+
  */
