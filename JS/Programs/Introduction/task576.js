39a8fb6b179443397a08e6656fb0eb95cad451c4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:22:18 +0400

    update2

--- JS/Programs/Introduction/task576.js
+++ JS/Programs/Introduction/task576.js
@@ -38,4 +38,24 @@ HTML Code:
 
 
 
+
+
+
+
+
+JavaScript Code:
+
+var is_date = function(input) {
+  if ( Object.prototype.toString.call(input) === "[object Date]" ) 
+    return true;
+  return false;   
+    };
+
+console.log(is_date("October 13, 2014 11:13:00"));
+console.log(is_date(new Date(86400000)));
+console.log(is_date(new Date(99,5,24,11,33,30,0)));
+console.log(is_date([1, 2, 4, 0]));
+
+
+
  */
