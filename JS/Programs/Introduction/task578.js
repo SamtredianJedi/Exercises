7e55123c6f10e3c41fdeae39a477b03bf3ced90c
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:30:23 +0400

    task

---
+++ JS/Programs/Introduction/task578.js
@@ -0,0 +1,22 @@
+// JavaScript: Get the number of days in a month
+
+
+/***
+ * 
+ * 
+ * JavaScript Datetime: Exercise-3 with Solution
+Write a JavaScript function to get the number of days in a month.
+
+Test Data:
+console.log(getDaysInMonth(1, 2012));
+console.log(getDaysInMonth(2, 2012));
+console.log(getDaysInMonth(9, 2012));
+console.log(getDaysInMonth(12, 2012));
+Output :
+31
+29
+30
+31
+
+
+ */
