a0f702b94c2395ed1aa70df9074e6fa6cd4b2ce5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:27:53 +0400

    update2

--- JS/Programs/Introduction/task577.js
+++ JS/Programs/Introduction/task577.js
@@ -37,4 +37,26 @@ HTML Code:
 
 
 
+
+
+
+
+
+JavaScript Code:
+
+var curday = function(sp){
+today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //As January is 0.
+var yyyy = today.getFullYear();
+
+if(dd<10) dd='0'+dd;
+if(mm<10) mm='0'+mm;
+return (mm+sp+dd+sp+yyyy);
+};
+console.log(curday('/'));
+console.log(curday('-'));
+
+
+
  */
