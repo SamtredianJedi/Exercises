0c0c83cf950cc851a1f170040833e8a5159f9b5a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:23:00 +0400

    update3

--- JS/Programs/Introduction/task576.js
+++ JS/Programs/Introduction/task576.js
@@ -58,4 +58,41 @@ console.log(is_date([1, 2, 4, 0]));
 
 
 
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Checking a date object</title>
+</head>
+<body>
+</body>
+
+
+js:
+
+
+
+
+var is_date = function(input) {
+  if ( Object.prototype.toString.call(input) === "[object Date]" ) 
+    return true;
+  return false;   
+    };
+​
+console.log(is_date("October 13, 2014 11:13:00"));
+console.log(is_date(new Date(86400000)));
+console.log(is_date(new Date(99,5,24,11,33,30,0)));
+console.log(is_date([1, 2, 4, 0]));
+
+
+
+
  */
