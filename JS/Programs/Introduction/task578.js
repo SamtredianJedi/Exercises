efbe9f7c9ff5850b0cbaf00bf60c62b1eca4de75
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:31:57 +0400

    update3

--- JS/Programs/Introduction/task578.js
+++ JS/Programs/Introduction/task578.js
@@ -58,4 +58,46 @@ console.log(getDaysInMonth(12, 2012));
 
 
 
+
+
+
+
+
+Live Demo:
+
+
+
+html:
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>JS Bin</title>
+</head>
+<body>
+</body>
+
+
+
+js:
+
+
+
+
+var getDaysInMonth = function(month,year) {
+  // Here January is 1 based
+  //Day 0 is the last day in the previous month
+ return new Date(year, month, 0).getDate();
+// Here January is 0 based
+// return new Date(year, month+1, 0).getDate();
+};
+console.log(getDaysInMonth(1, 2012));
+console.log(getDaysInMonth(2, 2012));
+console.log(getDaysInMonth(9, 2012));
+console.log(getDaysInMonth(12, 2012));
+
+
+
  */
