a474dabf5c431ffe39935331a9f2d356cf5fd70d
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:23:29 +0400

    JavaScript: Check whether an input is a date object or not

--- JS/Programs/Introduction/task576.js
+++ JS/Programs/Introduction/task576.js
@@ -1,5 +1,8 @@
 // JavaScript: Check whether an input is a date object or not
 
+
+
+
 /**
  * 
  * 
@@ -91,6 +94,9 @@ console.log(is_date("October 13, 2014 11:13:00"));
 console.log(is_date(new Date(86400000)));
 console.log(is_date(new Date(99,5,24,11,33,30,0)));
 console.log(is_date([1, 2, 4, 0]));
+
+
+
 
 
 
