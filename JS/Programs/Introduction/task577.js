cfb163604c584622109037d6b6507a7f86a70003
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-21 22:29:08 +0400

    update3

--- JS/Programs/Introduction/task577.js
+++ JS/Programs/Introduction/task577.js
@@ -59,4 +59,50 @@ console.log(curday('-'));
 
 
 
+
+
+
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Get the current date</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+js:
+
+
+
+var curday = function(sp){
+today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //As January is 0.
+var yyyy = today.getFullYear();
+
+if(dd<10) dd='0'+dd;
+if(mm<10) mm='0'+mm;
+return (mm+sp+dd+sp+yyyy);
+};
+console.log(curday('/'));
+console.log(curday('-'));
+
+
+
+
+
+
+
+
  */
