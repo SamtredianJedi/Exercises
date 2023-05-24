4d3b7462fe10e1aec8a45dccb4da2d1592c446cd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 22:57:16 +0400

    task

---
+++ JS/Programs/Introduction/task580.js
@@ -0,0 +1,22 @@
+// JavaScript: Comparison between two dates
+
+
+/**
+ * 
+
+
+
+JavaScript Datetime: Exercise-5 with Solution
+Write a JavaScript function to compare dates (i.e. greater than, less than or equal to).
+
+Test Data:
+console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:00')));
+console.log(compare_dates(new Date('11/14/2013 00:01'), new Date('11/14/2013 00:00')));
+console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:01')));
+Output :
+"Date1 = Date2"
+"Date1 > Date2"
+"Date2 > Date1"
+
+
+ */
