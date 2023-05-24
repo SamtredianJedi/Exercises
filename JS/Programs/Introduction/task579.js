e81e7458985f47822dd60b649289698f488f35a6
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 22:53:31 +0400

    task

---
+++ JS/Programs/Introduction/task579.js
@@ -0,0 +1,18 @@
+// JavaScript: Get the month name from a particular date
+
+
+/**
+ * 
+ * 
+ * JavaScript Datetime: Exercise-4 with Solution
+Write a JavaScript function to get the month name from a particular date.
+
+Test Data:
+console.log(month_name(new Date("10/11/2009")));
+console.log(month_name(new Date("11/13/2014")));
+Output :
+"October"
+"November"
+
+
+ */
