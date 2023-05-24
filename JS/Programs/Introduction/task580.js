2e646e93dbc7b9b5a6cec5b2096960b4f5f76951
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 22:57:44 +0400

    update1

--- JS/Programs/Introduction/task580.js
+++ JS/Programs/Introduction/task580.js
@@ -19,4 +19,23 @@ Output :
 "Date2 > Date1"
 
 
+
+
+
+
+
+JavaScript Code:
+
+var compare_dates = function(date1,date2){
+     if (date1>date2) return ("Date1 > Date2");
+   else if (date1<date2) return ("Date2 > Date1");
+   else return ("Date1 = Date2"); 
+  }
+
+console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:00')));
+console.log(compare_dates(new Date('11/14/2013 00:01'), new Date('11/14/2013 00:00')));
+console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:01')));
+
+
+
  */
