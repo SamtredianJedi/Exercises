5183f7842f1ea963638c7508a1477dd9b292e5ac
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 22:55:11 +0400

    update2

--- JS/Programs/Introduction/task579.js
+++ JS/Programs/Introduction/task579.js
@@ -31,4 +31,19 @@ HTML Code:
 </html>
 
 
+
+
+
+
+JavaScript Code:
+
+var month_name = function(dt){
+mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  return mlist[dt.getMonth()];
+};
+console.log(month_name(new Date("10/11/2009")));
+console.log(month_name(new Date("11/13/2014")));
+
+
+
  */
