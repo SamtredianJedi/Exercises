bb9c5c5572a9657b746a06d1ae4e584d724c31a5
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 23:02:24 +0400

    update3

--- JS/Programs/Introduction/task581.js
+++ JS/Programs/Introduction/task581.js
@@ -44,4 +44,36 @@ console.log(add_minutes(new Date(2014,10,2), 30).toString());
 
 
 
+
+
+
+
+
+Live Demo:
+
+
+html:
+
+
+
+
+<head>
+<meta charset="utf-8">
+<title>Add minutes to a date object.</title>
+</head>
+<body>
+</body>
+
+
+js:
+
+
+
+
+var add_minutes =  function (dt, minutes) {
+    return new Date(dt.getTime() + minutes*60000);
+}
+console.log(add_minutes(new Date(2014,10,2), 30).toString());
+
+
  */
