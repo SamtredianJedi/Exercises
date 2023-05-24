eac3214923d4a16e0aef3c12046a012822f697f4
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-24 23:00:47 +0400

    task

---
+++ JS/Programs/Introduction/task581.js
@@ -0,0 +1,16 @@
+// JavaScript: Add minutes to a Date object
+
+/**
+ * 
+ * 
+ * JavaScript Datetime: Exercise-6 with Solution
+Write a JavaScript function to add specified minutes to a Date object.
+
+Test Data:
+console.log(add_minutes(new Date(2014,10,2), 30).toString());
+Output :
+"Sun Nov 02 2014 00:30:00 GMT+0530 (India Standard Time)"
+
+
+
+ */
