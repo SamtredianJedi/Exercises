f431d734253768015dc99ad2dc0796bc58584fd9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:10:00 +0400

    update2

--- JS/Programs/Introduction/task582.js
+++ JS/Programs/Introduction/task582.js
@@ -35,4 +35,24 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+var is_weekend =  function(date1){
+    var dt = new Date(date1);
+     
+    if(dt.getDay() == 6 || dt.getDay() == 0)
+       {
+        return "weekend";
+        } 
+}
+
+console.log(is_weekend('Nov 15, 2014'));
+console.log(is_weekend('Nov 16, 2014'));
+console.log(is_weekend('Nov 17, 2014'));
+
+
+
  */
