90eeaa8db0761e703e29cd4c0a1282a4261040bd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:15:36 +0400

    update3

--- JS/Programs/Introduction/task583.js
+++ JS/Programs/Introduction/task583.js
@@ -48,4 +48,42 @@ console.log(date_diff_indays('12/02/2014', '11/04/2014'));
 
 
 
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JavaScript difference between two dates in days</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+
+js:
+
+
+var date_diff_indays = function(date1, date2) {
+dt1 = new Date(date1);
+dt2 = new Date(date2);
+return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+}
+console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+
+
+
+
+
+
+
+
+
  */
