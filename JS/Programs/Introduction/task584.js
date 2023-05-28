f9d6407965c8175548993d668df977000969e182
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:18:09 +0400

    task

---
+++ JS/Programs/Introduction/task584.js
@@ -0,0 +1,22 @@
+// JavaScript: Find out the last day of a month
+
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Datetime: Exercise-9 with Solution
+Write a JavaScript function to get the last day of a month.
+
+Test Data:
+console.log(lastday(2014,0));
+console.log(lastday(2014,1));
+console.log(lastday(2014,11));
+Output :
+31
+28
+31
+
+
+
+ */
