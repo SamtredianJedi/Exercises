b51c586ad2d5a5c0354c379ebc632caefde8d871
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:12:17 +0400

    task

---
+++ JS/Programs/Introduction/task583.js
@@ -0,0 +1,20 @@
+// JavaScript: Difference between two dates in days
+
+
+/**
+ * 
+
+
+JavaScript Datetime: Exercise-8 with Solution
+Write a JavaScript function to get the difference between two dates in days.
+
+Test Data:
+console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+Output :
+216
+-28
+
+
+
+ */
