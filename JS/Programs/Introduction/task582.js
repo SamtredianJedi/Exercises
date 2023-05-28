dcf6751a51ea48993f62cb9b32d8d44966ee0e25
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:08:04 +0400

    task

---
+++ JS/Programs/Introduction/task582.js
@@ -0,0 +1,21 @@
+// JavaScript: Check whether a date is a weekend date
+
+/**
+ * 
+ * 
+ * 
+ JavaScript Datetime: Exercise-7 with Solution
+Write a JavaScript function to test whether a date is a weekend.
+
+Test Data :
+console.log(is_weekend('Nov 15, 2014'));
+console.log(is_weekend('Nov 16, 2014'));
+console.log(is_weekend('Nov 17, 2014'));
+Output :
+"weekend"
+"weekend"
+undefined
+
+
+
+ */
