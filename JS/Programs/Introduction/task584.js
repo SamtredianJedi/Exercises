5d160e0f1ac666eea834dc2166f6975a62a9004a
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-28 23:19:10 +0400

    update2

--- JS/Programs/Introduction/task584.js
+++ JS/Programs/Introduction/task584.js
@@ -37,4 +37,18 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+var lastday = function(y,m){
+return  new Date(y, m +1, 0).getDate();
+}
+console.log(lastday(2014,0));
+console.log(lastday(2014,1));
+console.log(lastday(2014,11));
+
+
+
  */
