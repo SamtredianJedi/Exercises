670eb7fb054750cad035a9a14920d5160a4bbec0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:54:09 +0400

    task

---
+++ JS/Programs/Introduction/task586.js
@@ -0,0 +1,21 @@
+// JavaScript: Maximum date from an array of dates
+
+
+/**
+ * 
+
+
+
+
+
+JavaScript Datetime: Exercise-11 with Solution
+Write a JavaScript function to get the maximum date from an array of dates.
+
+Test Data:
+console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+Output :
+"2015/02/02"
+
+
+
+ */
