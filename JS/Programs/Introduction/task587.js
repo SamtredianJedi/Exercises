f8ac5be620467c2ae69828873db2864c7763d133
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:58:29 +0400

    update2

--- JS/Programs/Introduction/task587.js
+++ JS/Programs/Introduction/task587.js
@@ -34,4 +34,27 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function min_date(all_dates) {
+ var min_dt = all_dates[0],
+  min_dtObj = new Date(all_dates[0]);
+ all_dates.forEach(function(dt, index)
+  {
+  if ( new Date( dt ) < min_dtObj)
+  {
+  min_dt = dt;
+  min_dtObj = new Date(dt);
+  }
+  });
+ return min_dt;
+  }
+console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+
+
  */
