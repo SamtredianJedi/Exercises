34e76ed567c7fe8d20a8be56c98e41cbed214b7b
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:51:24 +0400

    update2

--- JS/Programs/Introduction/task585.js
+++ JS/Programs/Introduction/task585.js
@@ -37,4 +37,21 @@ HTML Code:
 
 
 
+
+
+
+JavaScript Code:
+
+var yesterday =  function(date1){
+    var dt = new Date(date1);
+  return new Date((dt.setDate(dt.getDate()-1))).toString();
+}
+console.log(yesterday('Nov 15, 2014'));
+console.log(yesterday('Nov 16, 2015'));
+console.log(yesterday('Nov 17, 2016'));
+
+
+
+
+
  */
