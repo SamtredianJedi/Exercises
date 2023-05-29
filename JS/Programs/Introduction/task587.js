1e22f9187ee048696a6c23ca41a1538329a3e30e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:59:14 +0400

    update3

--- JS/Programs/Introduction/task587.js
+++ JS/Programs/Introduction/task587.js
@@ -57,4 +57,49 @@ console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Minimum date from an array of dates</title>
+</head>
+<body>
+</body>
+</html>
+
+js:
+
+
+
+function min_date(all_dates) {
+ var min_dt = all_dates[0],
+  min_dtObj = new Date(all_dates[0]);
+ all_dates.forEach(function(dt, index)
+  {
+  if ( new Date( dt ) < min_dtObj)
+  {
+  min_dt = dt;
+  min_dtObj = new Date(dt);
+  }
+  });
+ return min_dt;
+  }
+console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+
+
+
+
+
+
+
  */
