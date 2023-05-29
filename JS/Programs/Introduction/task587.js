cc9dbdb1efbe23ad47ae1a73e6dba896d0ba6556
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:57:31 +0400

    task

---
+++ JS/Programs/Introduction/task587.js
@@ -0,0 +1,19 @@
+// JavaScript: Minimum date from an array of dates
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * JavaScript Datetime: Exercise-12 with Solution
+Write a JavaScript function to get the minimum date from an array of dates.
+
+Test Data:
+console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+Output :
+"2015/01/03"
+
+
+
+ */
