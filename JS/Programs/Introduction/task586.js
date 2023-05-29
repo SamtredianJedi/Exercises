7ab2c918ac3edc65795ea6ec21ee50cab7eca396
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:55:10 +0400

    update2

--- JS/Programs/Introduction/task586.js
+++ JS/Programs/Introduction/task586.js
@@ -36,4 +36,29 @@ HTML Code:
 
 
 
+
+
+
+
+
+JavaScript Code:
+
+function max_date(all_dates) {
+var max_dt = all_dates[0],
+  max_dtObj = new Date(all_dates[0]);
+all_dates.forEach(function(dt, index)
+  {
+  if ( new Date( dt ) > max_dtObj)
+  {
+  max_dt = dt;
+  max_dtObj = new Date(dt);
+  }
+  });
+ return max_dt;
+  }
+console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+
+
+
  */
