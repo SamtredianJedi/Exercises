4b087df3a989a55fd13b41069ac52b22c0f2ddbd
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:47:14 +0400

    task

---
+++ JS/Programs/Introduction/task585.js
@@ -0,0 +1,23 @@
+// JavaScript: Find out which day was in yesterday
+
+
+/**
+ * 
+ * 
+
+
+JavaScript Datetime: Exercise-10 with Solution
+Write a JavaScript function to calculate 'yesterday's day.
+
+Test Data:
+console.log(yesterday('Nov 15, 2014'));
+console.log(yesterday('Nov 16, 2015'));
+console.log(yesterday('Nov 17, 2016'));
+Output :
+"Fri Nov 14 2014 00:00:00 GMT+0530 (India Standard Time)"
+"Sun Nov 15 2015 00:00:00 GMT+0530 (India Standard Time)"
+"Wed Nov 16 2016 00:00:00 GMT+0530 (India Standard Time)"
+
+
+
+ */
