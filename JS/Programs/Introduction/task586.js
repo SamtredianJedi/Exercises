c4e47fd41b127f95cbf0d2a53b2a5269f2f7dff9
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-05-29 23:56:07 +0400

    update3

--- JS/Programs/Introduction/task586.js
+++ JS/Programs/Introduction/task586.js
@@ -41,7 +41,54 @@ HTML Code:
 
 
 
+
+
 JavaScript Code:
+
+function max_date(all_dates) {
+var max_dt = all_dates[0],
+  max_dtObj = new Date(all_dates[0]);
+all_dates.forEach(function(dt, index)
+  {
+  if ( new Date( dt ) > max_dtObj)
+  {
+  max_dt = dt;
+  max_dtObj = new Date(dt);
+  }
+  });
+ return max_dt;
+  }
+console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+
+
+
+
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Maximum date from an array of dates</title>
+</head>
+<body>
+</body>
+</html>
+
+
+
+
+js:
+
+
+
+
 
 function max_date(all_dates) {
 var max_dt = all_dates[0],
