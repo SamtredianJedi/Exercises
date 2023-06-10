99b609cf40f771017c1ede40879f7034cc8b0e33
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:37:51 +0400

    update3

--- JS/Programs/Introduction/task589.js
+++ JS/Programs/Introduction/task589.js
@@ -54,4 +54,41 @@ console.log(days_of_a_year(2016));
 
 
 
+
+Live Demo:
+
+
+html:
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to get the amount of days of a year</title>
+</head>
+<body>
+
+</body>
+
+
+js:
+
+
+
+
+function days_of_a_year(year) 
+{
+   
+  return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+console.log(days_of_a_year(2015));
+console.log(days_of_a_year(2016));
+
+
+
  */
