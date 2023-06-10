d3cedf79947cddb860dc44ad45058fcd1f108bff
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:39:04 +0400

    task

---
+++ JS/Programs/Introduction/task590.js
@@ -0,0 +1,16 @@
+// JavaScript: Get the quarter of the year
+
+/**
+ * 
+ * 
+ * JavaScript Datetime: Exercise-15 with Solution
+Write a JavaScript function to get the quarter (1 to 4) of the year.
+
+Test Data:
+console.log(quarter_of_the_year(new Date(2015, 1, 21)));
+2
+console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+4
+
+
+ */
