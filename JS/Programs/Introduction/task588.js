22ae324f1d86cb574020f90aae37d7eb612f8fad
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:32:55 +0400

    task

---
+++ JS/Programs/Introduction/task588.js
@@ -0,0 +1,17 @@
+// JavaScript: Return the number of minutes in hours and minutes
+
+/**
+ * 
+ * 
+ * 
+ * JavaScript Datetime: Exercise-13 with Solution
+Write a JavaScript function that returns the number of minutes in hours and minutes.
+
+Test Data :
+console.log(timeConvert(200));
+Output :
+"200 minutes = 3 hour(s) and 20 minute(s)."
+
+
+
+ */
