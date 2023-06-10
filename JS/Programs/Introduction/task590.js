27ebffc134e9690a765c889205a4261e2a0ed967
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:40:39 +0400

    update3

--- JS/Programs/Introduction/task590.js
+++ JS/Programs/Introduction/task590.js
@@ -48,4 +48,43 @@ console.log(quarter_of_the_year(new Date(2015, 10, 18)));
 
 
 
+
+
+
+Live Demo:
+
+
+html:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to get the the quarter (1 to 4) of the year</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+js:
+
+function quarter_of_the_year(date) 
+  {
+    var month = date.getMonth() + 1;
+    return (Math.ceil(month / 3));
+  }
+
+
+console.log(quarter_of_the_year(new Date()));
+console.log(quarter_of_the_year(new Date(2015, 1, 21))); 
+console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+
+
+
+
+
+
  */
