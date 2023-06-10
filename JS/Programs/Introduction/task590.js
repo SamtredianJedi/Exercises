44dda490776842173630ebdad2d3ba850db3595e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:39:56 +0400

    update2

--- JS/Programs/Introduction/task590.js
+++ JS/Programs/Introduction/task590.js
@@ -32,4 +32,20 @@ HTML Code:
 
 
 
+
+JavaScript Code:
+
+function quarter_of_the_year(date) 
+  {
+    var month = date.getMonth() + 1;
+    return (Math.ceil(month / 3));
+  }
+
+
+console.log(quarter_of_the_year(new Date()));
+console.log(quarter_of_the_year(new Date(2015, 1, 21))); 
+console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+
+
  */
