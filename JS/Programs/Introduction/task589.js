5e1469a75e0dbdc3c251947cdec9c6e64129475e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:36:42 +0400

    update1

--- JS/Programs/Introduction/task589.js
+++ JS/Programs/Introduction/task589.js
@@ -15,4 +15,22 @@ console.log(days_of_a_year(2016));
 
 
 
+
+
+
+HTML Code:
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>JavaScript function to get the amount of days of a year</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
  */
