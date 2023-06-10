1d6910d476a53665c9addf804ea63c8a180b4c76
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:37:07 +0400

    update2

--- JS/Programs/Introduction/task589.js
+++ JS/Programs/Introduction/task589.js
@@ -33,4 +33,25 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function days_of_a_year(year) 
+{
+   
+  return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+console.log(days_of_a_year(2015));
+console.log(days_of_a_year(2016));
+
+
+
  */
