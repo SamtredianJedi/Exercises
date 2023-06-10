a4a0337a41d18767c5ece95f10de6f3411d8999e
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:34:59 +0400

    update3

--- JS/Programs/Introduction/task588.js
+++ JS/Programs/Introduction/task588.js
@@ -51,4 +51,41 @@ console.log(timeConvert(200));
 
 
 
+
+Live Demo:
+
+
+
+html:
+
+
+
+
+<head>
+  <meta charset="utf-8">
+  <title>Return the number of minutes in hours and minutes.</title>
+</head>
+<body>
+​
+</body>
+
+
+
+js:
+
+
+
+
+function timeConvert(n) {
+var num = n;
+var hours = (num / 60);
+var rhours = Math.floor(hours);
+var minutes = (hours - rhours) * 60;
+var rminutes = Math.round(minutes);
+return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+}
+​
+console.log(timeConvert(200));
+
+
  */
