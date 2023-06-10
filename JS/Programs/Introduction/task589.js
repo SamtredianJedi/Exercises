e00572c2783267a53fbb4faf044a304957b9fa0f
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:36:16 +0400

    task

---
+++ JS/Programs/Introduction/task589.js
@@ -0,0 +1,18 @@
+// JavaScript: Get the amount of days of a year
+
+/**
+ * 
+ * 
+ * 
+ JavaScript Datetime: Exercise-14 with Solution
+Write a JavaScript function to get the number of days in a year.
+
+Test Data :
+console.log(days_of_a_year(2015));
+365
+console.log(days_of_a_year(2016));
+366
+
+
+
+ */
