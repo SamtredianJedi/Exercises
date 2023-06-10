20352f816688aadb1cb4ef3a49764beaf9eae9c3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-06-10 16:34:13 +0400

    update2

--- JS/Programs/Introduction/task588.js
+++ JS/Programs/Introduction/task588.js
@@ -32,4 +32,23 @@ HTML Code:
 
 
 
+
+
+
+
+JavaScript Code:
+
+function timeConvert(n) {
+var num = n;
+var hours = (num / 60);
+var rhours = Math.floor(hours);
+var minutes = (hours - rhours) * 60;
+var rminutes = Math.round(minutes);
+return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+}
+
+console.log(timeConvert(200));
+
+
+
  */
